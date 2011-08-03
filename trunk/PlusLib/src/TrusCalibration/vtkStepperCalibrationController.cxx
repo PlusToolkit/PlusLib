e11c290d73d15f75de87466a3865490bb9c4da75
pinter <pinter@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-08-03 20:13:06 +0000

    Clean up configuration files; 2 configuration files with most recent results (they do not go into the package); Old-type (separate) configuration files are not allowed any more (it threw a warning, but later it caused problems)

--- trunk/PlusLib/src/TrusCalibration/vtkStepperCalibrationController.cxx
+++ trunk/PlusLib/src/TrusCalibration/vtkStepperCalibrationController.cxx
@@ -2548,12 +2548,6 @@ PlusStatus vtkStepperCalibrationController::ReadConfiguration( vtkXMLDataElement
   }
 
 	vtkSmartPointer<vtkXMLDataElement> usCalibration = configData->FindNestedElementWithName("USCalibration");
-	if (usCalibration == NULL) { // Check if it is a separate data calibration configuration file
-		if (STRCASECMP(configData->GetName(), "USCalibration") == 0) {
-      LOG_WARNING("Non-unified configuration detected! Phantom definition has to be loaded from a separate file!");
-			usCalibration = configData;
-		}
-	}
 	if (usCalibration == NULL) {
     LOG_ERROR("Cannot find USCalibration element in XML tree!");
     return PLUS_FAIL;
