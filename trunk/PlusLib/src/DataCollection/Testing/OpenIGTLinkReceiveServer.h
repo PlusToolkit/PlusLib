3a25d71384a1424ab7c5ddd1a0d534125fc17ae7
ungi <ungi@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-08-13 04:07:33 +0000

    re #12: Updated test to actually do OpenIGTLink communication.

---
+++ trunk/PlusLib/src/DataCollection/Testing/OpenIGTLinkReceiveServer.h
@@ -0,0 +1,48 @@
+
+#ifndef __OpenIGTLinkReceiveServer_h
+#define __OpenIGTLinkReceiveServer_h
+
+
+#include "PlusConfigure.h"
+
+#include "vtkMultiThreader.h"
+
+#include "igtlMessageHeader.h"
+#include "igtlServerSocket.h"
+
+
+
+class OpenIGTLinkReceiveServer
+{
+
+public:
+  
+  OpenIGTLinkReceiveServer( int port );
+  ~OpenIGTLinkReceiveServer();
+  
+  PlusStatus Start();
+  PlusStatus Stop();
+  
+  bool GetActive();
+  int  GetNumberOfReceivedMessages();
+  
+  igtl::ServerSocket::Pointer GetServerSocket();
+  
+  friend static void* vtkReceiverThread( vtkMultiThreader::ThreadInfo *data );
+  
+private:
+  
+  int ReceiveTransform( igtl::Socket * socket, igtl::MessageHeader * header );
+  int ReceiveImage( igtl::Socket * socket, igtl::MessageHeader * header );
+  
+  igtl::ServerSocket::Pointer ServerSocket;
+  
+  vtkMultiThreader* Threader;
+  
+  bool IsActive;
+  int  ThreadId;
+  int  NumberOfReceivedMessages;
+
+};
+
+#endif
