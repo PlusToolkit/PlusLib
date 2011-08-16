b489e04a0bbb3b0bc1ff4cb347e049231fec5915
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-08-16 15:25:26 +0000

    re #249: Added vtkAccurateTimerTest for testing vtkAccurateTimer delays and potential thread hangs

--- trunk/PlusLib/src/PlusCommon/WindowsAccurateTimer.h
+++ trunk/PlusLib/src/PlusCommon/WindowsAccurateTimer.h
@@ -13,8 +13,6 @@
 // causes delays when run in trhe background unless priority is adjusted. Priority does not cause any problems unless your
 // process monopolises all available CPU - timer code by itself consumes pretty much 0%.
 
-//Uncomment TIMING_DEBUG define to provide you with statistical feedback from the timer.
-
 // ------------------------------------------------------
 #ifndef __WINDOWS_ACCURATE_TIMER_H__
 #define __WINDOWS_ACCURATE_TIMER_H__
@@ -65,12 +63,6 @@ public:
    ///////////////////////////////////////////////////////////////
    void Wait(int timeout)
    {
-//#define TIMING_DEBUG
-#ifdef TIMING_DEBUG
-      LARGE_INTEGER s[2]; 
-      QueryPerformanceCounter(&s[0]);     // timing starts
-
-#endif
       if ( timeout > 0 )   // anything to wait on ?
       {
          HANDLE tHandle = 0;
@@ -84,17 +76,6 @@ public:
          SuspendThread(tHandle);        // do the wait
          CloseHandle(tHandle);
       }
-#ifdef TIMING_DEBUG
-      QueryPerformanceCounter(&s[1]);   // wake up call and timing ends
-      double diff = ((double)s[1].QuadPart - (double)s[0].QuadPart)/(double)freq.QuadPart;
-      double percent = /*Absolute*/(diff-timeout)*100.0/(timeout?timeout:1);
-
-      printf("intended to sleep for: %3ld. slept for: %8.4f. error = %8.4f\n",
-             timeout, diff, percent);
-
-      error += percent;
-      ++count;
-#endif
    }
    inline double        Absolute(double val) { return val > 0 ? val : -val; }
    inline LARGE_INTEGER GetFrequency()       { return freq; };
@@ -139,13 +120,14 @@ public:
       __int64 now = s[1].QuadPart+pThis->halfMsec.QuadPart;
 
       EnterCriticalSection(&pThis->crit);
-	  std::deque<WaitData>::iterator it = pThis->waitPool.begin();
+	    std::deque<WaitData>::iterator it = pThis->waitPool.begin();
       while ( it != pThis->waitPool.end() )
       {
          if ( now >= it->timeout.QuadPart )
          {
             ResumeThread(it->h);
             it = pThis->waitPool.erase(it);
+            //it = pThis->waitPool.begin();
             continue;
          }
          ++it;
