3245a05b7dfac7746c7f4077ec0d57b122aa5bd8
ungi <ungi@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-08-22 05:11:59 +0000

    re #206: Organize files and classes.

---
+++ trunk/PlusLib/src/DataCollection/vtkPlusOpenIGTLinkServer.cxx
@@ -0,0 +1,63 @@
+
+#include "vtkPlusOpenIGTLinkServer.h"
+
+
+
+vtkCxxRevisionMacro( vtkPlusOpenIGTLinkServer, "$Revision: 1.0 $" );
+vtkStandardNewMacro( vtkPlusOpenIGTLinkServer ); 
+
+
+
+void
+vtkPlusOpenIGTLinkServer
+::PrintSelf( ostream& os, vtkIndent indent )
+{
+	this->Superclass::PrintSelf( os, indent );
+}
+
+
+
+void
+vtkPlusOpenIGTLinkServer
+::SetDataCollector( vtkDataCollector* dataCollector )
+{
+  this->DataCollector = dataCollector;
+}
+
+
+
+int
+vtkPlusOpenIGTLinkServer
+::Initialize( std::string &strError )
+{
+  if (    this->DataCollector == NULL
+       || this->DataCollector->GetTracker() == NULL )
+    {
+    LOG_ERROR( "Tried to initialize vtkPlusOpenIGTLinkServer without DataCollector." );
+    return 1;
+    }
+  
+  return 0;
+}
+
+
+
+/**
+ * Protected constructor.
+ */
+vtkPlusOpenIGTLinkServer
+::vtkPlusOpenIGTLinkServer()
+{
+  this->DataCollector = NULL;
+}
+
+
+
+/**
+ * Destructor.
+ */
+vtkPlusOpenIGTLinkServer
+::~vtkPlusOpenIGTLinkServer()
+{
+  
+}
---
+++ trunk/PlusLib/src/DataCollection/vtkPlusOpenIGTLinkServer.h
@@ -0,0 +1,58 @@
+
+#ifndef __VTKPLUSOPENIGTLINKSERVER_H
+#define __VTKPLUSOPENIGTLINKSERVER_H
+
+
+#include <string>
+#include <vector>
+
+#include "vtkObject.h"
+
+#include "igtlOSUtil.h"
+#include "igtlPositionMessage.h"
+#include "igtlClientSocket.h"
+
+#include "vtkDataCollector.h"
+
+
+
+/**
+ * This class provides a network interface for Plus functions
+ * as an OpenIGTLink server.
+ */
+class
+VTK_EXPORT 
+vtkPlusOpenIGTLinkServer
+: public vtkObject
+{
+public:
+  
+  static vtkPlusOpenIGTLinkServer *New();
+  vtkTypeRevisionMacro( vtkPlusOpenIGTLinkServer, vtkObject );
+  virtual void PrintSelf( ostream& os, vtkIndent indent );
+  
+  
+  void SetDataCollector( vtkDataCollector* dataCollector );
+  int Initialize( std::string &strError );
+  
+  
+protected:
+  
+  vtkPlusOpenIGTLinkServer();
+  virtual ~vtkPlusOpenIGTLinkServer();
+  
+  
+private:
+	
+  vtkPlusOpenIGTLinkServer( const vtkPlusOpenIGTLinkServer& );
+  void operator=( const vtkPlusOpenIGTLinkServer& );
+  
+  
+  vtkDataCollector*  DataCollector;
+  
+  
+};
+
+
+#endif
+
