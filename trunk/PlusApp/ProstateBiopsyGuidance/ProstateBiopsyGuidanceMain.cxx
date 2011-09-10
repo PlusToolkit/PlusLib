173f55f5233644b9a3f4c9ae411b2ab0de23f8b6
hussam.ashab <hussam.ashab@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-09-10 02:21:03 +0000

    Remove #include "vibrolib.h" from main source file

--- trunk/PlusApp/ProstateBiopsyGuidance/ProstateBiopsyGuidanceMain.cxx
+++ trunk/PlusApp/ProstateBiopsyGuidance/ProstateBiopsyGuidanceMain.cxx
@@ -1,10 +1,13 @@
 #include "ProstateBiopsyGuidanceGUI.h"
+<<<<<<< .mine
+=======
 #include <qapplication.h>
 #include <qpushbutton.h>
 #include <qfont.h>
 // QT includes
 #include <qapplication.h>
 #include "ProstateBiopsyGuidance.h"
+>>>>>>> .r719
 
 int main( int argc, char** argv )
 {
