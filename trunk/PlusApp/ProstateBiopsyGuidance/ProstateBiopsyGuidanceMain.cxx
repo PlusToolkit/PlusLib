7838cbbaae21a93cd7743f279b9c42e064a5eb1c
saman <saman@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-09-27 16:59:32 +0000

    A draft GUI is implemented for ProstateBiopsyGuidance project which uses a widget called "DeviceSetSelectorWidget" residing in CommonWidget project. The slots and signals are not implemented yet and this is just a combination of GUI files and resources with ProstateBiopsyGuidance basic functionality.

--- trunk/PlusApp/ProstateBiopsyGuidance/ProstateBiopsyGuidanceMain.cxx
+++ trunk/PlusApp/ProstateBiopsyGuidance/ProstateBiopsyGuidanceMain.cxx
@@ -1,18 +1,11 @@
-#include "ProstateBiopsyGuidanceGUI.h"
-#include <qapplication.h>
-#include <qpushbutton.h>
-#include <qfont.h>
-// QT includes
-#include <qapplication.h>
-#include "ProstateBiopsyGuidance.h"
+#include <QtGui/QApplication>
+#include "prostatebiopsyguidancegui.h"
 
-
-
-
-int main( int argc, char** argv )
+int main(int argc, char *argv[])
 {
-     QApplication app(argc, argv);
-     ProstateBiopsyGuidanceGUI dialog;
-     return dialog.exec();
+    QApplication a(argc, argv);
+    ProstateBiopsyGuidanceGUI w;
+    w.show();
 
+    return a.exec();
 }
