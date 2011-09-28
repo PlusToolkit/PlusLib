b35e75a4b473f64ff4813b70e9a1588205c4a21f
ungi <ungi@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-09-28 20:21:59 +0000

    re #182: Class skeleton added.

---
+++ trunk/PlusLib/src/Tracking/OpenIGTLinkTracking/vtkOpenIGTLinkTracker.cxx
@@ -0,0 +1,229 @@
+#include "PlusConfigure.h"
+
+#include "vtkOpenIGTLinkTracker.h"
+
+#include <sstream>
+
+
+#include "vtkMatrix4x4.h"
+#include "vtkObjectFactory.h"
+#include "vtksys/SystemTools.hxx"
+#include "vtkTransform.h"
+#include "vtkXMLDataElement.h"
+
+#include "PlusConfigure.h"
+#include "vtkTracker.h"
+#include "vtkTrackerTool.h"
+#include "vtkTrackerBuffer.h"
+
+vtkOpenIGTLinkTracker*
+vtkOpenIGTLinkTracker::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkOpenIGTLinkTracker");
+  if( ret )
+  {
+    return ( vtkOpenIGTLinkTracker* )ret;
+  }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkOpenIGTLinkTracker;
+}
+
+
+
+vtkOpenIGTLinkTracker
+::vtkOpenIGTLinkTracker()
+{
+  this->LocalTrackerBuffer = NULL;
+
+  this->FrameNumber = 0;
+
+    // Set the maximum number of sensors that this class can handle
+  this->SetNumberOfTools( 12 ); 
+  this->NumberOfSensors = 0; 
+}
+
+
+
+vtkOpenIGTLinkTracker
+::~vtkOpenIGTLinkTracker() 
+{
+  if ( this->Tracking )
+  {
+    this->StopTracking();
+  }
+
+  if ( this->LocalTrackerBuffer != NULL )
+  {
+    this->LocalTrackerBuffer->Delete(); 
+    this->LocalTrackerBuffer = NULL; 
+  }
+}
+
+
+
+void
+vtkOpenIGTLinkTracker
+::PrintSelf( ostream& os, vtkIndent indent )
+{
+  vtkTracker::PrintSelf( os, indent );
+}
+
+
+
+/**
+ * Starts up a data receiver thread.
+ * Sends the PlusServer a StartTracking command.
+ * After that, PlusServer sends tracking data to this client as long as the connection is alive.
+ */
+PlusStatus
+vtkOpenIGTLinkTracker
+::Connect()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::Connect" ); 
+  
+  
+  return PLUS_SUCCESS; 
+}
+
+
+
+PlusStatus
+vtkOpenIGTLinkTracker
+::Disconnect()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::Disconnect" ); 
+  return this->StopTracking(); 
+}
+
+
+
+PlusStatus
+vtkOpenIGTLinkTracker
+::Probe()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::Probe" ); 
+
+  return PLUS_SUCCESS; 
+} 
+
+
+
+/**
+* @returns 1 on success, 0 on failure.
+*/
+PlusStatus
+vtkOpenIGTLinkTracker
+::InternalStartTracking()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::InternalStartTracking" ); 
+  if ( this->Tracking )
+  {
+    return PLUS_SUCCESS;
+  }
+
+  if ( ! this->InitOpenIGTLinkTracker() )
+  {
+    LOG_ERROR( "Couldn't initialize vtkOpenIGTLinkTracker" );
+    return PLUS_FAIL;
+  } 
+  
+  return PLUS_SUCCESS;
+}
+
+
+
+PlusStatus
+vtkOpenIGTLinkTracker
+::InternalStopTracking()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::InternalStopTracking" ); 
+  
+  return PLUS_SUCCESS;
+}
+
+
+
+/**
+* This function is called by the tracker thread.
+*/
+PlusStatus
+vtkOpenIGTLinkTracker
+::InternalUpdate()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::InternalUpdate" ); 
+
+  if ( ! this->Tracking )
+  {
+    LOG_ERROR("called Update() when not tracking" );
+    return PLUS_FAIL;
+  }
+
+  // TODO: Frame number is fake here!
+  ++ this->FrameNumber;
+  
+  return PLUS_SUCCESS;
+}
+
+
+
+PlusStatus
+vtkOpenIGTLinkTracker
+::InitOpenIGTLinkTracker()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::InitOpenIGTLinkTracker" ); 
+  return this->Connect(); 
+}
+
+
+
+PlusStatus
+vtkOpenIGTLinkTracker
+::ReadConfiguration( vtkXMLDataElement* config )
+{
+  // Read superclass configuration first
+  Superclass::ReadConfiguration( config ); 
+
+  LOG_TRACE( "vtkOpenIGTLinkTracker::ReadConfiguration" ); 
+  if ( config == NULL ) 
+  {
+    LOG_ERROR("Unable to find OpenIGTLinkTracker XML data element");
+    return PLUS_FAIL; 
+  }
+
+	vtkSmartPointer<vtkXMLDataElement> dataCollectionConfig = config->FindNestedElementWithName("USDataCollection");
+	if (dataCollectionConfig == NULL)
+  {
+    LOG_ERROR("Cannot find USDataCollection element in XML tree!");
+		return PLUS_FAIL;
+	}
+
+  vtkSmartPointer<vtkXMLDataElement> trackerConfig = dataCollectionConfig->FindNestedElementWithName("Tracker"); 
+  if (trackerConfig == NULL) 
+  {
+    LOG_ERROR("Cannot find Tracker element in XML tree!");
+		return PLUS_FAIL;
+  }
+
+  return PLUS_SUCCESS;
+}
+
+
+
+PlusStatus
+vtkOpenIGTLinkTracker
+::WriteConfiguration( vtkXMLDataElement* config )
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::WriteConfiguration" ); 
+  if ( config == NULL )
+  {
+    LOG_ERROR("Invalid write configuration pointer");
+    return PLUS_FAIL;
+  }
+
+  //config->SetName("Ascension3DGTracker");
+
+  LOG_ERROR("Not implemented");
+  return PLUS_FAIL;
+}
+
---
+++ trunk/PlusLib/src/Tracking/OpenIGTLinkTracking/vtkOpenIGTLinkTracker.h
@@ -0,0 +1,100 @@
+
+#ifndef __vtkOpenIGTLinkTracker_h
+#define __vtkOpenIGTLinkTracker_h
+
+#include "vtkTracker.h"
+
+class vtkTrackerBuffer; 
+
+
+
+/**
+ * This is an OpenIGTLink client.
+ * On connect, it sends the PlusServer a StartTracking plus command.
+ * After that, PlusServer sends tracking data to this client as long as the connection is alive.
+ * On "start tracking" this client starts buffering the incoming transforms.
+ * On "stop tracking" it stops buffering.
+ * On disconnect, it releases socket connection to server.
+ * When PlusServer detects this, stops the tracking hardware.
+ */
+class
+VTK_EXPORT
+vtkOpenIGTLinkTracker : public vtkTracker
+{
+public:
+
+	static vtkOpenIGTLinkTracker *New();
+	vtkTypeMacro( vtkOpenIGTLinkTracker,vtkTracker );
+	void PrintSelf( ostream& os, vtkIndent indent );
+
+	// Description:
+	// Connect to device
+	PlusStatus Connect();
+
+	// Description:
+	// Disconnect from device 
+	virtual PlusStatus Disconnect();
+
+	// Description:
+	// Probe to see if the tracking system is present on the
+	// specified serial port.  If the SerialPort is set to -1, then all serial ports will be checked.
+	PlusStatus Probe();
+
+	// Description:
+	// Get an update from the tracking system and push the new transforms
+	// to the tools.  This should only be used within vtkTracker.cxx.
+	PlusStatus InternalUpdate();
+
+	// Description:
+	// Read/write BrachyStepper configuration to xml data
+	PlusStatus ReadConfiguration( vtkXMLDataElement* config ); 
+	PlusStatus WriteConfiguration( vtkXMLDataElement* config );
+
+	// Description:
+	// Get number of device names
+	vtkGetMacro(NumberOfSensors, int);
+ 
+  
+protected:
+  
+	vtkOpenIGTLinkTracker();
+	~vtkOpenIGTLinkTracker();
+
+	// Description:
+	// Initialize the tracking device
+	PlusStatus InitOpenIGTLinkTracker();
+
+	// Description:
+	// Start the tracking system.  The tracking system is brought from its ground state into full tracking mode.
+    // The device will only be reset if communication cannot be established without a reset.
+	PlusStatus InternalStartTracking();
+
+	// Description:
+	// Stop the tracking system and bring it back to its ground state:
+	// Initialized, not tracking, at 9600 Baud.
+	PlusStatus InternalStopTracking();
+  
+	vtkTrackerBuffer* LocalTrackerBuffer; 
+	
+  
+private:  // Definitions.
+	
+	// typedef std::map< std::string, std::vector < double > >  TrackerToolTransformContainerType;
+	
+	
+private:  // Functions.
+  
+  vtkOpenIGTLinkTracker( const vtkOpenIGTLinkTracker& );
+	void operator=( const vtkOpenIGTLinkTracker& );  
+	
+  
+  
+private:  // Variables.
+  	
+	
+	unsigned int FrameNumber;
+	int NumberOfSensors; 
+  
+};
+
+#endif
