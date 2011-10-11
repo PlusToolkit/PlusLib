0eebd1d76312478c279a163ce51a71b8e8bd644d
Andras Lasso <lasso@queensu.ca>
2011-10-11 02:48:01 +0000

    Added output redirection of CreateDoc.bat to CreateDoc.log Removed wrong comment on the itk namespace.

--- PlusLib/src/ImageAcquisition/itkFcsvReader.h
+++ PlusLib/src/ImageAcquisition/itkFcsvReader.h
@@ -11,17 +11,13 @@
 
 #include <vector>
 #include <fstream>
-#include "itkMetaSceneConverter.h"  // needed???? .... convert Meta to Spatial
-#include "itkGroupSpatialObject.h"  // needed????
-#include "itkSceneSpatialObject.h"  // needed????
 #include "itkProcessObject.h"
 #include "itkSpatialObject.h"
 #include "itkMetaEvent.h"
 #include <itkDefaultStaticMeshTraits.h>
 #include "itkSpatialObjectReader.h"
 
-#include "itkImageIOBase.h" /// needed??
-// LightProcessObject
+#include "itkImageIOBase.h"
 
 namespace itk
 {
