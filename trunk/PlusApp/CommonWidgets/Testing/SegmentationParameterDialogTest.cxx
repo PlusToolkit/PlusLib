66a6ad008059206a97698ee1bb9c4ffa64d8d43e
pinter <pinter@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-10-18 16:41:36 +0000

    Re #309, Re #276: Standalone app that provides connecting to device sets and changing changing the segmentation parameters using SegmentationParameterDialog. It will act as the test for that dialog too.

---
+++ trunk/PlusApp/CommonWidgets/Testing/SegmentationParameterDialogTest.cxx
@@ -0,0 +1,189 @@
+/*=Plus=header=begin======================================================
+  Program: Plus
+  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+  See License.txt for details.
+=========================================================Plus=header=end*/ 
+
+#include "SegmentationParameterDialogTest.h"
+
+#include "vtkXMLUtilities.h"
+
+#include "vtkTracker.h"
+#include "vtkDataCollector.h"
+#include "DeviceSetSelectorWidget.h"
+#include "SegmentationParameterDialog.h"
+#include "ConfigFileSaverDialog.h"
+
+//-----------------------------------------------------------------------------
+
+SegmentationParameterDialogTest::SegmentationParameterDialogTest(QWidget *parent, Qt::WFlags flags)
+  : QDialog(parent, flags)
+  , m_DeviceSetSelectorWidget(NULL)
+  , m_DataCollector(NULL)
+{
+  this->setMinimumSize(480, 320);
+  this->setMaximumSize(480, 320);
+
+  // Create device set selector widget
+	m_DeviceSetSelectorWidget = new DeviceSetSelectorWidget(this);
+  m_DeviceSetSelectorWidget->setMinimumWidth(472);
+  m_DeviceSetSelectorWidget->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+	connect( m_DeviceSetSelectorWidget, SIGNAL( ConnectToDevicesByConfigFileInvoked(std::string) ), this, SLOT( ConnectToDevicesByConfigFile(std::string) ) );
+
+  // Create save configuration button
+  m_SaveButton = new QPushButton(tr("Save configuration..."), this);
+  m_SaveButton->setMinimumWidth(120);
+  m_SaveButton->setEnabled(false);
+	connect( m_SaveButton, SIGNAL( clicked() ), this, SLOT( SaveConfigurationClicked() ) );
+
+  // Insert widgets into placeholders
+  QGridLayout* mainGrid = new QGridLayout(this, 2, 1, 4, 4, "");
+  mainGrid->addWidget(m_DeviceSetSelectorWidget, 0, 0, Qt::AlignHCenter);
+  mainGrid->addWidget(m_SaveButton, 1, 0, Qt::AlignHCenter);
+  this->setLayout(mainGrid);
+}
+
+//-----------------------------------------------------------------------------
+
+SegmentationParameterDialogTest::~SegmentationParameterDialogTest()
+{
+  if (m_DataCollector != NULL) {
+	  m_DataCollector->Stop();
+  }
+  m_DataCollector = NULL;
+}
+
+//-----------------------------------------------------------------------------
+
+void SegmentationParameterDialogTest::ConnectToDevicesByConfigFile(std::string aConfigFile)
+{
+	LOG_TRACE("SegmentationParameterDialogTest::ConnectToDevicesByConfigFile");
+
+  QApplication::setOverrideCursor(QCursor(Qt::BusyCursor));
+
+  // If not empty, then try to connect; empty parameter string means disconnect
+  if (STRCASECMP(aConfigFile.c_str(), "") != 0) {
+    // Read configuration
+    vtkSmartPointer<vtkXMLDataElement> configRootElement = vtkXMLUtilities::ReadElementFromFile(aConfigFile.c_str());
+    if (configRootElement == NULL) {	
+      LOG_ERROR("Unable to read configuration from file " << aConfigFile); 
+      return;
+    }
+
+    vtkPlusConfig::GetInstance()->SetDeviceSetConfigurationData(configRootElement); 
+
+    // If connection has been successfully created then this action should disconnect
+    if (! m_DeviceSetSelectorWidget->GetConnectionSuccessful()) {
+	    LOG_INFO("Connect to devices"); 
+
+      // Disable main window
+   	  this->setVisible(false);
+
+	    // Create dialog
+	    QDialog* connectDialog = new QDialog(this, Qt::Dialog);
+	    connectDialog->setMinimumSize(QSize(360,80));
+	    connectDialog->setCaption(tr("fCal"));
+	    connectDialog->setBackgroundColor(QColor(224, 224, 224));
+
+	    QLabel* connectLabel = new QLabel(QString("Connecting to devices, please wait..."), connectDialog);
+	    connectLabel->setFont(QFont("SansSerif", 16));
+
+	    QHBoxLayout* layout = new QHBoxLayout();
+	    layout->addWidget(connectLabel);
+
+	    connectDialog->setLayout(layout);
+	    connectDialog->show();
+
+	    QApplication::processEvents();
+
+	    // Connect to devices
+	    if (this->StartDataCollection() != PLUS_SUCCESS) {
+		    LOG_ERROR("Unable to start collecting data!");
+		    m_DeviceSetSelectorWidget->SetConnectionSuccessful(false);
+
+	    } else {
+        // Successful connection
+		    m_DeviceSetSelectorWidget->SetConnectionSuccessful(true);
+
+        vtkPlusConfig::GetInstance()->SaveApplicationConfigurationToFile();
+
+        // Show segmentation parameter dialog
+        SegmentationParameterDialog* segmentationParamDialog = new SegmentationParameterDialog(this, m_DataCollector);
+        segmentationParamDialog->exec();
+
+        delete segmentationParamDialog;
+
+        m_SaveButton->setEnabled(true);
+	    }
+
+	    // Close dialog
+	    connectDialog->done(0);
+      connectDialog->hide();
+      delete connectDialog;
+
+      // Re-enable main window
+   	  this->setVisible(true);
+    }
+
+  } else { // Disconnect
+	  if ((m_DataCollector != NULL) && (m_DataCollector->GetInitialized())) {
+
+		  m_DataCollector->Stop();
+		  m_DataCollector->Disconnect();
+
+		  m_DeviceSetSelectorWidget->SetConnectionSuccessful(false);
+	  }
+  }
+
+  QApplication::restoreOverrideCursor();
+}
+
+//-----------------------------------------------------------------------------
+
+PlusStatus SegmentationParameterDialogTest::StartDataCollection()
+{
+  LOG_TRACE("SegmentationParameterDialogTest::StartDataCollection"); 
+
+  // Stop data collection if already started
+  if (m_DataCollector != NULL) {
+	  m_DataCollector->Stop();
+  } else {
+	  m_DataCollector = vtkDataCollector::New();
+  }
+
+  // Initialize data collector and read configuration
+  if (m_DataCollector->ReadConfiguration(vtkPlusConfig::GetInstance()->GetDeviceSetConfigurationData()) != PLUS_SUCCESS) {
+	  return PLUS_FAIL;
+  }
+
+  if (m_DataCollector->Initialize() != PLUS_SUCCESS) {
+	  return PLUS_FAIL;
+  }
+
+  if (m_DataCollector->Start() != PLUS_SUCCESS) {
+	  return PLUS_FAIL;
+  }
+
+  if ((m_DataCollector->GetTracker() == NULL) || (m_DataCollector->GetTracker()->GetNumberOfTools() < 1)) {
+	  LOG_WARNING("Unable to initialize Tracker!"); 
+  }
+
+  if (! m_DataCollector->GetInitialized()) {
+	  LOG_ERROR("Unable to initialize DataCollector!"); 
+	  return PLUS_FAIL;
+  }
+
+  return PLUS_SUCCESS;
+}
+
+//-----------------------------------------------------------------------------
+
+void SegmentationParameterDialogTest::SaveConfigurationClicked()
+{
+	LOG_TRACE("SegmentationParameterDialogTest::SaveConfigurationClicked"); 
+
+  ConfigFileSaverDialog* configSaverDialog = new ConfigFileSaverDialog(this);
+  configSaverDialog->exec();
+
+  delete configSaverDialog;
+}
---
+++ trunk/PlusApp/CommonWidgets/Testing/SegmentationParameterDialogTest.h
@@ -0,0 +1,75 @@
+/*=Plus=header=begin======================================================
+  Program: Plus
+  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+  See License.txt for details.
+=========================================================Plus=header=end*/ 
+
+#ifndef __fCalMainWindow_h
+#define __fCalMainWindow_h
+
+#include "PlusConfigure.h"
+
+#include <QDialog>
+
+class vtkDataCollector;
+class DeviceSetSelectorWidget;
+
+//-----------------------------------------------------------------------------
+
+/*! \class SegmentationParameterDialogTest 
+ *
+ * \brief Qt application for testing SegmentationParameterDialog and for using the dialog outside fCal
+ *
+ * \ingroup PlusAppCommonWidgets
+ *
+ */
+class SegmentationParameterDialogTest : public QDialog
+{
+	Q_OBJECT
+
+public:
+	/*!
+	* \brief Constructor
+	* \param aParent parent
+	* \param aFlags widget flag
+	*/
+	SegmentationParameterDialogTest(QWidget *parent = 0, Qt::WFlags flags = 0);
+
+  //-----------------------------------------------------------------------------
+	/*!
+	* \brief Destructor
+	*/
+	~SegmentationParameterDialogTest();
+
+protected slots:
+	/*!
+	* \brief Connect to devices described in the argument configuration file in response by clicking on the Connect button
+	* \param aConfigFile DeviceSet configuration file path and name
+	*/
+	void ConnectToDevicesByConfigFile(std::string aConfigFile);
+
+	/*!
+	* \brief Shows configuration file saver dialog that enables saving the new segmentation parameters
+	*/
+  void SaveConfigurationClicked();
+
+protected:
+	/*!
+	 * \brief Read configuration file and start data collection
+	 * \return Success flag
+	 */
+  PlusStatus StartDataCollection();
+
+protected:
+  //! Device set selector widget
+	DeviceSetSelectorWidget*	m_DeviceSetSelectorWidget;
+
+	//! Data collector object
+	vtkDataCollector*	        m_DataCollector;
+
+  //! Save configuration button
+  QPushButton*              m_SaveButton;
+
+};
+
+#endif // __fCalMainWindow_h
