da4924fee2413337b541a738390a46e7289a262b
Tamas Heffter <heffter@users.noreply.github.com>
2011-11-01 19:33:02 +0000

    Updated vtkDataCollectorTrackingTest to display the selected tool transform Added GetTransformParametersString(vtkMatrix4x4* matrix)

--- PlusLib/src/DataCollection/Testing/vtkDataCollectorTrackingTest.cxx
+++ PlusLib/src/DataCollection/Testing/vtkDataCollectorTrackingTest.cxx
@@ -17,10 +17,12 @@ See License.txt for details.
 #include "vtkTimerLog.h"
 #include "vtksys/SystemTools.hxx"
 #include "vtkTracker.h"
+#include "vtkTrackerTool.h"
 
 int main(int argc, char **argv)
 {
 	std::string inputConfigFileName;
+  std::string inputToolName; 
 	double inputAcqTimeLength(60);
 	std::string outputTrackerBufferSequenceFileName("TrackerBufferMetafile"); 
 	std::string outputFolder("./");
@@ -31,7 +33,8 @@ int main(int argc, char **argv)
 	args.Initialize(argc, argv);
 
 	args.AddArgument("--input-config-file-name", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputConfigFileName, "Name of the input configuration file.");
-	args.AddArgument("--input-acq-time-length", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputAcqTimeLength, "Length of acquisition time in seconds (Default: 60s)");	
+	args.AddArgument("--input-tool-name", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputToolName, "Will print the actual transform of this tool (names were defined in the config file, default is the first active tool)");	
+  args.AddArgument("--input-acq-time-length", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputAcqTimeLength, "Length of acquisition time in seconds (Default: 60s)");	
 	args.AddArgument("--output-tracker-buffer-seq-file-name", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &outputTrackerBufferSequenceFileName, "Filename of the output tracker bufffer sequence metafile (Default: TrackerBufferMetafile)");
 	args.AddArgument("--output-folder", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &outputFolder, "Output folder (Default: ./)");
 	args.AddArgument("--verbose", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &verboseLevel, "Verbose level (1=error only, 2=warning, 3=info, 4=debug, 5=trace)");	
@@ -68,10 +71,61 @@ int main(int argc, char **argv)
 
 	const double acqStartTime = vtkTimerLog::GetUniversalTime(); 
 
+  if ( !dataCollector->GetTrackingEnabled() )
+  {
+    LOG_ERROR("Tracking is not enabled!"); 
+    return EXIT_FAILURE; 
+  }
+
+  int portNumber = dataCollector->GetTracker()->GetToolPortByName(inputToolName.c_str());
+  if ( portNumber < 0 )
+  {
+    if ( dataCollector->GetTracker()->GetFirstActiveTool(portNumber) != PLUS_SUCCESS )
+    {
+      LOG_ERROR("There is no active tool!"); 
+      return EXIT_FAILURE; 
+    }
+  }
+
+  vtkTrackerTool* tool = dataCollector->GetTracker()->GetTool(portNumber); 
+
+  if ( tool == NULL )
+  {
+    LOG_ERROR("Tool is not exist anymore!"); 
+    return EXIT_FAILURE;
+  }
+
+  TrackerBufferItem bufferItem; 
+  vtkSmartPointer<vtkMatrix4x4> matrix = vtkSmartPointer<vtkMatrix4x4>::New(); 
+
 	while ( acqStartTime + inputAcqTimeLength > vtkTimerLog::GetUniversalTime() )
 	{
-		LOG_INFO( acqStartTime + inputAcqTimeLength - vtkTimerLog::GetUniversalTime() << " seconds left..." ); 
-		vtksys::SystemTools::Delay(1000); 
+    
+    tool->GetBuffer()->GetLatestTrackerBufferItem(&bufferItem); 
+    if ( bufferItem.GetMatrix(matrix) != PLUS_SUCCESS )
+    {
+      LOG_ERROR("Failed to get matrix from buffer item!"); 
+      continue; 
+    }
+
+    std::string transformParameters = PlusMath::GetTransformParametersString(matrix); 
+    std::string status = vtkTracker::ConvertTrackerStatusToString(bufferItem.GetStatus()); 
+
+    std::ostringstream message;
+    message << "Tool name: " << tool->GetToolName() << "Transform:  "; 
+    for ( int r = 0; r < 4; r++ )
+    {
+      for ( int c = 0; c < 4; c++ ) 
+      {
+        message << "  " << std::fixed << std::setprecision(5) << std::setw(8) << std::setfill(' ') << matrix->GetElement(r,c); 
+      }
+      message << "    "; 
+    }
+    message << "  Status: " << status; 
+
+
+    LOG_INFO( message.str() ); 
+		vtksys::SystemTools::Delay(200); 
 	}
 
 
