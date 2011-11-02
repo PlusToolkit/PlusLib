797f5b1a97482464d6c16be0ceaa4a13cecd64b4
Csaba Pinter <cpinter@users.noreply.github.com>
2011-11-02 16:28:02 +0000

    Re #287: Standard comments in vtkAccurateTimer and vtkPlusConfig. Parameter description added to vtkDataCollector

--- PlusLib/src/PlusCommon/vtkPlusConfig.h
+++ PlusLib/src/PlusCommon/vtkPlusConfig.h
@@ -4,10 +4,6 @@
   See License.txt for details.
 =========================================================Plus=header=end*/
 
-// .NAME vtkPlusConfig - class for finding files in a directory and its subdirectorys
-// .SECTION Description
-// This class is used for finding files in a directory and its subdirectorys, handling configuration (directory, file name, XML tree)
-
 #ifndef __vtkPlusConfig_h
 #define __vtkPlusConfig_h
 
@@ -19,20 +15,22 @@
 //-----------------------------------------------------------------------------
 
 /*!
-* \brief Singleton class providing tools needed for handling the configuration - finding files, assembling file names, holding and saving the configuration data
-*/
+  \class vtkPlusConfig 
+  \brief Singleton class providing tools needed for handling the configuration - finding files, assembling file names, holding and saving the configuration data
+  \ingroup PlusLibCommon
+*/ 
 class VTK_EXPORT vtkPlusConfig : public vtkObject
 {
 public:
 	vtkTypeRevisionMacro(vtkPlusConfig, vtkObject);
 
 	/*!
-	* \brief New
+	* New
 	*/
 	static vtkPlusConfig *New();
 
 	/*!
-	* \brief Instance getter for the singleton class
+	* Instance getter for the singleton class
 	* \return Instance object
 	*/
 	static vtkPlusConfig* GetInstance();
@@ -41,19 +39,19 @@ public:
   static void SetInstance(vtkPlusConfig* instance);
 
 	/*!
-	* \brief Saves application configuration data to file with the name that is stored in ApplicationConfigurationFileName
+	* Saves application configuration data to file with the name that is stored in ApplicationConfigurationFileName
 	* \return Success flag
 	*/
   PlusStatus SaveApplicationConfigurationToFile();
 
 	/*!
-	 * \brief Assembles a filename for the new device set configuration that is the same as the input file name, only with the current date and time in the end (for saving to a new file)
+	 * Assembles a filename for the new device set configuration that is the same as the input file name, only with the current date and time in the end (for saving to a new file)
 	 * \return New device set configuration file name
 	 */
   std::string GetNewDeviceSetConfigurationFileName();
 
 	/*!
-	 * \brief Replaces an element with a name on the level under the top level in device set configuration with the element in the parameter root XML data element
+	 * Replaces an element with a name on the level under the top level in device set configuration with the element in the parameter root XML data element
    * \param Element name to replace
    * \param Root element containing the element to replace with
 	 * \return Success flag
@@ -61,7 +59,7 @@ public:
   static PlusStatus ReplaceElementInDeviceSetConfiguration(const char* aElementName, vtkXMLDataElement* aNewRootElement);
 
   /*!
-	 * \brief Searches a data element in an XML tree: the child of aElementName that has the name aChildName and has an attribute aChildAttributeName with the value aChildAttributeValue
+	 * Searches a data element in an XML tree: the child of aElementName that has the name aChildName and has an attribute aChildAttributeName with the value aChildAttributeValue
    * \param aConfig Root XML element in that the search is conducted
    * \param aElementName Name of the parent of the searched element
    * \param aChildName Name of the searched element
@@ -72,14 +70,14 @@ public:
 	static vtkXMLDataElement* LookupElementWithNameContainingChildWithNameAndAttribute(vtkXMLDataElement* aConfig, const char* aElementName, const char* aChildName, const char* aChildAttributeName, const char* aChildAttributeValue);
 
   /*!
-	* \brief Search recursively for a file in the configuration directory
+	* Search recursively for a file in the configuration directory
 	* \param aFileName Name of the file to be searched
 	* \return The first file that is found (with full path)
 	*/
 	static std::string GetFirstFileFoundInConfigurationDirectory(const char* aFileName);
 
 	/*!
-	* \brief Search recursively for a file in the parent of a specified directory
+	* Search recursively for a file in the parent of a specified directory
 	* \param aFileName Name of the file to be searched
 	* \param aDirectory Directory in whose parent the search is performed
 	* \return The first file that is found (with full path)
@@ -87,7 +85,7 @@ public:
 	static std::string GetFirstFileFoundInParentOfDirectory(const char* aFileName, const char* aDirectory);
 
 	/*!
-	* \brief Search recursively for a file in a specified directory
+	* Search recursively for a file in a specified directory
 	* \param aFileName Name of the file to be searched
 	* \param aDirectory Directory in which search is performed
 	* \return The first file that is found (with full path)
@@ -95,68 +93,84 @@ public:
 	static std::string GetFirstFileFoundInDirectory(const char* aFileName, const char* aDirectory);
 
 	/*!
-	* \brief Set program path and read application configuration
+	* Set program path and read application configuration
 	* \return Success flag
 	*/
   PlusStatus SetProgramPath(const char* aProgramDirectory);
 
   /*!
-	* \brief Set output directory and change vtkPlusLog file name
+	* Set output directory and change vtkPlusLog file name
 	*/
   void SetOutputDirectory(const char* outputDir);
 
 	/*!
-	* \brief Get absolute image path from input image path
+	* Get absolute image path from input image path
 	* \param aImagePath Image path read from configuration file
 	* \return Absolute image path
 	*/
   static std::string GetAbsoluteImagePath(const char* aImagePath);
 
 public:
-	//! Get/Set macros
+	/*! Get device set configuration directory path */
 	vtkGetStringMacro(DeviceSetConfigurationDirectory);
+	/*! Set device set configuration directory path */
 	vtkSetStringMacro(DeviceSetConfigurationDirectory);
 
+	/*! Get device set configuration file name */
 	vtkGetStringMacro(DeviceSetConfigurationFileName);
+	/*! Set device set configuration file name */
 	vtkSetStringMacro(DeviceSetConfigurationFileName);
 
+	/*! Get device set configuration data */
   vtkGetObjectMacro(DeviceSetConfigurationData, vtkXMLDataElement); 
+	/*! Set device set configuration data */
   vtkSetObjectMacro(DeviceSetConfigurationData, vtkXMLDataElement); 
 
+	/*! Get application configuration file name */
 	vtkGetStringMacro(ApplicationConfigurationFileName);
+	/*! Set application configuration file name */
 	vtkSetStringMacro(ApplicationConfigurationFileName);
 
+	/*! Get editor application executable path and file name */
 	vtkGetStringMacro(EditorApplicationExecutable);
+	/*! Set editor application executable path and file name */
 	vtkSetStringMacro(EditorApplicationExecutable);
 
+	/*! Get output directory path */
 	vtkGetStringMacro(OutputDirectory);
 	
+	/*! Get program directory path */
 	vtkGetStringMacro(ProgramDirectory);
 
+	/*! Get image directory path */
 	vtkGetStringMacro(ImageDirectory);
+	/*! Set image directory path */
 	vtkSetStringMacro(ImageDirectory);
 
 protected:
+	/*! Set program directory path */
 	vtkSetStringMacro(ProgramDirectory);
 
+	/*! Get application configuration data */
   vtkGetObjectMacro(ApplicationConfigurationData, vtkXMLDataElement); 
+	/*! Set application configuration data */
   vtkSetObjectMacro(ApplicationConfigurationData, vtkXMLDataElement); 
 
 protected:
   /*!
-	* \brief Save application configuration file
+	* Save application configuration file
   * \return Success flag
 	*/
   PlusStatus WriteApplicationConfiguration();
 
 	/*!
-	* \brief Read application configuration from file
+	* Read application configuration from file
   * \return Success flag
 	*/
   PlusStatus ReadApplicationConfiguration();
 
   /*!
-	* \brief Search recursively for a file in a specified directory (core, protected function)
+	* Search recursively for a file in a specified directory (core, protected function)
 	* \param aFileName Name of the file to be searched
 	* \param aDirectory Directory in which search is performed
 	* \return The first file that is found with full path
@@ -165,45 +179,45 @@ protected:
 
 protected:
 	/*!
-	* \brief Constructor
+	* Constructor
 	*/
 	vtkPlusConfig();
 
 	/*!
-	* \brief Destructor
+	* Destructor
 	*/
 	virtual	~vtkPlusConfig();
 
 protected:
-	//! Device set configuration directory path
+	/*! Device set configuration directory path */
 	char*               DeviceSetConfigurationDirectory;
 
-  //! Used device set configuration file name (for assembling the new file name with the date)
+  /*! Used device set configuration file name (for assembling the new file name with the date) */
 	char*               DeviceSetConfigurationFileName;
 
-  //! Session device set configuration data
+  /*! Session device set configuration data */
   vtkXMLDataElement*  DeviceSetConfigurationData;
 
-  //! Application configuration file name (./Config.xml by default)
+  /*! Application configuration file name (./Config.xml by default) */
 	char*               ApplicationConfigurationFileName;
 
-  //! Application configuration data
+  /*! Application configuration data */
   vtkXMLDataElement*  ApplicationConfigurationData;
 
-  //! Path and filename of the editor application executable to be used
+  /*! Path and filename of the editor application executable to be used */
   char*               EditorApplicationExecutable;
 
-  //! Output directory path
+  /*! Output directory path */
   char*               OutputDirectory;
 
-  //! Program path
+  /*! Program path */
   char*               ProgramDirectory;
 
-  //! Image directory path. It is used as home directory for images when the image path for a saved dataset is not an absolute path
+  /*! Image directory path. It is used as home directory for images when the image path for a saved dataset is not an absolute path */
   char*               ImageDirectory;
 
 private:
-	//! Instance of the singleton
+	/*! Instance of the singleton */
 	static vtkPlusConfig*	Instance;
 }; 
 
