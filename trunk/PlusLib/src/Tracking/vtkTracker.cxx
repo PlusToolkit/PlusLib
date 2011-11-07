3ef59299ec2bb286d4295068c5f70e74545e59c0
heffter <heffter@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2011-11-07 14:39:02 +0000

    Move generated files to the output directory

--- trunk/PlusLib/src/Tracking/vtkTracker.cxx
+++ trunk/PlusLib/src/Tracking/vtkTracker.cxx
@@ -751,7 +751,7 @@ PlusStatus vtkTracker::GenerateTrackingDataAcquisitionReport( vtkHTMLGenerator* 
     return PLUS_FAIL; 
   }
 
-  std::string reportFile = vtksys::SystemTools::GetCurrentWorkingDirectory() + std::string("/TrackerBufferTimestamps.txt"); 
+  std::string reportFile = vtkPlusConfig::GetInstance()->GetOutputDirectory() + std::string("/TrackerBufferTimestamps.txt"); 
 
   if ( vtkGnuplotExecuter::DumpTableToFileInGnuplotFormat( timestampReportTable, reportFile.c_str() ) != PLUS_SUCCESS )
   {
