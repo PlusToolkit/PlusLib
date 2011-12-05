b260558b611934caccbe1503a0272b9f73dcbcbc
Csaba Pinter <cpinter@users.noreply.github.com>
2011-12-05 20:44:25 +0000

    Re #287: BrachyStepper project doxygen-ized

--- PlusLib/src/Tracking/BrachyTracking/Utils/CivcoBrachyStepper.h
+++ PlusLib/src/Tracking/BrachyTracking/Utils/CivcoBrachyStepper.h
@@ -6,63 +6,77 @@
 
 #ifndef _CIVCOBRACHYSTEPPER_H_
 #define _CIVCOBRACHYSTEPPER_H_
-/**************************************************************
-*
-*       Communication protocol for ED2 encoder controller from US digital
-*       Communication runs at 19200 baud, 8 data bits, and
-*       no parity using the COM port of the PC. (settable parms)
-*
-**************************************************************/
 
 #include "PlusConfigure.h"
 #include "BrachyStepper.h"
 
+/*! \class CivcoBrachyStepper 
+ * \brief Interface for position tracking using Civco brachy steppers
+ *
+ * Communication protocol for ED2 encoder controller from US digital
+ * Communication runs at 19200 baud, 8 data bits, and
+ * no parity using the COM port of the PC. (settable parms)
+ *
+ * \ingroup PlusLibBrachyStepper
+ */
 class  CivcoBrachyStepper : public BrachyStepper
 {
 public:
 
+  /*! Constructor */
   CivcoBrachyStepper(unsigned long COMPort=1, unsigned long BaudRate=9600);
+  /*! Destructor */
   virtual ~CivcoBrachyStepper();
 
-  // Description: 
-  // Connect to CIVCO stepper    
+  /*! Connect to CIVCO stepper */
   virtual PlusStatus Connect(); 
 
-  // Description:
-  // Disconnect from CIVCO stepper 
+  /*! Disconnect from CIVCO stepper */
   virtual PlusStatus Disconnect(); 
 
-  // Description:
-  // Get raw encoder values from stepper 
+  /*! Get raw encoder values from stepper */
   virtual PlusStatus GetEncoderValues(double &PPosition, 
     double &GPosition, double &RPosition, unsigned long &PositionRequestNumber);
 
+  /*! Get device mode information */
   virtual PlusStatus GetDeviceModelInfo( std::string& version, std::string& model, std::string& serial ); 
 
+  /*! Reset stepper */
   virtual PlusStatus ResetStepper();
 
+  /*! Initialize stepper */
   virtual PlusStatus InitializeStepper(std::string &CalibMsg);
 
+  /*! Set baud rate of communication */
   virtual PlusStatus SetBaudRate(unsigned long BaudRate); 
 
+  /*! Set used COM port for communication */
   virtual PlusStatus SetCOMPort(unsigned long COMPort); 
 
+  /*! Return whether stepper is alive */
   virtual PlusStatus IsStepperAlive();
 
 protected: 
 
-  // Description: 
-  // IEEE Standard 754 floating point to double conversion 
+  /*! IEEE Standard 754 floating point to double conversion  */
   double ConvertFloatToDecimal( long floatPoint ); 
 
+  /*! Returns number of devices found on the SEI bus */
   long GetNumberOfSeiDevices(); 
 
+  /*! Get information about the SEI device */
   PlusStatus GetSeiDeviceInfo(long devnum, long &model, long &serialnum, long &version, long &addr); 
 
+  /*! Used COM port number */
   long m_COMPort; 
+
+  /*! Baud rate */
   long m_BaudRate; 
 
+  /*! Device number */
   long m_DeviceNumber; 
+
+  /*! Device address */
   long m_DeviceAddress;
 
 };
