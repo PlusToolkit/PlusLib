182ba2a12f218b00ed9b34226cd12cb27410783c
EricMoult <EricMoult@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-02-10 15:57:59 +0000

    re #346: Added (previously missing) testing file for the temporal calibration algo (WIP).

---
+++ PlusLib/src/DataCollection/Testing/TemporalCalibrationTest.cxx
@@ -0,0 +1,26 @@
+#include "TemporalCalibration.h"
+
+int main()
+{
+  // Test default constructor
+  double defaultSamplingResolution = 0.001;
+  std::string defaultTrackerFilename = "S:\\data\\TemporalTrackedUSCalibration\\CARS_DATA\\BaselineStudy\\Trial_01\\BufferDump_Tracker_20111229_122334.mha";
+  std::string defaultVideoFilename = "S:\\data\\TemporalTrackedUSCalibration\\CARS_DATA\\BaselineStudy\\Trial_01\\BufferDump_Video_20111229_122334.mha";
+  std::string defaultOutputFilename = "C:\\Documents and Settings\\moult\\My Documents";
+  TemporalCalibration *testTemporalCalibrationObject = new TemporalCalibration(defaultTrackerFilename,
+                                                                               defaultVideoFilename, 
+                                                                               defaultOutputFilename, 
+                                                                               defaultSamplingResolution);
+
+  
+  testTemporalCalibrationObject->CalculateTimeOffset();
+  std::cout << testTemporalCalibrationObject->getTimeOffset() << std::endl;
+
+  int i = 0;
+  //-----------------------------------------------------------------------//
+
+ 
+
+
+  return 0;
+}
