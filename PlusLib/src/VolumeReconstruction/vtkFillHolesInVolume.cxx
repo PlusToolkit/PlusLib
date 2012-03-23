61e6ed994952ffe245f97dff5451212e1e9e7d37
Thomas Vaughan <tavaughan@users.noreply.github.com>
2012-03-23 22:22:55 +0000

    re #436 Implemented variable kernel size. Kernels now have to be specified whenever hole filling is turned on.

--- PlusLib/src/VolumeReconstruction/vtkFillHolesInVolume.cxx
+++ PlusLib/src/VolumeReconstruction/vtkFillHolesInVolume.cxx
@@ -82,11 +82,14 @@ vtkFillHolesInVolume::vtkFillHolesInVolume()
   this->SetNumberOfInputPorts(2);
   this->SetNumberOfOutputPorts(1);
   this->Compounding=0;
+  Kernels = NULL;
 }
 
 //----------------------------------------------------------------------------
 vtkFillHolesInVolume::~vtkFillHolesInVolume()
 {
+	if (Kernels != NULL)
+		delete[] Kernels;
 }
 
 //----------------------------------------------------------------------------
@@ -137,7 +140,7 @@ int vtkFillHolesInVolume::RequestUpdateExtent (vtkInformation* vtkNotUsed(reques
 }
 
 //----------------------------------------------------------------------------
-template <class T>
+/*template <class T>
 int vtkFillHolesInVolume::weightedAverageOverNeighborhood(T* inputData,// contains the dataset being interpolated between
 											  unsigned short* accData, // contains the weights of each voxel
 											  int* inputOffsets,       // contains the indexing offsets between adjacent x,y,z
@@ -194,40 +197,38 @@ int vtkFillHolesInVolume::weightedAverageOverNeighborhood(T* inputData,// contai
 
 	return 1;
 
-}
+}*/
 
 //----------------------------------------------------------------------------
 template <class T>
-int vtkFillHolesInVolume::weightedAverageOverNeighborhoodWithGaussian(
+double vtkFillHolesInVolume::weightedAverageOverNeighborhoodWithGaussian(
 											  T* inputData,            // contains the dataset being interpolated between
 											  unsigned short* accData, // contains the weights of each voxel
 											  int* inputOffsets,       // contains the indexing offsets between adjacent x,y,z
 											  int* accOffsets,
 											  const int& inputComp,	   // the component index of interest
 											  int* bounds,             // the boundaries of the volume, outputExtent
-											  const int& neighborSize, // The size of the neighborhood, odd positive integer
+											  const int& kernelIndex, // The size of the neighborhood, odd positive integer
 											  unsigned int* kernel,    // the gaussian kernel matrix
 											  int* thisPixel,		   // The x,y,z coordinates of the voxel being calculated
 											  T& returnVal)            // The value of the pixel being calculated (unknown)
 {
 
-	if (neighborSize%2 != 1 || neighborSize <= 1)
-	{
-		LOG_ERROR("vtkFillHolesInVolume::weightedAverageOverNeighborhood: 'neighborSize' must be a positive odd integer greater or equal to 3");
-		return 0;
-	}
-
 	// set the x, y, and z range
-	int range = (neighborSize-1)/2; // so with N = 3, our range is x-1 through x+1, and so on
-	int minX = thisPixel[0] - range;
-	int minY = thisPixel[1] - range;
-	int minZ = thisPixel[2] - range;
-	int maxX = thisPixel[0] + range;
-	int maxY = thisPixel[1] + range;
-	int maxZ = thisPixel[2] + range;
+	int rangeX = (Kernels[kernelIndex].size[0]-1)/2; // so with N = 3, our range is x-1 through x+1, and so on
+	int rangeY = (Kernels[kernelIndex].size[1]-1)/2;
+	int rangeZ = (Kernels[kernelIndex].size[2]-1)/2;
+	int minX = thisPixel[0] - rangeX;
+	int minY = thisPixel[1] - rangeY;
+	int minZ = thisPixel[2] - rangeZ;
+	int maxX = thisPixel[0] + rangeX;
+	int maxY = thisPixel[1] + rangeY;
+	int maxZ = thisPixel[2] + rangeZ;
 
 	unsigned long long sumIntensities(0); // unsigned long because these rise in value quickly
 	unsigned long long sumAccumulator(0);
+	unsigned long long currentAccumulation(0);
+	int numKnownVoxels(0);
 
 	for (int x = minX; x <= maxX; x++)
 	{
@@ -239,12 +240,16 @@ int vtkFillHolesInVolume::weightedAverageOverNeighborhoodWithGaussian(
 					y <= bounds[3] && y >= bounds[2] &&
 					z <= bounds[5] && z >= bounds[4] ) // check bounds
 				{
-					int volIndex = inputOffsets[0]*x+inputOffsets[1]*y+inputOffsets[2]*z+inputComp;
 					int accIndex =   accOffsets[0]*x+  accOffsets[1]*y+  accOffsets[2]*z;
-					int kerIndex = neighborSize*neighborSize*(z-minZ)+neighborSize*(y-minY)+(x-minX);
-					unsigned long long weight = (unsigned long long)accData[accIndex] * (unsigned long long)kernel[kerIndex];
-					sumIntensities += (unsigned long long)inputData[volIndex] * weight;
-					sumAccumulator += weight;
+					currentAccumulation = (unsigned long long)accData[accIndex];
+					if (currentAccumulation) {
+						int volIndex = inputOffsets[0]*x+inputOffsets[1]*y+inputOffsets[2]*z+inputComp;
+						int kerIndex = Kernels[kernelIndex].size[1]*Kernels[kernelIndex].size[0]*(z-minZ)+Kernels[kernelIndex].size[0]*(y-minY)+(x-minX);
+						unsigned long long weight = currentAccumulation * (unsigned long long)kernel[kerIndex];
+						sumIntensities += (unsigned long long)inputData[volIndex] * weight;
+						sumAccumulator += weight;
+						numKnownVoxels++;
+					}
 				} // end boundary check
 			} // end z loop
 		} // end y loop
@@ -256,28 +261,50 @@ int vtkFillHolesInVolume::weightedAverageOverNeighborhoodWithGaussian(
 	// TODO: Overflow protection
 	returnVal = (T)(sumIntensities/sumAccumulator);
 
-	return 1;
+	return ((double)numKnownVoxels)/(Kernels[kernelIndex].size[0]*Kernels[kernelIndex].size[1]*Kernels[kernelIndex].size[2]);
 
 }
 //----------------------------------------------------------------------------
 
-void vtkFillHolesInVolume::calculateGaussianMatrix(const int& size, unsigned int* matrix)
+unsigned int* vtkFillHolesInVolume::calculateGaussianMatrix(const int& kernelIndex) // stdev as input based on hole size
+																		      // kernel size is a compromise between image quality and computation time
 {
-	// voxels outside 3 * rho are considered insignificant, so if we want to have a filter that adds up to 1,
-	double rho = (size-1)/6.;
-	double range = (size-1)/2.;
+
+    unsigned int* matrix = new unsigned int[(Kernels[kernelIndex].size[0]*Kernels[kernelIndex].size[1]*Kernels[kernelIndex].size[2])];
+	// stdev for each of x, y, z
+	double stdev[3];
+	stdev[0] = Kernels[kernelIndex].stdev[0]; //x
+	stdev[1] = Kernels[kernelIndex].stdev[1]; //y
+	stdev[2] = Kernels[kernelIndex].stdev[2]; //z
+
+	double rangeX = (Kernels[kernelIndex].size[0]-1)/2.;
+	double rangeY = (Kernels[kernelIndex].size[1]-1)/2.;
+	double rangeZ = (Kernels[kernelIndex].size[2]-1)/2.;
+	const double Pi = 3.1415926535897932384626433832795;
+
+	// divisors in the exponent
+	double divisor[3];
+	divisor[0] = Kernels[kernelIndex].stdev[0] * Kernels[kernelIndex].stdev[0] * 2.;
+	divisor[1] = Kernels[kernelIndex].stdev[1] * Kernels[kernelIndex].stdev[1] * 2.;
+	divisor[2] = Kernels[kernelIndex].stdev[2] * Kernels[kernelIndex].stdev[2] * 2.;
+
+	// divisor in the non-exponent
+	double termDivisor = pow(2*Pi,3./2) * stdev[0] * stdev[1] * stdev[2];
+
 	double min(0);
-	double Pi = 3.1415926535897932384626433832795;
-	double commonDivisor = rho * rho * 2.;
 	int index(0);
-	for (int z = 0; z < size; z++)
+	for (int z = 0; z < Kernels[kernelIndex].size[2]; z++)
 	{
-		for (int y = 0; y < size; y++)
+		for (int y = 0; y < Kernels[kernelIndex].size[1]; y++)
 		{
-			for (int x = 0; x < size; x++)
+			for (int x = 0; x < Kernels[kernelIndex].size[0]; x++)
 			{
-				double dimsSq = pow((double)x-range,2)+pow((double)y-range,2)+pow((double)z-range,2);
-				double calcVal = 1./(pow((double)(Pi*commonDivisor),1.5))*exp((double)(-dimsSq/commonDivisor));
+				double xExp = pow((double)x-rangeX,2)/divisor[0];
+				double yExp = pow((double)y-rangeY,2)/divisor[1];
+				double zExp = pow((double)z-rangeZ,2)/divisor[2];
+				double eExp = -(xExp + yExp + zExp);
+
+				double calcVal = 1./(termDivisor)*exp(eExp);
 				if (!min)
 					min = calcVal;
 				matrix[index] = PlusMath::Round(calcVal/min);
@@ -285,15 +312,20 @@ void vtkFillHolesInVolume::calculateGaussianMatrix(const int& size, unsigned int
 			}
 		}
 	}
+	return matrix;
 
 }
 
 //----------------------------------------------------------------------------
 template <class T>
-void vtkFillHolesInVolumeExecute(vtkFillHolesInVolume *self,
-  vtkImageData *inVolData, T *inVolPtr, vtkImageData *accData,
-  unsigned short *accPtr, vtkImageData *outData, T *outPtr,
-  int outExt[6], int id)
+void vtkFillHolesInVolume::vtkFillHolesInVolumeExecute(vtkImageData *inVolData,
+													   T *inVolPtr, 
+													   vtkImageData *accData,
+													   unsigned short *accPtr, 
+													   vtkImageData *outData, 
+													   T *outPtr,
+													   int outExt[6], 
+													   int id)
 {
 
   if (outData==NULL || outData->GetScalarPointer()==NULL)
@@ -311,9 +343,11 @@ void vtkFillHolesInVolumeExecute(vtkFillHolesInVolume *self,
     LOG_ERROR("vtkPasteSliceIntoVolumeFillHolesInOutput accPtr is invalid");
     return;
   }
-	int FILTER_SIZE = 3;
-	unsigned int* kernel = new unsigned int[FILTER_SIZE*FILTER_SIZE*FILTER_SIZE];
-	vtkFillHolesInVolume::calculateGaussianMatrix(FILTER_SIZE, kernel);
+
+	unsigned int** kernelFilter = new unsigned int*[NumKernels];
+	for (int i = 0; i < NumKernels; i++) {
+      kernelFilter[i] = calculateGaussianMatrix(i);
+	}
 
 	// get increments for volume and for accumulation buffer
 	int byteIncVol[3]; //x,y,z
@@ -343,16 +377,26 @@ void vtkFillHolesInVolumeExecute(vtkFillHolesInVolume *self,
 					{
 						// volume index for this component
 						int volCompIndex = (currentPos[0]*byteIncVol[0])+(currentPos[1]*byteIncVol[1])+(currentPos[2]*byteIncVol[2])+c;
-						int result = 0;
-						result = vtkFillHolesInVolume::weightedAverageOverNeighborhoodWithGaussian(inVolPtr,accPtr,byteIncVol,byteIncAcc,c,outExt,FILTER_SIZE,kernel,currentPos,outPtr[volCompIndex]);
-						if (!result) // if there are no voxels within range, just set this one (and its alpha) to zero
+						double result(0);
+
+
+						for (int k = 0; k < NumKernels; k++) // k is the index of the kernel being tried
+						{
+							result = weightedAverageOverNeighborhoodWithGaussian(inVolPtr,accPtr,byteIncVol,byteIncAcc,c,outExt,k,kernelFilter[k],currentPos,outPtr[volCompIndex]);
+							if (result >= Kernels[k].minRatio) {
+								break;
+							} // end checking interpolation success
+						}
+
+						if (result < Kernels[NumKernels-1].minRatio) // if there are not enough voxels within range, just set this one (and its alpha) to zero
 						{
 							outPtr[volCompIndex] = 0;
-						} // end checking interpolation success
+						}
+
 						if (!alphaSet)
 						{
 							alphaSet = true; // only do this check once
-							if (!result) // if there are no voxels within range, alpha should be 0
+							if (result < Kernels[NumKernels-1].minRatio) // if there are no voxels within range, alpha should be 0
 							{
 								outPtr[volAlphaIndex] = 0;
 							}
@@ -361,6 +405,7 @@ void vtkFillHolesInVolumeExecute(vtkFillHolesInVolume *self,
 								outPtr[volAlphaIndex] = (T)OPAQUE_ALPHA;
 							} // end checking interpolation success
 						}
+
 					} // end component loop
 				}
 				else // if hit, just use the apparent value
@@ -376,7 +421,10 @@ void vtkFillHolesInVolumeExecute(vtkFillHolesInVolume *self,
 		} // end y loop
 	} // end z loop
 
-	delete[] kernel;
+	for (int i = 0; i < NumKernels; i++) {
+      delete[] kernelFilter[i];
+	}
+	delete[] kernelFilter;
 
 }
 
@@ -410,7 +458,7 @@ void vtkFillHolesInVolume::ThreadedRequestData(vtkInformation *request,
   switch (inVolData->GetScalarType())
     {
       vtkTemplateMacro(
-      vtkFillHolesInVolumeExecute(this, 
+      vtkFillHolesInVolumeExecute(/*this,*/ 
                                        inVolData, static_cast<VTK_TT *>(inVolPtr),
                                        inAccData, static_cast<unsigned short *>(inAccPtr),
                                        outVolData,
@@ -420,6 +468,36 @@ void vtkFillHolesInVolume::ThreadedRequestData(vtkInformation *request,
       LOG_ERROR("Execute: Unknown ScalarType");
       return;
     }
+}
+
+// TODO: Remove if not needed anymore
+/*vtkFillHolesInVolumeKernel vtkFillHolesInVolume::GetKernel(int index) {
+	return Kernels[index];
+}*/
+
+void vtkFillHolesInVolume::SetKernel(int index, vtkFillHolesInVolumeKernel& kernel) {
+	for (int i = 0; i < 3; i++) {
+		Kernels[index].size[i] = kernel.size[i];
+		Kernels[index].stdev[i] = kernel.stdev[i];
+	}
+	Kernels[index].minRatio = kernel.minRatio;
+}
+
+void vtkFillHolesInVolume::AllocateKernels() {
+	if (Kernels != NULL) {
+		delete[] Kernels;
+		Kernels = NULL;
+	}
+	Kernels = new vtkFillHolesInVolumeKernel[NumKernels];
+}
+
+// TODO: Remove if not needed
+/*int vtkFillHolesInVolume::GetNumKernels() {
+	return NumKernels;
+}*/
+
+void vtkFillHolesInVolume::SetNumKernels(int n) {
+	NumKernels = n;
 }
 
 void vtkFillHolesInVolume::SetReconstructedVolume(vtkImageData *reconstructedVolume)
