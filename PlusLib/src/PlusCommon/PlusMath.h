9958361f3a479bbe902f2c236851f7389cbc0378
Andras Lasso <lasso@queensu.ca>
2012-04-21 05:22:23 +0000

    re #478: Fixed PlusMath::Floor (it was much slower and returned different values compared to the 32-bit implementation). Note that PlusMath::Floor(x) still does not return always the same value as Floor(x).

--- PlusLib/src/PlusCommon/PlusMath.h
+++ PlusLib/src/PlusCommon/PlusMath.h
@@ -15,6 +15,7 @@
 #include "vnl/vnl_sparse_matrix.h"   
 #include "vtkMatrix4x4.h"
 #include "vtkTransform.h"
+#include "vtkMath.h"
 
 /*!
   \class PlusMath
@@ -89,52 +90,104 @@ public:
 
   /*! Print VTK matrix into log as info */
   static void LogVtkMatrix(vtkMatrix4x4* matrix, int precision = 3);
+  
+  // Fast floor implementation. Adopted from vtkImageReslice.
 
-  /*! 
-  The 'floor' function on x86 and mips is 4-10 times slower than these
-  and is used a lot in this code, optimize for different CPU architectures.
-  The output type is int, as we don't want to round if the result is a float.
-  */
+  // The 'floor' function is slow, so we want to do an integer
+  // cast but keep the "floor" behavior of always rounding down,
+  // rather than truncating, i.e. we want -0.6 to become -1.
+  // The easiest way to do this is to add a large value in
+  // order to make the value "unsigned", then cast to int, and
+  // then subtract off the large value.
+
+  // On the old i386 architecture, even a cast to int is very
+  // expensive because it requires changing the rounding mode
+  // on the FPU.  So we use a bit-trick similar to the one
+  // described at http://www.stereopsis.com/FPU.html
+
+  // Note that these floor/ceil/round functions do not always return the
+  // same value as floor(), when it is slightly less than an integer.
+  // e.g.,
+  // double x = 16.999995;
+  // PlusMath::Floor(x)) => 17
+  // vtkMath::Floor(x))  => 16
+  // floor(x)            => 16
+
+#if defined ia64 || defined __ia64__ || defined _M_IA64
+#define VTK_RESLICE_64BIT_FLOOR
+#elif defined __ppc64__ || defined __x86_64__ || defined _M_X64
+#define VTK_RESLICE_64BIT_FLOOR
+#elif defined __ppc__ || defined sparc || defined mips
+#define VTK_RESLICE_32BIT_FLOOR
+#elif defined i386 || defined _M_IX86
+#define VTK_RESLICE_I386_FLOOR
+#endif
+
+  // We add a tolerance of 2^-17 (around 7.6e-6) so that float
+  // values that are just less than the closest integer are
+  // rounded up.  This adds robustness against rounding errors.
+
+#define VTK_RESLICE_FLOOR_TOL 7.62939453125e-06
+
+  /*! Performance-optimized Floor computation. May return slightly incorrect value (when x is slightly smaller than an integer, it may return the ceil). Adopted from vtkImageReslice. */
   static inline int Floor(double x)
   {
-#if defined mips || defined sparc || defined __ppc__
-    x += 2147483648.0;
-    unsigned int i = (unsigned int)(x);
-    return (int)(i - 2147483648U);
-#elif defined i386 || defined _M_IX86
-    union { double d; unsigned short s[4]; unsigned int i[2]; } dual;
-    // use 52-bit precision of IEEE double to round (x - 0.25) to 
-    // the nearest multiple of 0.5, according to prevailing rounding
-    // mode which is IEEE round-to-nearest,even
-    dual.d = x + 103079215104.0;  // (2**(52-16))*1.5
-    // extract mantissa, use shift to divide by 2 and hence get rid
-    // of the bit that gets messed up because the FPU uses
-    // round-to-nearest,even mode instead of round-to-nearest,+infinity
-    return (int)((dual.i[1]<<16)|((dual.i[0])>>16));
-#elif defined ia64 || defined __ia64__ || defined IA64
-    x += 103079215104.0;
-    long long i = (long long)(x);
-    return (int)(i - 103079215104LL);
+#if defined VTK_RESLICE_64BIT_FLOOR
+    x += (103079215104.0 + VTK_RESLICE_FLOOR_TOL);
+#ifdef VTK_TYPE_USE___INT64
+    __int64 i = static_cast<__int64>(x);
+    return static_cast<int>(i - 103079215104i64);
 #else
-    double y = floor(x);
-    return (int)(y);
+    long long i = static_cast<long long>(x);
+    return static_cast<int>(i - 103079215104LL);
+#endif
+#elif defined VTK_RESLICE_32BIT_FLOOR
+    x += (2147483648.0 + VTK_RESLICE_FLOOR_TOL);
+    unsigned int i = static_cast<unsigned int>(x);
+    return static_cast<int>(i - 2147483648U);
+#elif defined VTK_RESLICE_I386_FLOOR
+    union { double d; unsigned short s[4]; unsigned int i[2]; } dual;
+    dual.d = x + 103079215104.0;  // (2**(52-16))*1.5
+    return static_cast<int>((dual.i[1]<<16)|((dual.i[0])>>16));
+#else
+    int i = vtkMath::Floor(x + VTK_RESLICE_FLOOR_TOL);
+    return i;
 #endif
   }
 
-  /*! Ceiling operation optimized for speed */
+  /*! Performance-optimized Round computation. May return slightly incorrect value (when x is slightly smaller than ???.5, it may round to the ceil). Adopted from vtkImageReslice. */
+  static inline int Round(double x)
+  {
+#if defined VTK_RESLICE_64BIT_FLOOR
+    x += (103079215104.5 + VTK_RESLICE_FLOOR_TOL);
+#ifdef VTK_TYPE_USE___INT64
+    __int64 i = static_cast<__int64>(x);
+    return static_cast<int>(i - 103079215104i64);
+#else
+    long long i = static_cast<long long>(x);
+    return static_cast<int>(i - 103079215104LL);
+#endif
+#elif defined VTK_RESLICE_32BIT_FLOOR
+    x += (2147483648.5 + VTK_RESLICE_FLOOR_TOL);
+    unsigned int i = static_cast<unsigned int>(x);
+    return static_cast<int>(i - 2147483648U);
+#elif defined VTK_RESLICE_I386_FLOOR
+    union { double d; unsigned int i[2]; } dual;
+    dual.d = x + 103079215104.5;  // (2**(52-16))*1.5
+    return static_cast<int>((dual.i[1]<<16)|((dual.i[0])>>16));
+#else
+    return vtkMath::Floor(x + (0.5 + VTK_RESLICE_FLOOR_TOL));
+#endif
+  } 
+
+  /*! Ceiling operation optimized for speed, may return slightly incorrect value*/
   static inline int Ceil(double x)
   {
     return -Floor(-x - 1.0) - 1;
   }
-
-  /*! Round operation optimized for speed  */
-  static inline int Round(double x)
-  {
-    return Floor(x + 0.5);
-  }
-
+  
   /*! 
-  Convert a float into an integer plus a fraction, optimized for speed 
+  Convert a float into an integer plus a fraction, optimized for speed, may return slightly incorrect value
   \param x Input floating point value
   \param f Output fraction part
   \return Output integer part
@@ -148,7 +201,7 @@ public:
   }
 
   /*! 
-  Round operation that copies the result to a specified memory address, optimized for speed  
+  Round operation that copies the result to a specified memory address, optimized for speed, may return slightly incorrect value
   \param val Input floating point value
   \param rnd Output rounded value
   */
