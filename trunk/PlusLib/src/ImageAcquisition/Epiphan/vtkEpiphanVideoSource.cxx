daaca2443195f909770aaaec78f33b32f258a338
vikasrs <vikasrs@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-05-25 16:06:43 +0000

    re #465 * Removed timing vtkTimerLog function. Time stamp now done internally by PLUS * Added delete this->cropRect in destructor * Added FrmGrab_Start and FrmGrab_Stop in InternalStartRecording and InternalStopRecording * Added error message if frame==NULL and changed return value to PLUS_FAIL * The following specification can read from configuration file:    SerialNumber    VideoFormat - Support for RGB8 and Y8 (default Y8)    CropRectX, CropRectY,CropRectWidth,CropRectHeight (default NULL) * Cleaned the code * Ready to be tested by others

--- trunk/PlusLib/src/ImageAcquisition/Epiphan/vtkEpiphanVideoSource.cxx
+++ trunk/PlusLib/src/ImageAcquisition/Epiphan/vtkEpiphanVideoSource.cxx
@@ -7,68 +7,23 @@
 #include "PlusConfigure.h"
 
 #include "vtkEpiphanVideoSource.h"
-//#include "ICCapturingListener.h"
-//#include <tisudshl.h>
-
 #include "vtkImageData.h"
-#include "vtkCriticalSection.h"
 #include "vtkObjectFactory.h"
-#include "vtkInformation.h"
-#include "vtkInformationVector.h"
-#include "vtkStreamingDemandDrivenPipeline.h"
 #include "vtksys/SystemTools.hxx"
 #include "vtkVideoBuffer.h"
-#include "vtkMultiThreader.h"
-
-#include <ctype.h>
-
-#include <vector>
-#include <string>
-
-#include "vtkTimerLog.h"
-#include "vtkCriticalSection.h"
-#include "vtkUnsignedCharArray.h"
-#include "vtkMutexLock.h"
-
-#include <vtkstd/string> 
 
 vtkCxxRevisionMacro(vtkEpiphanVideoSource, "$Revision: 1.0$");
 vtkStandardNewMacro(vtkEpiphanVideoSource);
-//vtkEpiphanVideoSource* vtkEpiphanVideoSource::Instance = 0;
-//vtkEpiphanVideoSourceCleanup vtkEpiphanVideoSource::Cleanup;
-
-//vtkEpiphanVideoSourceCleanup::vtkEpiphanVideoSourceCleanup(){}
-
-/*
-vtkEpiphanVideoSourceCleanup::~vtkEpiphanVideoSourceCleanup()
-{
-	// Destroy any remaining output window.
-	vtkEpiphanVideoSource::SetInstance(NULL);
-}
-*/
 
 //----------------------------------------------------------------------------
 vtkEpiphanVideoSource::vtkEpiphanVideoSource()
 {
-	this->ICBufferSize = 50; 
-
-	this->DeviceName = NULL; 
-	this->VideoNorm = NULL; 
-	this->VideoFormat = NULL; 
-	this->InputChannel = NULL; 
-
-	this->SetFrameBufferSize(200); 
-  this->Buffer->Modified();
-
-  this->Modified();
-
-	this->FrameGrabber = NULL;
-
-	this->status = V2U_GRABFRAME_STATUS_OK;
-	this->cropRect = new V2URect;
+	this->VideoFormat = V2U_GRABFRAME_FORMAT_Y8; 
+	this->CropRect = new V2URect;
+	this->SerialNumber = NULL;
 	this->SpawnThreadForRecording = true;
-//  this->FrameGrabberListener = NULL; 
-
+	this->SetFrameBufferSize(200); 
+	this->Buffer->Modified();
 }
 
 //----------------------------------------------------------------------------
@@ -82,78 +37,9 @@ vtkEpiphanVideoSource::~vtkEpiphanVideoSource()
 	if ( this->fg != NULL) 
 	{
 		FrmGrab_Deinit();
-		//delete this->FrameGrabber; 
 		this->fg = NULL;
 	}
-}
-
-
-//----------------------------------------------------------------------------
-// Up the reference count so it behaves like New
-/*
-vtkEpiphanVideoSource* vtkEpiphanVideoSource::New()
-{
-	vtkEpiphanVideoSource* ret = vtkEpiphanVideoSource::GetInstance();
-	ret->Register(NULL);
-	return ret;
-}
-*/
-
-//----------------------------------------------------------------------------
-// Return the single instance of the vtkOutputWindow
-/*
-vtkEpiphanVideoSource* vtkEpiphanVideoSource::GetInstance()
-{
-	if(!vtkEpiphanVideoSource::Instance)
-	{
-		// Try the factory first
-		vtkEpiphanVideoSource::Instance = (vtkEpiphanVideoSource*)vtkObjectFactory::CreateInstance("vtkEpiphanVideoSource");    
-		if(!vtkEpiphanVideoSource::Instance)
-		{
-			vtkEpiphanVideoSource::Instance = new vtkEpiphanVideoSource();     
-		}
-		if(!vtkEpiphanVideoSource::Instance)
-		{
-			int error = 0;
-		}
-	}
-	// return the instance
-	return vtkEpiphanVideoSource::Instance;
-}
-*/
-//----------------------------------------------------------------------------
-/*
-void vtkEpiphanVideoSource::SetInstance(vtkEpiphanVideoSource* instance)
-{
-	if (vtkEpiphanVideoSource::Instance==instance)
-	{
-		return;
-	}
-	// preferably this will be NULL
-	if (vtkEpiphanVideoSource::Instance)
-	{
-		vtkEpiphanVideoSource::Instance->Delete();;
-	}
-	vtkEpiphanVideoSource::Instance = instance;
-	if (!instance)
-	{
-		return;
-	}
-	// user will call ->Delete() after setting instance
-	instance->Register(NULL);
-}
-*/
-//----------------------------------------------------------------------------
-/////
-std::string vtkEpiphanVideoSource::GetSdkVersion()
-{
-
-  std::ostringstream version; 
-/**
-  version << "The Imaging Source UDSHL-" << UDSHL_LIB_VERSION_MAJOR << "." << UDSHL_LIB_VERSION_MINOR; 
-  */
-  return version.str(); 
-
+	delete CropRect;
 }
 
 //----------------------------------------------------------------------------
@@ -175,49 +61,57 @@ PlusStatus vtkEpiphanVideoSource::InternalConnect()
   // Initialize frmgrab library
   FrmGrabNet_Init();
 
-  strncpy_s(this->serialNumber,15,"V2U109999",9);
-
-  if ( (this->fg = FrmGrab_Open( this->serialNumber )) == NULL ) 
+  if ( this->SerialNumber != NULL )
   {
-    LOG_ERROR("Epiphan Device with set serial number not found, looking for any available device instead");
-	if ( (this->fg = FrmGrabLocal_Open()) == NULL )
-	{
-	  LOG_ERROR("Epiphan Device Not found");
-	  return PLUS_FAIL;
-	}
+	  if ( (this->fg = FrmGrab_Open( this->SerialNumber )) == NULL ) 
+	  {
+		LOG_WARNING("Epiphan Device with set serial number not found, looking for any available device instead");
+		if ( (this->fg = FrmGrabLocal_Open()) == NULL )
+		{
+		  LOG_ERROR("Epiphan Device Not found");
+		  return PLUS_FAIL;
+		}
+	  }
+  }
+  else
+  {
+	  LOG_WARNING("Serial Number not specified. Looking for any available device");
+	  if ( (this->fg = FrmGrabLocal_Open()) == NULL )
+		{
+		  LOG_ERROR("Epiphan Device Not found");
+		  return PLUS_FAIL;
+		}
   }
   
   V2U_VideoMode vm;
 
-  if (FrmGrab_DetectVideoMode(this->fg,&vm) && vm.width && vm.height) {
-	 //this->SetFrameSize(vm.width,vm.height);
-	 this->SetFrameRate((vm.vfreq+50)/1000);
-  } else {
-	LOG_ERROR("No signal detected");
+  if (FrmGrab_DetectVideoMode(this->fg,&vm) && vm.width && vm.height) 
+  {
+	  //this->SetFrameSize(vm.width,vm.height);
+	  this->SetFrameRate(vm.vfreq/1000);
+  } 
+  else
+  {
+	  LOG_ERROR("No signal detected");
   }
 
   this->GetBuffer()->SetPixelType(itk::ImageIOBase::UCHAR);
 
-  this->cropRect->x = 0;
-  this->cropRect->y = 0;
-  this->cropRect->width = 400;
-  this->cropRect->height = 400;
-
-  this->FrameSize[0] = cropRect->width;
-  this->FrameSize[1] = cropRect->height;
-
-  //FrmGrab_SetMaxFps(this->fg, 25.0);
-
-  // Initialization worked
-  //this->Initialized = 1;
-  
-  // Update frame buffer  to reflect any changes
-  //this->UpdateFrameBuffer();
+  if( (CropRect->height != NULL) && (CropRect->width != NULL) )
+  {
+	  this->FrameSize[0] = CropRect->width;
+	  this->FrameSize[1] = CropRect->height;
+  }
+  else
+  {
+	  this->FrameSize[0] = vm.width;
+	  this->FrameSize[1] = vm.height;
+  }
 
   return PLUS_SUCCESS;
+}
 
-  }  
-
+    
 //----------------------------------------------------------------------------
 PlusStatus vtkEpiphanVideoSource::InternalDisconnect()
 {
@@ -228,7 +122,6 @@ PlusStatus vtkEpiphanVideoSource::InternalDisconnect()
   if (this->fg != NULL) {
 	FrmGrab_Close(this->fg);
   }
-  //delete fg;
   this->fg = NULL;
 
   return this->StopRecording();
@@ -238,28 +131,25 @@ PlusStatus vtkEpiphanVideoSource::InternalDisconnect()
   }
 
 //----------------------------------------------------------------------------
-/////
 PlusStatus vtkEpiphanVideoSource::InternalStartRecording()
 {
   if (!this->Recording)
     {
 		return PLUS_SUCCESS;
 	}
+  FrmGrab_Start(fg); 
 
   return this->Connect();
-    //this->Initialize();
-
-    //this->Recording = 1;
-    //this->FrameCount = 0;
-	//this->pauseFeed = 0;
-    //this->Modified();
-    //this->PlayerThreadId = 
-    //  this->PlayerThreader->SpawnThread((vtkThreadFunctionType)\
-    //                            &vtkEpiphanVideoSourceRecordThread,this);
-    //}
 }
 
+//----------------------------------------------------------------------------
+PlusStatus vtkEpiphanVideoSource::InternalStopRecording()
+{
+  FrmGrab_Stop(fg);
+  return PLUS_SUCCESS;
+}
 
+//----------------------------------------------------------------------------
 PlusStatus vtkEpiphanVideoSource::InternalGrab()
 {
 
@@ -270,37 +160,15 @@ PlusStatus vtkEpiphanVideoSource::InternalGrab()
   }
 
   V2U_GrabFrame2 * frame = NULL;
-  //V2U_UINT32 format = V2U_GRABFRAME_BOTTOM_UP_FLAG; // seems to be needed to orientate correctly.
-
-  V2U_UINT32 format = V2U_GRABFRAME_FORMAT_RGB8;
-  /*
-  if (this->OutputFormat == VTK_LUMINANCE) {
-	format |= V2U_GRABFRAME_FORMAT_YUY2;
-  } else if (this->OutputFormat == VTK_RGB) {
-
-	format |= V2U_GRABFRAME_FORMAT_RGB24;
-  } else if (this->OutputFormat == VTK_RGBA) {
-	format |= V2U_GRABFRAME_FORMAT_ARGB32;
-  } else {
-	  // no clue what format to grab, you can add more.
-	  return;
-  }
-*/
-  
-
-
-  frame= FrmGrab_Frame(this->fg, format, cropRect);
+  frame= FrmGrab_Frame(this->fg, VideoFormat, CropRect);
 
   if (frame == NULL)
   {
-	  return PLUS_SUCCESS;
+	  LOG_WARNING("Frame not captured");
+	  return PLUS_FAIL;
   }
 
-  this->FrameNumber++;
-
-  // Set unfiltered and filtered timestamp
-  double unfilteredTimestamp = vtkTimerLog::GetUniversalTime();//v2u_computeTime();  
-  double filteredTimestamp = vtkTimerLog::GetUniversalTime();//v2u_computeTime();  
+  this->FrameNumber++; 
 
   // If the buffer is empty, set the pixel type and frame size to the first received properties 
   if ( this->GetBuffer()->GetNumberOfItems() == 0 )
@@ -310,52 +178,20 @@ PlusStatus vtkEpiphanVideoSource::InternalGrab()
   }
 
   PlusStatus status = this->Buffer->AddItem(frame->pixbuf ,this->GetUsImageOrientation(), FrameSize, 
-	  itk::ImageIOBase::UCHAR,0,this->FrameNumber);//,unfilteredTimestamp, filteredTimestamp); 
+	  itk::ImageIOBase::UCHAR,0,this->FrameNumber);
   this->Modified();
   FrmGrab_Release(this->fg, frame);
   return status;
 }
 
-V2U_TIME vtkEpiphanVideoSource::v2u_computeTime()
-{
-    V2U_TIME now;
-    SYSTEMTIME sysTime;
-    FILETIME   fileTime;
-    GetSystemTime(&sysTime);
-    SystemTimeToFileTime(&sysTime, &fileTime);
-    now = ((V2U_TIME)fileTime.dwHighDateTime << 32) 
-         + (V2U_TIME)fileTime.dwLowDateTime;
-    now /= 10000;  /* convert from 100-ns ticks to milliseconds */
-    now -= 11644473600000i64;   /* Jan 1st 1601 to Jan 1st 1970 */
-    return now;
-}
-
-//----------------------------------------------------------------------------
-/////
-/**
-PlusStatus vtkEpiphanVideoSource::InternalStopRecording()
-{
-
-  if (!static_cast<DShowLib::Grabber*>(FrameGrabber)->stopLive())
-  {
-    LOG_ERROR("Framegrabber stopLive failed");
-    return PLUS_FAIL;
-  }
-
-
-
-}
-  */
 //-----------------------------------------------------------------------------
-
-/////
 PlusStatus vtkEpiphanVideoSource::ReadConfiguration(vtkXMLDataElement* config)
 {
 
 	LOG_TRACE("vtkEpiphanVideoSource::ReadConfiguration"); 
 	if ( config == NULL )
 	{
-		LOG_ERROR("Unable to configure IC Capturing video source! (XML data element is NULL)"); 
+		LOG_ERROR("Unable to configure Epiphan video source! (XML data element is NULL)"); 
 		return PLUS_FAIL; 
 	}
 
@@ -363,58 +199,89 @@ PlusStatus vtkEpiphanVideoSource::ReadConfiguration(vtkXMLDataElement* config)
 
 	vtkXMLDataElement* dataCollectionConfig = config->FindNestedElementWithName("DataCollection");
 	if (dataCollectionConfig == NULL)
-  {
-    LOG_ERROR("Cannot find DataCollection element in XML tree!");
+	{
+		LOG_ERROR("Cannot find DataCollection element in XML tree!");
 		return PLUS_FAIL;
 	}
 
-  vtkXMLDataElement* imageAcquisitionConfig = dataCollectionConfig->FindNestedElementWithName("ImageAcquisition"); 
-  if (imageAcquisitionConfig == NULL) 
-  {
-    LOG_ERROR("Unable to find ImageAcquisition element in configuration XML structure!");
-    return PLUS_FAIL;
-  }
-
-	const char* deviceName = imageAcquisitionConfig->GetAttribute("DeviceName"); 
-	if ( deviceName != NULL) 
+	vtkXMLDataElement* imageAcquisitionConfig = dataCollectionConfig->FindNestedElementWithName("ImageAcquisition"); 
+	if (imageAcquisitionConfig == NULL) 
 	{
-		this->SetDeviceName(deviceName); 
-	}
-
-	const char* videoNorm = imageAcquisitionConfig->GetAttribute("VideoNorm"); 
-	if ( videoNorm != NULL) 
-	{
-		this->SetVideoNorm(videoNorm); 
+		LOG_ERROR("Unable to find ImageAcquisition element in configuration XML structure!");
+		return PLUS_FAIL;
 	}
 
 	const char* videoFormat = imageAcquisitionConfig->GetAttribute("VideoFormat"); 
 	if ( videoFormat != NULL) 
 	{
-		this->SetVideoFormat(videoFormat); 
+		if( !strcmp(videoFormat,"RGB8") )
+		{
+			this->SetVideoFormat( V2U_GRABFRAME_FORMAT_RGB8 );
+		}
+		else if ( !strcmp(videoFormat,"Y8") )
+		{
+			this->SetVideoFormat( V2U_GRABFRAME_FORMAT_Y8 );
+		}
+		else 
+		{
+			LOG_WARNING("Video Format unspecified/not supported. Using Y8"); 
+			this->SetVideoFormat( V2U_GRABFRAME_FORMAT_Y8 );
+		}
 	}
 
-	const char* inputChannel = imageAcquisitionConfig->GetAttribute("InputChannel"); 
-	if ( inputChannel != NULL) 
+	const char* serialNumber = imageAcquisitionConfig->GetAttribute("SerialNumber");
+	if( serialNumber !=NULL)
 	{
-		this->SetInputChannel(inputChannel); 
+		this->SetSerialNumber(serialNumber);
 	}
-
-	int icBufferSize = 0; 
-	if ( imageAcquisitionConfig->GetScalarAttribute("ICBufferSize", icBufferSize) ) 
+	else
 	{
-		this->SetICBufferSize(icBufferSize); 
+		LOG_WARNING("Serial Number not specified. Will try detecting it automatically");
 	}
 
-  return PLUS_SUCCESS;
+	int CropRectX,CropRectY,CropRectWidth,CropRectHeight;
+	if ( imageAcquisitionConfig->GetScalarAttribute("CropRectX",CropRectX) )
+	{
+		this->CropRect->x=CropRectX;
+	}
+	else
+	{
+		this->CropRect->x=NULL;
+	}
+	if ( imageAcquisitionConfig->GetScalarAttribute("CropRectY",CropRectY) )
+	{
+		this->CropRect->y=CropRectY;
+	}
+	else
+	{
+		this->CropRect->y=NULL;
+	}
+	if ( imageAcquisitionConfig->GetScalarAttribute("CropRectWidth",CropRectWidth) )
+	{
+		this->CropRect->width=CropRectWidth;
+	}
+	else
+	{
+		LOG_WARNING("Crop Rectangle Width not specified. Setting it to NULL");
+		this->CropRect->width=NULL;
+	}
+	if ( imageAcquisitionConfig->GetScalarAttribute("CropRectHeight",CropRectHeight) )
+	{
+		this->CropRect->height=CropRectHeight;
+	}
+	else
+	{
+		LOG_WARNING("Crop Rectangle Height not specified. Setting it to NULL");
+		this->CropRect->height=NULL;
+	}
+
+	return PLUS_SUCCESS;
 
 }
 
 //-----------------------------------------------------------------------------
-
-/////
 PlusStatus vtkEpiphanVideoSource::WriteConfiguration(vtkXMLDataElement* config)
 {
-/**
   // Write superclass configuration
   Superclass::WriteConfiguration(config); 
 
@@ -438,14 +305,19 @@ PlusStatus vtkEpiphanVideoSource::WriteConfiguration(vtkXMLDataElement* config)
     return PLUS_FAIL;
   }
 
-  imageAcquisitionConfig->SetAttribute("DeviceName", this->DeviceName);
-  imageAcquisitionConfig->SetAttribute("VideoNorm", this->VideoNorm);
-  imageAcquisitionConfig->SetAttribute("VideoFormat", this->VideoFormat);
-  imageAcquisitionConfig->SetAttribute("InputChannel", this->InputChannel);
-  imageAcquisitionConfig->SetIntAttribute("ICBufferSize", this->ICBufferSize);
-*/
+  if ( this->VideoFormat == V2U_GRABFRAME_FORMAT_RGB8 )
+  {
+	  imageAcquisitionConfig->SetAttribute("VideoFormat", "RGB8");
+  }
+  else
+  {
+	  imageAcquisitionConfig->SetAttribute("VideoFormat", "Y8");
+  }
+  imageAcquisitionConfig->SetAttribute("SerialNumber", this->SerialNumber);
+  imageAcquisitionConfig->SetIntAttribute("CropX", this->CropRect->x);
+  imageAcquisitionConfig->SetIntAttribute("CropY", this->CropRect->y);
+  imageAcquisitionConfig->SetIntAttribute("CropWidth", this->CropRect->width);
+  imageAcquisitionConfig->SetIntAttribute("CropHeight", this->CropRect->height);
+
   return PLUS_SUCCESS;
-
 }
-
-
--- trunk/PlusLib/src/ImageAcquisition/Epiphan/vtkEpiphanVideoSource.h
+++ trunk/PlusLib/src/ImageAcquisition/Epiphan/vtkEpiphanVideoSource.h
@@ -8,8 +8,6 @@
 #define __vtkEpiphanVideoSource_h
 
 #include "vtkPlusVideoSource.h"
-#include "vtkMultiThreader.h"
-#include "vtkTimerLog.h"
 #include "epiphan/frmgrab.h"
 
 /* exit codes */
@@ -25,22 +23,8 @@
 class VTK_EXPORT vtkEpiphanVideoSource;
 
 /*!
-  \class vtkICCapturingSourceCleanup 
-  \brief Class that cleans up (deletes singleton instance of) vtkICCapturingSource when destroyed
-  \ingroup PlusLibImageAcquisition
-*/
-/*
-class VTK_EXPORT vtkEpiphanVideoSourceCleanup
-{
-public:
-	vtkEpiphanVideoSourceCleanup();
-	~vtkEpiphanVideoSourceCleanup();
-};
-*/
-
-/*!
-  \class vtkICCapturingSource 
-  \brief Class for providing video input interfaces between VTK and ICCapturing frame grabber device
+  \class vtkEpiphanVideoSource 
+  \brief Class for providing video input interfaces between VTK and Epiphan frame grabber device
   \ingroup PlusLibImageAcquisition
 */
 class VTK_EXPORT vtkEpiphanVideoSource : public vtkPlusVideoSource
@@ -49,59 +33,25 @@ public:
 	static vtkEpiphanVideoSource *New();
 	vtkTypeRevisionMacro(vtkEpiphanVideoSource,vtkPlusVideoSource);
 	void PrintSelf(ostream& os, vtkIndent indent);   
-  /*! This is a singleton pattern New.  There will only be ONE
-	 reference to a vtkOutputWindow object per process.  Clients that
-	 call this must call Delete on the object so that the reference
-	 counting will work.   The single instance will be unreferenced when
-	 the program exits
-  */
-  /*! Return the singleton instance with no reference counting. */
-	//static vtkEpiphanVideoSource* GetInstance();
 
-  /*! Supply a user defined output window. Call ->Delete() on the supplied instance after setting it. */
-	//static void SetInstance(vtkEpiphanVideoSource *instance);
-
-	//BTX
-	/*! Use this as a way of memory management when the
-	 program exits the SmartPointer will be deleted which
-	 will delete the Instance singleton
-  */
-	//static vtkEpiphanVideoSourceCleanup Cleanup;
-	//ETX
-
-  /*! Hardware device SDK version. */
-  virtual std::string GetSdkVersion();
   /*! Read configuration from xml data */	
-  virtual PlusStatus ReadConfiguration(vtkXMLDataElement* config); 
+    virtual PlusStatus ReadConfiguration(vtkXMLDataElement* config); 
   /*! Write configuration to xml data */
-  virtual PlusStatus WriteConfiguration(vtkXMLDataElement* config);
+	virtual PlusStatus WriteConfiguration(vtkXMLDataElement* config);
+  /*! Set the Epiphan device video format (e.g. "Y8" ) */
+	vtkSetMacro(VideoFormat,V2U_UINT32); 
+  /*! Get the Epiphan device video format (e.g. "Y8" ) */
+	vtkGetMacro(VideoFormat,V2U_UINT32);  
 
-  /*! Set the IC capturing device name (e.g. "DFG/USB2-lt") */
-	vtkSetStringMacro(DeviceName); 
-  /*! Get the IC capturing device name (e.g. "DFG/USB2-lt") */
-	vtkGetStringMacro(DeviceName); 
+  /*! Set the Epiphan device serial */
+	vtkSetStringMacro(SerialNumber); 
+  /*! Get the Epiphan device serial */
+	vtkGetStringMacro(SerialNumber); 
 
-  /*! Set the IC capturing device video norm (e.g. "PAL_B", "NTSC_M") */
-	vtkSetStringMacro(VideoNorm); 
-  /*! Get the IC capturing device video norm (e.g. "PAL_B", "NTSC_M") */
-	vtkGetStringMacro(VideoNorm); 
-
-  /*! Set the IC capturing device video format (e.g. "Y800 (640x480)" ) */
-	vtkSetStringMacro(VideoFormat); 
-  /*! Get the IC capturing device video format (e.g. "Y800 (640x480)" ) */
-	vtkGetStringMacro(VideoFormat); 
-
-  /*! Set the IC capturing device input channel (e.g. "01 Video: SVideo" ) */
-	vtkSetStringMacro(InputChannel); 
-  /*! Get the IC capturing device input channel (e.g. "01 Video: SVideo" ) */
-	vtkGetStringMacro(InputChannel); 
-
-  /*! Set the IC capturing device buffer size ( Default: 50 frame ) */
-	vtkSetMacro(ICBufferSize, int); 
-  /*! Get the IC capturing device buffer size ( Default: 50 frame ) */
-	vtkGetMacro(ICBufferSize, int); 
-
-	V2U_TIME v2u_computeTime();
+  /*! Set the crop rectangle of frame grabber */
+//	vtkSetMacro(CropRect, V2URect);
+  /*! Get the crop rectangle of frame grabber */
+//	vtkGetMacro(CropRect, V2URect);
 
 protected:
   /*! Constructor */
@@ -119,50 +69,27 @@ protected:
 	virtual PlusStatus InternalStartRecording();
 
   /*! Device-specific recording stop */
-//	virtual PlusStatus InternalStopRecording();
+	virtual PlusStatus InternalStopRecording();
 
   /*! The internal function which actually does the grab.  */
 	PlusStatus InternalGrab();
 
-  /*! Adds a frame to the frame buffer. Called whenever the driver notified a new frame acquisition. */
-	//PlusStatus AddFrameToBuffer(unsigned char * data, unsigned long size, unsigned long frameNumber);
+  /*! Video format (e.g. Y8) */
+	V2U_UINT32 VideoFormat;
 
-  /*! Frame grabber device - DShowLib::Grabber type */
-	void* FrameGrabber;
-	
-  /*! Device name (e.g. DFG/USB2-lt) */
-	char* DeviceName;
+  /*! Crop rectangle for the grabber */
+	V2URect * CropRect;
 
-  /*! Video norm (e.g. PAL_B or NTSC_M) */
-	char* VideoNorm;
+  /*! Serial number of the frame grabber */
+	char* SerialNumber;
 
-  /*! Video format (e.g. Y800) */
-	char* VideoFormat;
-
-  /*! Input channel name */
-	char* InputChannel; 
-
-  /*! IC buffer size */
-	int ICBufferSize; 
-
-	// crop rectangle for the grabber
-	V2URect * cropRect;
-
-	 // serial number of the frame grabber
-	 char serialNumber[15];
-
-	// pointer to the grabber
+  /*! Epiphan Pointer to the grabber */
 	FrmGrabber* fg;
 
-	// status of the frame grabber
-	int status;
-
+  /*! Frame size of the captured image */
 	int FrameSize[2];
 
 private:
-
-	//static vtkEpiphanVideoSource* Instance;
-	//static bool vtkEpiphanVideoSourceNewFrameCallback(unsigned char * data, unsigned long size, unsigned long frameNumber);
 	vtkEpiphanVideoSource(const vtkEpiphanVideoSource&);  // Not implemented.
 	void operator=(const vtkEpiphanVideoSource&);  // Not implemented.
 };
