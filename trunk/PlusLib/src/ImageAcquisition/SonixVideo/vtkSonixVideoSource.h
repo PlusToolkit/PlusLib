119dd06e57bfdc660b3236fee54e7e13eef89634
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-07-26 18:34:19 +0000

    Removed redundant file header information

--- trunk/PlusLib/src/ImageAcquisition/SonixVideo/vtkSonixVideoSource.h
+++ trunk/PlusLib/src/ImageAcquisition/SonixVideo/vtkSonixVideoSource.h
@@ -5,47 +5,13 @@
 =========================================================Plus=header=end*/
 
 /*=========================================================================
-
-Module:    $RCSfile: vtkSonixVideoSource.h,v $
-Author:  Siddharth Vikal, Queens School Of Computing
-
+The following copyright notice is applicable to parts of this file:
 Copyright (c) 2008, Queen's University, Kingston, Ontario, Canada
 All rights reserved.
-
-Author: Danielle Pace
-        Robarts Research Institute and The University of Western Ontario
-
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
- * Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-
- * Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in
-   the documentation and/or other materials provided with the
-   distribution.
-
- * Neither the name of Queen's University nor the names of any
-   contributors may be used to endorse or promote products derived
-   from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-=========================================================================*/
+Authors include: Danielle Pace
+(Robarts Research Institute and The University of Western Ontario)
+Siddharth Vikal (Queen's University, Kingston, Ontario, Canada)
+=========================================================================*/  
 
 #ifndef __vtkSonixVideoSource_h
 #define __vtkSonixVideoSource_h
