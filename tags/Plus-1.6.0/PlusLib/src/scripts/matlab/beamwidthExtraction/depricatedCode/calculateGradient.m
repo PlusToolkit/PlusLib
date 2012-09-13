7d4383ba33b8c36d6b6f11a92fb5ea08d83802c6
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-09-13 17:53:06 +0000

    Removing incorrect tag

--- tags/Plus-1.6.0/PlusLib/src/scripts/matlab/beamwidthExtraction/depricatedCode/calculateGradient.m
+++
@@ -1,9 +0,0 @@
-function grad = calculateGradient(image,GradientMatrix)
-
-    for (i = 1:1:size(image,1))
-        for (j = 1:1:size(image,2))
-            grad(i,j) = calculateGradientHelper(image,i,j,GradientMatrix);
-        end
-    end
-
-end
