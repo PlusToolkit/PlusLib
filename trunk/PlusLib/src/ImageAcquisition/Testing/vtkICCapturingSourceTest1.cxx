91f14d88038152d2ef896bf231e5d221d308135b
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-09-21 13:19:48 +0000

    Fixed #603: Added missing image orientation initialization. Simplified vtkICCapturingSourceTest1.cxx: use smart pointers, remove unnecessary logging callback (all VTK errors and warnings are redirected to stdout globally). Fixed typo (orinted=>oriented).

--- trunk/PlusLib/src/ImageAcquisition/Testing/vtkICCapturingSourceTest1.cxx
+++ trunk/PlusLib/src/ImageAcquisition/Testing/vtkICCapturingSourceTest1.cxx
@@ -1,7 +1,7 @@
 /*=Plus=header=begin======================================================
-  Program: Plus
-  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
-  See License.txt for details.
+Program: Plus
+Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+See License.txt for details.
 =========================================================Plus=header=end*/
 
 #include "PlusConfigure.h"
@@ -15,187 +15,120 @@
 #include "vtkCommand.h"
 #include "vtkCallbackCommand.h"
 
-
-vtkImageViewer2 *viewer = NULL;
-vtkRenderWindowInteractor *iren = NULL;
-
-void PrintLogsCallback(vtkObject* obj, unsigned long eid, void* clientdata, void* calldata); 
-
 class vtkMyCallback : public vtkCommand
 {
 public:
-	static vtkMyCallback *New()
-	{return new vtkMyCallback;}
-	virtual void Execute(vtkObject *caller, unsigned long, void*)
-	{
-		viewer->Render();
-				
-		//update the timer so it will trigger again
-		iren->CreateTimer(VTKI_TIMER_UPDATE);
-	}
+  static vtkMyCallback *New() {return new vtkMyCallback;}
+  virtual void Execute(vtkObject *caller, unsigned long, void*)
+  {
+    viewer->Render();
+
+    //update the timer so it will trigger again
+    iren->CreateTimer(VTKI_TIMER_UPDATE);
+  }
+  vtkImageViewer2* viewer;
+  vtkRenderWindowInteractor *iren;
 };
 
 int main(int argc, char **argv)
 {
+  bool printHelp(false); 
+  bool renderingOff(false);
+  std::string deviceName("DFG/USB2-lt");
+  std::string videoNorm("NTSC_M");
+  std::string videoFormat("Y800 (640x480)");
+  std::string inputChannel("01 Video: SVideo");
 
-	bool printHelp(false); 
-	bool renderingOff(false);
-	std::string deviceName("DFG/USB2-lt");
-	std::string videoNorm("NTSC_M");
-	std::string videoFormat("Y800 (640x480)");
-	std::string inputChannel("01 Video: SVideo");
+  vtksys::CommandLineArguments args;
+  args.Initialize(argc, argv);
 
-	vtksys::CommandLineArguments args;
-	args.Initialize(argc, argv);
+  int verboseLevel = vtkPlusLogger::LOG_LEVEL_UNDEFINED;
 
-	int verboseLevel = vtkPlusLogger::LOG_LEVEL_UNDEFINED;
+  args.AddArgument("--help", vtksys::CommandLineArguments::NO_ARGUMENT, &printHelp, "Print this help.");	
+  args.AddArgument("--rendering-off", vtksys::CommandLineArguments::NO_ARGUMENT, &renderingOff, "Run test without rendering.");	
+  args.AddArgument("--device-name", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &deviceName, "IC Capturing device name (Default: DFG/USB2-lt)." );
+  args.AddArgument("--video-norm", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &videoNorm, "IC Capturing device video norm (Default: NTSC_M)." );
+  args.AddArgument("--video-format", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &videoFormat, "IC Capturing device video format (Default: Y800 (640x480))." );
+  args.AddArgument("--input-channel", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputChannel, "IC Capturing device input channel (Default: 01 Video: SVideo)." );
+  args.AddArgument("--verbose", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &verboseLevel, "Verbose level (1=error only, 2=warning, 3=info, 4=debug, 5=trace)");	
 
-	args.AddArgument("--help", vtksys::CommandLineArguments::NO_ARGUMENT, &printHelp, "Print this help.");	
-	args.AddArgument("--rendering-off", vtksys::CommandLineArguments::NO_ARGUMENT, &renderingOff, "Run test without rendering.");	
-	args.AddArgument("--device-name", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &deviceName, "IC Capturing device name (Default: DFG/USB2-lt)." );
-	args.AddArgument("--video-norm", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &videoNorm, "IC Capturing device video norm (Default: NTSC_M)." );
-	args.AddArgument("--video-format", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &videoFormat, "IC Capturing device video format (Default: Y800 (640x480))." );
-	args.AddArgument("--input-channel", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputChannel, "IC Capturing device input channel (Default: 01 Video: SVideo)." );
-	args.AddArgument("--verbose", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &verboseLevel, "Verbose level (1=error only, 2=warning, 3=info, 4=debug, 5=trace)");	
-
-	if ( !args.Parse() )
-	{
-		std::cerr << "Problem parsing arguments" << std::endl;
-		std::cout << "\n\nvtkICCapturingSourceTest1 help:" << args.GetHelp() << std::endl;
-		exit(EXIT_FAILURE);
-	}
+  if ( !args.Parse() )
+  {
+    std::cerr << "Problem parsing arguments" << std::endl;
+    std::cout << "\n\nvtkICCapturingSourceTest1 help:" << args.GetHelp() << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
   vtkPlusLogger::Instance()->SetLogLevel(verboseLevel);
-  
-	if ( printHelp ) 
-	{
-		std::cout << "\n\nvtkICCapturingSourceTest1 help:" << args.GetHelp() << std::endl;
-		exit(EXIT_SUCCESS); 
 
-	}
+  if ( printHelp ) 
+  {
+    std::cout << "\n\nvtkICCapturingSourceTest1 help:" << args.GetHelp() << std::endl;
+    exit(EXIT_SUCCESS); 
+  }
 
-	vtkICCapturingSource* frameGrabber = vtkICCapturingSource::New();
+  vtkSmartPointer<vtkICCapturingSource> frameGrabber = vtkSmartPointer<vtkICCapturingSource>::New();
 
-	frameGrabber->SetDeviceName(deviceName.c_str()); 
-	frameGrabber->SetVideoNorm(videoNorm.c_str()); 
-	frameGrabber->SetVideoFormat(videoFormat.c_str()); 
-	frameGrabber->SetInputChannel(inputChannel.c_str()); 
+  frameGrabber->SetDeviceName(deviceName.c_str()); 
+  frameGrabber->SetVideoNorm(videoNorm.c_str()); 
+  frameGrabber->SetVideoFormat(videoFormat.c_str()); 
+  frameGrabber->SetInputChannel(inputChannel.c_str()); 
 
-	// Add an observer to warning and error events for redirecting it to the stdout 
-	vtkCallbackCommand* callbackCommand = vtkCallbackCommand::New();
-	callbackCommand->SetCallback(PrintLogsCallback);
-	frameGrabber->AddObserver("WarningEvent", callbackCommand); 
-	frameGrabber->AddObserver("ErrorEvent", callbackCommand); 
+  frameGrabber->SetDeviceImageOrientation(US_IMG_ORIENT_MF);
 
-	LOG_INFO("Initialize..."); 
-	frameGrabber->Connect(); 
+  LOG_INFO("Initialize..."); 
+  frameGrabber->Connect(); 
 
-	if ( frameGrabber->GetConnected() )
-	{
-		LOG_INFO("Start recording..."); 
-		frameGrabber->StartRecording(); 
-	}
-	else
-	{
-		frameGrabber->Disconnect();
-		if ( frameGrabber != NULL ) 
-		{
-			frameGrabber->Delete();
-		}
+  if ( frameGrabber->GetConnected() )
+  {
+    LOG_INFO("Start recording..."); 
+    frameGrabber->StartRecording(); 
+  }
+  else
+  {
+    frameGrabber->Disconnect();
+    LOG_ERROR( "Unable to connect to IC capture device: " << deviceName ); 
+    exit(EXIT_FAILURE); 
+  }
 
-		if ( viewer != NULL ) 
-		{
-			viewer->Delete();
-		}
+  if (renderingOff)
+  {
+    LOG_INFO("No need for rendering, stop the device..."); 
+    frameGrabber->StopRecording(); 
+    frameGrabber->Disconnect();
+    LOG_INFO("Exit successfully"); 
+    exit(EXIT_SUCCESS); 
+  }
 
-		if ( iren != NULL ) 
-		{
-			iren->Delete();
-		}
+  vtkSmartPointer<vtkImageViewer2> viewer = vtkSmartPointer<vtkImageViewer2>::New();
+  viewer->SetColorWindow(255);
+  viewer->SetColorLevel(100.5);
+  viewer->SetSize(640,480); 
 
-		if ( callbackCommand != NULL ) 
-		{
-			callbackCommand->Delete(); 
-		}
+  viewer->SetInput(frameGrabber->GetOutput()); 
 
-		LOG_ERROR( "Unable to connect to IC capture device: " << deviceName ); 
-		exit(EXIT_FAILURE); 
-	}
+  //Create the interactor that handles the event loop
+  vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  iren->SetRenderWindow(viewer->GetRenderWindow());
+  viewer->SetupInteractor(iren);
 
+  viewer->Render(); 
 
-	if (renderingOff)
-	{
-		LOG_INFO("No need for rendering, stop the device..."); 
-		frameGrabber->StopRecording(); 
-		frameGrabber->Disconnect();
+  //establish timer event and create timer
+  vtkSmartPointer<vtkMyCallback> call = vtkSmartPointer<vtkMyCallback>::New();
+  call->viewer=viewer;
+  call->iren=iren;
+  iren->AddObserver(vtkCommand::TimerEvent, call);
+  iren->CreateTimer(VTKI_TIMER_FIRST);		//VTKI_TIMER_FIRST = 0
 
-		if ( frameGrabber != NULL ) 
-		{
-			frameGrabber->Delete();
-		}
+  // iren must be initialized so that it can handle events
+  iren->Initialize();
+  // start the event loop (exit with 'q' key)
+  iren->Start();
 
-		if ( viewer != NULL ) 
-		{
-			viewer->Delete();
-		}
+  frameGrabber->StopRecording(); 
+  frameGrabber->Disconnect();
 
-		if ( iren != NULL ) 
-		{
-			iren->Delete();
-		}
-
-		if ( callbackCommand != NULL ) 
-		{
-			callbackCommand->Delete(); 
-		}
-
-		LOG_INFO("Exit successfully"); 
-		exit(EXIT_SUCCESS); 
-	}
-
-	viewer = vtkImageViewer2::New();
-
-	viewer->SetColorWindow(255);
-	viewer->SetColorLevel(100.5);
-	viewer->SetSize(640,480); 
-
-	viewer->SetInput(frameGrabber->GetOutput()); 
-
-	//Create the interactor that handles the event loop
-	iren = vtkRenderWindowInteractor::New();
-	iren->SetRenderWindow(viewer->GetRenderWindow());
-	viewer->SetupInteractor(iren);
-	
-	viewer->Render(); 
-
-	//establish timer event and create timer
-	vtkSmartPointer<vtkMyCallback> call = vtkSmartPointer<vtkMyCallback>::New();
-	iren->AddObserver(vtkCommand::TimerEvent, call);
-	iren->CreateTimer(VTKI_TIMER_FIRST);		//VTKI_TIMER_FIRST = 0
-
-	//iren must be initialized so that it can handle events
-	iren->Initialize();
-	iren->Start();
-
-	frameGrabber->Delete(); 
-	viewer->Delete();
-	iren->Delete();
-	callbackCommand->Delete(); 
-
-	LOG_INFO("Exit successfully"); 
-	return EXIT_SUCCESS; 
-
- }
-
- // Callback function for error and warning redirects
-void PrintLogsCallback(vtkObject* obj, unsigned long eid, void* clientdata, void* calldata)
-{
-	if ( eid == vtkCommand::GetEventIdFromString("WarningEvent") )
-	{
-		LOG_WARNING((const char*)calldata);
-	}
-	else if ( eid == vtkCommand::GetEventIdFromString("ErrorEvent") )
-	{
-		LOG_ERROR((const char*)calldata);
-	}
+  LOG_INFO("Exit successfully"); 
+  return EXIT_SUCCESS; 
 }
