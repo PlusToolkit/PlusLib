fed0659a1f9c11f4dddbed45b515c52c08763ba8
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-10-12 21:06:08 +0000

    re #615: Added PlusConfiguration_VideoNone_3dConnexion.xml to the installation package

--- trunk/PlusApp/InstallFiles.cmake
+++ trunk/PlusApp/InstallFiles.cmake
@@ -15,6 +15,7 @@ SET ( PLUSLIB_CONFIG_FILES
   ${PLUSLIB_DATA_DIR}/ConfigFiles/PlusConfiguration_SonixTouch_L14-5_NDICertus_fCal_1.0.xml
   ${PLUSLIB_DATA_DIR}/ConfigFiles/PlusConfiguration_VideoNone_FakeTracker_PivotCalibration_fCal.xml
   ${PLUSLIB_DATA_DIR}/ConfigFiles/PlusConfiguration_VideoNone_FakeTracker_RecordPhantomLandmarks_fCal2.0.xml
+  ${PLUSLIB_DATA_DIR}/ConfigFiles/PlusConfiguration_VideoNone_3dConnexion.xml
   ${PLUSLIB_DATA_DIR}/CADModels/EndocavityProbe/EC9-5_10_Assembly.stl
   ${PLUSLIB_DATA_DIR}/CADModels/fCalPhantom/FCal_1.0.stl
   ${PLUSLIB_DATA_DIR}/CADModels/fCalPhantom/FCal_1.1.stl
