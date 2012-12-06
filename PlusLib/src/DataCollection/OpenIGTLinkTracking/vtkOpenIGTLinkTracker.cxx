8092df58041299ed0e95eb665c74c111341ae11f
Adam Rankin <arankin@robarts.ca>
2012-12-06 18:12:10 +0000

    Merging the multistream branch into the trunk.
    New stream architecture implemented.
    
    Current artificial restriction is one image buffer per stream (easily fixed by adding a <ImageBuffer> sub-tag to streams).
    
    Full documentation on new config file structure will be written up shortly. See PlusConfiguration_File_fCal_2.0.xml and PlusConfiguration_SonixTouch_Ascension3DG_L14_fCal2.0.xml for samples.
    
    Presentation will be made fully outlining the current state of the architecture and a discussion will be held to review changes.

---
+++ PlusLib/src/DataCollection/OpenIGTLinkTracking/vtkOpenIGTLinkTracker.cxx
@@ -0,0 +1,348 @@
+/*=Plus=header=begin======================================================
+  Program: Plus
+  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+  See License.txt for details.
+=========================================================Plus=header=end*/
+
+#include "PlusConfigure.h"
+#include "PlusConfigure.h"
+#include "igtlMessageHeader.h"
+#include "igtlPlusClientInfoMessage.h"
+#include "igtlPositionMessage.h"
+#include "igtlTransformMessage.h"
+#include "vtkMatrix4x4.h"
+#include "vtkObjectFactory.h"
+#include "vtkOpenIGTLinkTracker.h"
+#include "vtkPlusIgtlMessageCommon.h"
+#include "vtkPlusStreamBuffer.h"
+#include "vtkPlusStreamTool.h"
+#include "vtkTransform.h"
+#include "vtkXMLDataElement.h"
+#include "vtksys/SystemTools.hxx"
+#include <sstream>
+
+static const int CLIENT_SOCKET_TIMEOUT_MSEC = 500; 
+
+vtkStandardNewMacro(vtkOpenIGTLinkTracker);
+
+//----------------------------------------------------------------------------
+vtkOpenIGTLinkTracker::vtkOpenIGTLinkTracker()
+{
+  this->MessageType = NULL; 
+  this->ServerAddress = NULL; 
+  this->ServerPort = -1; 
+  this->NumberOfRetryAttempts = 10; 
+  this->DelayBetweenRetryAttemptsSec = 0.100; // there is already a delay with a CLIENT_SOCKET_TIMEOUT_MSEC timeout, so we just add a little extra idle delay
+  this->IgtlMessageCrcCheckEnabled = 0; 
+  this->ClientSocket = igtl::ClientSocket::New(); 
+
+  this->RequireDeviceImageOrientationInDeviceSetConfiguration = false;
+  this->RequireFrameBufferSizeInDeviceSetConfiguration = false;
+  this->RequireAcquisitionRateInDeviceSetConfiguration = false;
+  this->RequireAveragedItemsForFilteringInDeviceSetConfiguration = true;
+  this->RequireLocalTimeOffsetSecInDeviceSetConfiguration = false;
+  this->RequireUsImageOrientationInDeviceSetConfiguration = false;
+  this->RequireRfElementInDeviceSetConfiguration = false;
+}
+
+//----------------------------------------------------------------------------
+vtkOpenIGTLinkTracker::~vtkOpenIGTLinkTracker() 
+{
+  if ( this->Recording )
+  {
+    this->StopRecording();
+  }
+}
+
+//----------------------------------------------------------------------------
+void vtkOpenIGTLinkTracker::PrintSelf( ostream& os, vtkIndent indent )
+{
+  Superclass::PrintSelf( os, indent );
+}
+
+//----------------------------------------------------------------------------
+std::string vtkOpenIGTLinkTracker::GetSdkVersion()
+{
+  std::ostringstream version; 
+  version << "OpenIGTLink v" << PLUS_OPENIGTLINK_VERSION; 
+  return version.str(); 
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkOpenIGTLinkTracker::Connect()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::Connect" ); 
+
+  if ( this->ClientSocket->GetConnected() )
+  {
+    return PLUS_SUCCESS; 
+  }
+
+  if ( this->ServerAddress == NULL )
+  {
+    LOG_ERROR("Unable to connect OpenIGTLink server - server address is undefined" ); 
+    return PLUS_FAIL; 
+  }
+
+  if ( this->ServerPort < 0 )
+  {
+    LOG_ERROR("Unable to connect OpenIGTLink server - server port is invalid: " << this->ServerPort ); 
+    return PLUS_FAIL; 
+  }
+
+  int errorCode = 0; // 0 means success
+  RETRY_UNTIL_TRUE( 
+    (errorCode = this->ClientSocket->ConnectToServer( this->ServerAddress, this->ServerPort ))==0,
+    this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
+
+  if ( errorCode != 0 )
+  {
+    LOG_ERROR( "Cannot connect to the server (" << this->ServerAddress << ":" << this->ServerPort << ")." );
+    return PLUS_FAIL;
+  }
+  else
+  {
+    LOG_DEBUG( "Client successfully connected to server (" << this->ServerAddress << ":" << this->ServerPort << ")."  );
+  }
+
+  this->ClientSocket->SetTimeout(CLIENT_SOCKET_TIMEOUT_MSEC); 
+
+  // Clear buffers on connect
+  this->ClearAllBuffers(); 
+
+  // If we specified message type, try to send it to the server
+  if ( this->MessageType != NULL )
+  {
+    // Send clinet info request to the server
+    PlusIgtlClientInfo clientInfo; 
+    // Set message type
+    clientInfo.IgtlMessageTypes.push_back(this->MessageType); 
+
+    // We need the following tool names from the server 
+    for ( ToolContainerConstIterator it = this->GetToolIteratorBegin(); it != this->GetToolIteratorEnd(); ++it )
+    {
+      PlusTransformName tName( it->second->GetToolName(), this->GetToolReferenceFrameName() ); 
+      clientInfo.TransformNames.push_back( tName ); 
+    }
+
+    // Pack client info message 
+    igtl::PlusClientInfoMessage::Pointer clientInfoMsg = igtl::PlusClientInfoMessage::New(); 
+    clientInfoMsg->SetClientInfo(clientInfo); 
+    clientInfoMsg->Pack(); 
+
+    // Send message to server 
+    int retValue = 0;
+    RETRY_UNTIL_TRUE( 
+      (retValue = this->ClientSocket->Send( clientInfoMsg->GetPackPointer(), clientInfoMsg->GetPackSize() ))!=0,
+      this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
+
+    if ( retValue == 0 )
+    {
+      LOG_ERROR("Failed to send PlusClientInfo message to server!"); 
+      return PLUS_FAIL; 
+    }
+  }
+
+  return PLUS_SUCCESS; 
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkOpenIGTLinkTracker::Disconnect()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::Disconnect" ); 
+  this->ClientSocket->CloseSocket(); 
+  return this->StopRecording(); 
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkOpenIGTLinkTracker::Probe()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::Probe" ); 
+
+  PlusStatus trackerStatus = PLUS_FAIL; 
+  if ( this->Connect() == PLUS_SUCCESS )
+  {
+    trackerStatus = PLUS_SUCCESS; 
+    this->Disconnect(); 
+  }
+  
+  return trackerStatus; 
+} 
+
+//----------------------------------------------------------------------------
+PlusStatus vtkOpenIGTLinkTracker::InternalStartRecording()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::InternalStopRecording" ); 
+  if ( this->Recording )
+  {
+    return PLUS_SUCCESS;
+  }
+
+  return this->Connect(); 
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkOpenIGTLinkTracker::InternalStopRecording()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::InternalStopRecording" ); 
+  
+  return PLUS_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkOpenIGTLinkTracker::InternalUpdate()
+{
+  LOG_TRACE( "vtkOpenIGTLinkTracker::InternalUpdate" ); 
+
+  if ( ! this->Recording )
+  {
+    LOG_ERROR("called Update() when not tracking" );
+    return PLUS_FAIL;
+  }
+
+  igtl::MessageHeader::Pointer headerMsg;
+  headerMsg = igtl::MessageHeader::New();
+  headerMsg->InitPack();
+
+  int numOfBytesReceived = 0;
+  RETRY_UNTIL_TRUE( 
+    (numOfBytesReceived = this->ClientSocket->Receive( headerMsg->GetPackPointer(), headerMsg->GetPackSize() ))!=0,
+    this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
+   
+  if ( numOfBytesReceived == 0 ) 
+  {
+	// No message received - server disconnected
+    LOG_ERROR("OpenIGTLink tracker connection lost with server - try to reconnect!");
+    this->ClientSocket->CloseSocket(); 
+    return this->Connect(); 
+  }
+
+  // Received data is not as we expected
+  if ( numOfBytesReceived != headerMsg->GetPackSize() )
+  {
+    LOG_ERROR("Couldn't receive data from OpenIGTLink tracker"); 
+    return PLUS_FAIL; 
+  }
+
+  vtkSmartPointer<vtkMatrix4x4> toolMatrix = vtkSmartPointer<vtkMatrix4x4>::New(); 
+  double unfilteredTimestampUtc = 0; 
+
+  igtl::TimeStamp::Pointer igtlTimestamp = igtl::TimeStamp::New(); 
+  igtl::Matrix4x4 igtlMatrix;
+  igtl::IdentityMatrix(igtlMatrix);
+  std::string igtlTransformName; 
+  
+  headerMsg->Unpack(this->IgtlMessageCrcCheckEnabled);
+  if (strcmp(headerMsg->GetDeviceType(), "TRANSFORM") == 0)
+  {
+    if ( vtkPlusIgtlMessageCommon::UnpackTransformMessage(headerMsg, this->ClientSocket.GetPointer(), toolMatrix, igtlTransformName, unfilteredTimestampUtc, this->IgtlMessageCrcCheckEnabled) != PLUS_SUCCESS )
+    {
+      LOG_ERROR("Couldn't receive transform message from server!"); 
+      return PLUS_FAIL;
+    }
+  }
+  else if (strcmp(headerMsg->GetDeviceType(), "POSITION") == 0)
+  {
+    float position[3] = {0}; 
+    if ( vtkPlusIgtlMessageCommon::UnpackPositionMessage(headerMsg, this->ClientSocket.GetPointer(), position, igtlTransformName, unfilteredTimestampUtc, this->IgtlMessageCrcCheckEnabled) != PLUS_SUCCESS )
+    {
+      LOG_ERROR("Couldn't receive position message from server!"); 
+      return PLUS_FAIL;
+    }
+
+    toolMatrix->Identity(); 
+    toolMatrix->SetElement(0,3, position[0]); 
+    toolMatrix->SetElement(1,3, position[1]); 
+    toolMatrix->SetElement(2,3, position[2]); 
+  }
+  else
+  {
+    // if the data type is unknown, skip reading. 
+    this->ClientSocket->Skip(headerMsg->GetBodySizeToRead(), 0);
+    return PLUS_SUCCESS; 
+  }
+
+  // Set transform name
+  PlusTransformName transformName;
+  if ( transformName.SetTransformName( igtlTransformName.c_str() ) != PLUS_SUCCESS )
+  {
+    LOG_ERROR("Failed to update tracker tool - unrecognized transform name: " << igtlTransformName ); 
+    return PLUS_FAIL; 
+  }
+  
+  // Convert timestamp from UTC to system  
+  double unfilteredTimestamp = vtkAccurateTimer::GetSystemTimeFromUniversalTime(unfilteredTimestampUtc); 
+
+  double filteredTimestamp = unfilteredTimestamp; // No need to filter already filtered timestamped items received over OpenIGTLink 
+  if ( this->ToolTimeStampedUpdateWithoutFiltering(transformName.From().c_str(), toolMatrix, TOOL_OK, unfilteredTimestamp, filteredTimestamp) != PLUS_SUCCESS )
+  {
+    LOG_ERROR("ToolTimeStampedUpdate failed for tool: " << transformName.From() << " with timestamp: " << std::fixed << unfilteredTimestamp); 
+    return PLUS_FAIL;
+  }
+
+  return PLUS_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkOpenIGTLinkTracker::ReadConfiguration( vtkXMLDataElement* config )
+{
+  // Read superclass configuration first
+  Superclass::ReadConfiguration( config ); 
+
+  LOG_TRACE( "vtkOpenIGTLinkTracker::ReadConfiguration" ); 
+  if ( config == NULL ) 
+  {
+    LOG_ERROR("Unable to find OpenIGTLinkTracker XML data element");
+    return PLUS_FAIL; 
+  }
+
+  vtkXMLDataElement* trackerConfig = this->FindThisDeviceElement(config);
+  if (trackerConfig == NULL) 
+  {
+    LOG_ERROR("Cannot find Tracker element in XML tree!");
+		return PLUS_FAIL;
+  }
+
+  const char* messageType = trackerConfig->GetAttribute("MessageType"); 
+  if ( messageType != NULL )
+  {
+    this->SetMessageType(messageType); 
+  }
+
+  const char* serverAddress = trackerConfig->GetAttribute("ServerAddress"); 
+  if ( serverAddress != NULL )
+  {
+    this->SetServerAddress(serverAddress); 
+  }
+  else
+  {
+    LOG_ERROR("Unable to find ServerAddress attribute!"); 
+    return PLUS_FAIL; 
+  }
+
+  int serverPort = -1; 
+  if ( trackerConfig->GetScalarAttribute("ServerPort", serverPort ) )
+  {
+    this->SetServerPort(serverPort); 
+  }
+  else
+  {
+    LOG_ERROR("Unable to find ServerPort attribute!"); 
+    return PLUS_FAIL; 
+  } 
+
+  const char* igtlMessageCrcCheckEnabled = trackerConfig->GetAttribute("IgtlMessageCrcCheckEnabled"); 
+  if ( igtlMessageCrcCheckEnabled != NULL )
+  {
+    if ( STRCASECMP(igtlMessageCrcCheckEnabled, "true") == 0 )
+    {
+      this->SetIgtlMessageCrcCheckEnabled(1);
+    }
+    else
+    {
+      this->SetIgtlMessageCrcCheckEnabled(0);
+    }
+  }
+  
+  return PLUS_SUCCESS;
+}
---
+++ PlusLib/src/DataCollection/PhidgetSpatialTracking/vtkPhidgetSpatialTracker.h
@@ -0,0 +1,176 @@
+/*=Plus=header=begin======================================================
+Program: Plus
+Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+See License.txt for details.
+=========================================================Plus=header=end*/
+
+#ifndef __vtkPhidgetSpatialTracker_h
+#define __vtkPhidgetSpatialTracker_h
+
+#include "vtkPlusDevice.h"
+#include <phidget21.h>
+
+class vtkPlusStreamBuffer; 
+class AhrsAlgo;
+
+/*!
+\class vtkPhidgetSpatialTracker 
+\brief Interface for the Phidget 3/3/3 tracker 
+
+This class talks with PhidgetSpatial 3/3/3 accelerometer/magnetometer/gyroscope device.
+
+Tracker coordinate system: South-West-Down.
+
+Sensor coordinate system is drawn on the sensor PCB. If the PCB is laying flat on the table
+with cable socket towards East then the axis directions are: South-West-Down.
+
+Tools:
+  Accelerometer, Gyroscope, Magnetometer: Raw sensor measurements.
+    The values are stored in the translation part of the transformation matrix.
+    The rotation part is identity.
+  TiltSensor: 2-DOF sensor tilt is computed as a rotation matrix. Only the accelerometer is used.
+  OrientationSensor: 3-DOF sensor orientation is computed using sensor fusion.
+    With ...IMU algorithm only the accelerometer and gyroscope data are used.
+    With ...AHRS algorithm accelerometer, gyroscope, and magnetometer data are used.
+
+Gyroscope zeroing is performed automatically at device connect, therefore the sensor shall not be moved
+for 2 seconds after the vtkPhidgetSpatialTracker::Connect() call if the OrientationSensor or Accelerometer tool is used.
+
+AHRS algorithm gain values:
+ The Madgwick method uses only one parameter (beta). A value of 1.0 works well at 125Hz.
+ The Mahony method uses two parameters (proportional and integral). A value of proportional=15.0, integral=1.0 work well at 125Hz.
+
+If magnetic fields nearby the sensor have non-negligible effect then compass can be ignored by choosing an ..._IMU AHRS algorithm
+(as opposed to ..._MARG) or compass correction may be performed (see http://www.phidgets.com/docs/Compass_Primer). If compass correction
+parameters cannot be stored in the device flash then they should be set in vtkPhidgetSpatialTracker::Connect().
+
+The magnetometer has an internal calibration mechanism, which is running continuously. During this internal calibration no measurement
+data can be retrieved from the magnetometer. When magnetometer data is not available then the associated tool status is set to INVALID.
+
+\ingroup PlusLibTracking
+*/
+class
+  VTK_EXPORT
+vtkPhidgetSpatialTracker : public vtkPlusDevice
+{
+public:
+
+  static vtkPhidgetSpatialTracker *New();
+  vtkTypeMacro( vtkPhidgetSpatialTracker,vtkPlusDevice );
+  void PrintSelf( ostream& os, vtkIndent indent );
+
+  virtual bool IsTracker() const { return true; }
+
+  /*! Connect to device */
+  PlusStatus Connect();
+
+  /*! Disconnect from device */
+  virtual PlusStatus Disconnect();
+
+  /*! 
+  Probe to see if the tracking system is present on the specified serial port.  
+  */
+  PlusStatus Probe();
+
+  /*!
+  Get an update from the tracking system and push the new transforms
+  to the tools.  This should only be used within vtkTracker.cxx.
+  This method is called by the tracker thread.
+  */
+  PlusStatus InternalUpdate();
+
+  /*! Read configuration from xml data */
+  virtual PlusStatus ReadConfiguration(vtkXMLDataElement* config); 
+
+  /*! Write configuration to xml data */
+  virtual PlusStatus WriteConfiguration(vtkXMLDataElement* config);
+
+  /*
+  * Override the default resettable behavior of the device
+  * because Phidget trackers can be reset
+  */
+  virtual bool IsResettable();
+
+  /*!
+    Reset the device
+  */
+  virtual PlusStatus Reset();
+
+protected:
+
+  vtkPhidgetSpatialTracker();
+  ~vtkPhidgetSpatialTracker();
+
+  /*! 
+  Start the tracking system.  The tracking system is brought from its ground state into full tracking mode.
+  The device will only be reset if communication cannot be established without a reset.
+  */
+  PlusStatus InternalStartRecording();
+
+  /*! Stop the tracking system and bring it back to its ground state: Initialized, not tracking, at 9600 Baud. */
+  PlusStatus InternalStopRecording();
+
+  static int CCONV SpatialDataHandler(CPhidgetSpatialHandle spatial, void *trackerPtr, CPhidgetSpatial_SpatialEventDataHandle *data, int count);
+
+private:  // Functions.
+
+  vtkPhidgetSpatialTracker( const vtkPhidgetSpatialTracker& );
+  void operator=( const vtkPhidgetSpatialTracker& );  
+
+private:  // Variables.  
+
+  CPhidgetSpatialHandle SpatialDeviceHandle;
+
+  unsigned int FrameNumber;
+  double TrackerTimeToSystemTimeSec; // time_System = time_Tracker + TrackerTimeToSystemTimeSec
+  bool TrackerTimeToSystemTimeComputed; // the time offset is always computed when the first frame is received after start tracking  
+
+  vtkMatrix4x4* LastAccelerometerToTrackerTransform;
+  vtkMatrix4x4* LastGyroscopeToTrackerTransform;
+  vtkMatrix4x4* LastMagnetometerToTrackerTransform;
+  vtkMatrix4x4* LastTiltSensorToTrackerTransform;
+  vtkMatrix4x4* LastOrientationSensorToTrackerTransform;
+
+  vtkSmartPointer<vtkPlusStreamTool> AccelerometerTool;
+  vtkSmartPointer<vtkPlusStreamTool> GyroscopeTool;
+  vtkSmartPointer<vtkPlusStreamTool> MagnetometerTool;
+  vtkSmartPointer<vtkPlusStreamTool> TiltSensorTool;
+  vtkSmartPointer<vtkPlusStreamTool> OrientationSensorTool;
+
+  enum AHRS_METHOD
+  {
+    AHRS_MADGWICK,
+    AHRS_MAHONY
+  };
+
+  AhrsAlgo* AhrsAlgo;
+
+  /*!
+    If AhrsUseMagnetometer enabled (a ..._MARG algorithm is chosen) then heading will be estimated using magnetometer data.
+    Otherwise (when a ..._IMU algorithm is chosen) only the gyroscope data will be used for getting the heading information.
+    IMU may be more noisy, but not sensitive to magnetic field distortions.
+  */
+  bool AhrsUseMagnetometer;
+
+  /*!
+    Gain values used by the AHRS algorithm (Mahony: first parameter is proportional, second is integral gain; Madgwick: only the first parameter is used)
+    Higher gain gives higher reliability to accelerometer&magnetometer data.
+  */
+  double AhrsAlgorithmGain[2];
+
+  /*! last AHRS update time (in system time) */
+  double AhrsLastUpdateTime;
+
+  /*!
+    In tilt sensor mode we don't use the magnetometer, so we have to provide a direction reference.
+    The orientation is specified by specifying an axis that will always point to the "West" direction.
+    Recommended values:
+    If sensor axis 0 points down (the sensor plane is about vertical) => TiltSensorDownAxisIndex = 2.
+    If sensor axis 1 points down (the sensor plane is about vertical) => TiltSensorDownAxisIndex = 0.
+    If sensor axis 2 points down (the sensor plane is about horizontal) => TiltSensorDownAxisIndex = 1.
+  */
+  int TiltSensorWestAxisIndex;
+
+};
+
+#endif
