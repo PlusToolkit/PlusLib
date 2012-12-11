d8e45a1af576586c822b88e1da21b4414a79c0b8
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-12-11 00:24:29 +0000

    Updating igt tracker to accept tdata

--- branches/tamas-sandbox/PlusLib/src/Tracking/OpenIGTLinkTracking/vtkOpenIGTLinkTracker.cxx
+++ branches/tamas-sandbox/PlusLib/src/Tracking/OpenIGTLinkTracking/vtkOpenIGTLinkTracker.cxx
@@ -1,31 +1,25 @@
 /*=Plus=header=begin======================================================
-  Program: Plus
-  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
-  See License.txt for details.
+Program: Plus
+Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+See License.txt for details.
 =========================================================Plus=header=end*/
 
 #include "PlusConfigure.h"
-
-#include "vtkOpenIGTLinkTracker.h"
-#include "vtkPlusIgtlMessageCommon.h"
-
-#include <sstream>
-
+#include "igtlMessageHeader.h"
+#include "igtlPlusClientInfoMessage.h"
+#include "igtlPositionMessage.h"
+#include "igtlTrackingDataMessage.h"
+#include "igtlTransformMessage.h"
 #include "vtkMatrix4x4.h"
 #include "vtkObjectFactory.h"
-#include "vtksys/SystemTools.hxx"
+#include "vtkOpenIGTLinkTracker.h"
+#include "vtkPlusIgtlMessageCommon.h"
+#include "vtkPlusDataBuffer.h"
+#include "vtkTrackerTool.h"
 #include "vtkTransform.h"
 #include "vtkXMLDataElement.h"
-
-#include "PlusConfigure.h"
-#include "vtkTracker.h"
-#include "vtkTrackerTool.h"
-#include "vtkPlusDataBuffer.h"
-
-#include "igtlMessageHeader.h"
-#include "igtlTransformMessage.h"
-#include "igtlPositionMessage.h"
-#include "igtlPlusClientInfoMessage.h"
+#include "vtksys/SystemTools.hxx"
+#include <sstream>
 
 static const int CLIENT_SOCKET_TIMEOUT_MSEC = 500; 
 
@@ -55,7 +49,7 @@ vtkOpenIGTLinkTracker::~vtkOpenIGTLinkTracker()
 //----------------------------------------------------------------------------
 void vtkOpenIGTLinkTracker::PrintSelf( ostream& os, vtkIndent indent )
 {
-  vtkTracker::PrintSelf( os, indent );
+  Superclass::PrintSelf( os, indent );
 }
 
 //----------------------------------------------------------------------------
@@ -108,6 +102,28 @@ PlusStatus vtkOpenIGTLinkTracker::Connect()
   // Clear buffers on connect
   this->ClearAllBuffers(); 
 
+  // If we need TDATA, request server to start streaming.
+  if ( std::string( this->MessageType ).compare( "TDATA" ) == 0 )
+  {
+    igtl::StartTrackingDataMessage::Pointer sttMsg = igtl::StartTrackingDataMessage::New();
+
+
+    sttMsg->SetDeviceName("");
+    sttMsg->SetResolution(50);
+    sttMsg->SetCoordinateName("Reference");
+    sttMsg->Pack();
+
+    int retValue = 0;
+    RETRY_UNTIL_TRUE( 
+      (retValue = this->ClientSocket->Send( sttMsg->GetPackPointer(), sttMsg->GetPackSize() ))!=0,
+      this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
+    if ( retValue == 0 )
+    {
+      LOG_ERROR("Failed to send STT_TDATA message to server!"); 
+      return PLUS_FAIL; 
+    }
+  }
+
   // If we specified message type, try to send it to the server
   if ( this->MessageType != NULL )
   {
@@ -148,6 +164,24 @@ PlusStatus vtkOpenIGTLinkTracker::Connect()
 PlusStatus vtkOpenIGTLinkTracker::Disconnect()
 {
   LOG_TRACE( "vtkOpenIGTLinkTracker::Disconnect" ); 
+
+  // If we need TDATA, request server to stop streaming.
+  if ( std::string( this->MessageType ).compare( "TDATA" ) == 0 )
+  {
+    igtl::StopTrackingDataMessage::Pointer stpMsg = igtl::StopTrackingDataMessage::New();
+    stpMsg->SetDeviceName("");
+    stpMsg->Pack();
+
+    int retValue = 0;
+    RETRY_UNTIL_TRUE( 
+      (retValue = this->ClientSocket->Send( stpMsg->GetPackPointer(), stpMsg->GetPackSize() ))!=0,
+      this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
+    if ( retValue == 0 )
+    {
+      LOG_ERROR("Failed to send STP_TDATA message to server!"); 
+      return PLUS_FAIL; 
+    } 
+  }
   this->ClientSocket->CloseSocket(); 
   return this->StopTracking(); 
 }
@@ -206,10 +240,10 @@ PlusStatus vtkOpenIGTLinkTracker::InternalUpdate()
   RETRY_UNTIL_TRUE( 
     (numOfBytesReceived = this->ClientSocket->Receive( headerMsg->GetPackPointer(), headerMsg->GetPackSize() ))!=0,
     this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
-   
+
   if ( numOfBytesReceived == 0 ) 
   {
-	// No message received - server disconnected
+    // No message received - server disconnected
     LOG_ERROR("OpenIGTLink tracker connection lost with server - try to reconnect!");
     this->ClientSocket->CloseSocket(); 
     return this->Connect(); 
@@ -229,7 +263,7 @@ PlusStatus vtkOpenIGTLinkTracker::InternalUpdate()
   igtl::Matrix4x4 igtlMatrix;
   igtl::IdentityMatrix(igtlMatrix);
   std::string igtlTransformName; 
-  
+
   headerMsg->Unpack(this->IgtlMessageCrcCheckEnabled);
   if (strcmp(headerMsg->GetDeviceType(), "TRANSFORM") == 0)
   {
@@ -253,6 +287,64 @@ PlusStatus vtkOpenIGTLinkTracker::InternalUpdate()
     toolMatrix->SetElement(1,3, position[1]); 
     toolMatrix->SetElement(2,3, position[2]); 
   }
+  else if (strcmp( headerMsg->GetDeviceType(), "TDATA" ) == 0 )
+  {
+    igtl::TrackingDataMessage::Pointer tdataMsg = igtl::TrackingDataMessage::New();
+    tdataMsg->SetMessageHeader( headerMsg );
+    tdataMsg->AllocatePack();
+    this->ClientSocket->Receive( tdataMsg->GetPackBodyPointer(), tdataMsg->GetPackBodySize() );
+
+    int c = tdataMsg->Unpack( this->IgtlMessageCrcCheckEnabled );
+
+    if ( ! ( c & igtl::MessageHeader::UNPACK_BODY ) )
+    {
+      LOG_ERROR( "Couldn't receive TDATA message from server!" );
+      return PLUS_FAIL;
+    }
+
+    for ( int i = 0; i < tdataMsg->GetNumberOfTrackingDataElements(); ++ i )
+    {
+      igtl::TrackingDataElement::Pointer tdataElem = igtl::TrackingDataElement::New();
+      tdataMsg->GetTrackingDataElement( i, tdataElem );
+      igtl::Matrix4x4 igtlMatrix;
+      tdataElem->GetMatrix(igtlMatrix);
+      vtkSmartPointer<vtkMatrix4x4> toolMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
+
+      // convert igtl matrix to vtk matrix 
+      for ( int r = 0; r < 4; r++ )
+      {
+
+        for ( int c = 0; c < 4; c++ )
+
+        {
+          toolMatrix->SetElement(r,c, igtlMatrix[r][c]); 
+        }
+      }
+      // Get timestamp 
+      igtl::TimeStamp::Pointer igtlTimestamp = igtl::TimeStamp::New(); 
+      tdataMsg->GetTimeStamp(igtlTimestamp); 
+      unfilteredTimestampUtc = igtlTimestamp->GetTimeStamp();  
+
+      // Get igtl transform name 
+      igtlTransformName = tdataElem->GetName();
+
+      // Set internal transform name
+      PlusTransformName transformName(igtlTransformName.c_str(), "Reference");
+
+      // for now just use system time, all coordinates will be sequential.
+      double unfilteredTimestamp = vtkAccurateTimer::GetSystemTime();
+      double filteredTimestamp = unfilteredTimestamp; // No need to filter already filtered timestamped items received over OpenIGTLink 
+
+      if ( this->ToolTimeStampedUpdateWithoutFiltering(transformName.From().c_str(), toolMatrix, TOOL_OK, unfilteredTimestamp, filteredTimestamp) !=
+        PLUS_SUCCESS )
+      {
+        LOG_ERROR("ToolTimeStampedUpdate failed for tool: " << transformName.From() << " with timestamp: " << std::fixed
+          << unfilteredTimestamp); 
+        // DO NOT return here: we want to update the other tools.
+      }
+    }
+    return PLUS_SUCCESS;
+  }
   else
   {
     // if the data type is unknown, skip reading. 
@@ -267,7 +359,7 @@ PlusStatus vtkOpenIGTLinkTracker::InternalUpdate()
     LOG_ERROR("Failed to update tracker tool - unrecognized transform name: " << igtlTransformName ); 
     return PLUS_FAIL; 
   }
-  
+
   // Convert timestamp from UTC to system  
   double unfilteredTimestamp = vtkAccurateTimer::GetSystemTimeFromUniversalTime(unfilteredTimestampUtc); 
 
@@ -305,7 +397,7 @@ PlusStatus vtkOpenIGTLinkTracker::ReadConfiguration( vtkXMLDataElement* config )
   if (trackerConfig == NULL) 
   {
     LOG_ERROR("Cannot find Tracker element in XML tree!");
-		return PLUS_FAIL;
+    return PLUS_FAIL;
   }
 
   const char* messageType = trackerConfig->GetAttribute("MessageType"); 
@@ -348,6 +440,6 @@ PlusStatus vtkOpenIGTLinkTracker::ReadConfiguration( vtkXMLDataElement* config )
       this->SetIgtlMessageCrcCheckEnabled(0);
     }
   }
-  
+
   return PLUS_SUCCESS;
 }
