64f0fc670ccca41bd608902c314ebf1ab55b7701
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2012-12-12 21:30:27 +0000

    Incorrect configuration for certus trackers

--- trunk/PlusLib/src/DataCollection/NDICertusTracking/vtkNDICertusTracker.cxx
+++ trunk/PlusLib/src/DataCollection/NDICertusTracking/vtkNDICertusTracker.cxx
@@ -59,7 +59,8 @@ vtkNDICertusTracker::vtkNDICertusTracker()
   this->RequireDeviceImageOrientationInDeviceSetConfiguration = false;
   this->RequireFrameBufferSizeInDeviceSetConfiguration = false;
   this->RequireAcquisitionRateInDeviceSetConfiguration = false;
-  this->RequireAveragedItemsForFilteringInDeviceSetConfiguration = true;
+  this->RequireAveragedItemsForFilteringInDeviceSetConfiguration = false;
+  this->RequireToolAveragedItemsForFilteringInDeviceSetConfiguration = true;
   this->RequireLocalTimeOffsetSecInDeviceSetConfiguration = false;
   this->RequireUsImageOrientationInDeviceSetConfiguration = false;
   this->RequireRfElementInDeviceSetConfiguration = false;
