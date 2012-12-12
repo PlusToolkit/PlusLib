fb2700d1cacede90c6badd7f13ff6cd1ba0a93ed
Adam Rankin <arankin@robarts.ca>
2012-12-12 17:22:25 +0000

    Restoring TDATA handling to OpenIGTLinkTracker

--- PlusLib/src/DataCollection/OpenIGTLinkTracking/vtkOpenIGTLinkTracker.cxx
+++ PlusLib/src/DataCollection/OpenIGTLinkTracking/vtkOpenIGTLinkTracker.cxx
@@ -5,10 +5,10 @@
 =========================================================Plus=header=end*/
 
 #include "PlusConfigure.h"
-#include "PlusConfigure.h"
 #include "igtlMessageHeader.h"
 #include "igtlPlusClientInfoMessage.h"
 #include "igtlPositionMessage.h"
+#include "igtlTrackingDataMessage.h"
 #include "igtlTransformMessage.h"
 #include "vtkMatrix4x4.h"
 #include "vtkObjectFactory.h"
@@ -111,6 +111,27 @@ PlusStatus vtkOpenIGTLinkTracker::Connect()
   // Clear buffers on connect
   this->ClearAllBuffers(); 
 
+  // If we need TDATA, request server to start streaming.
+  if ( std::string( this->MessageType ).compare( "TDATA" ) == 0 )
+  {
+    igtl::StartTrackingDataMessage::Pointer sttMsg = igtl::StartTrackingDataMessage::New();
+    sttMsg->SetDeviceName("");
+    sttMsg->SetResolution(50);
+    sttMsg->SetCoordinateName("Reference");
+    sttMsg->Pack();
+
+    int retValue = 0;
+    RETRY_UNTIL_TRUE( 
+      (retValue = this->ClientSocket->Send( sttMsg->GetPackPointer(), sttMsg->GetPackSize() ))!=0,
+      this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
+
+    if ( retValue == 0 )
+    {
+      LOG_ERROR("Failed to send STT_TDATA message to server!"); 
+      return PLUS_FAIL; 
+    }
+  }
+
   // If we specified message type, try to send it to the server
   if ( this->MessageType != NULL )
   {
@@ -151,6 +172,26 @@ PlusStatus vtkOpenIGTLinkTracker::Connect()
 PlusStatus vtkOpenIGTLinkTracker::Disconnect()
 {
   LOG_TRACE( "vtkOpenIGTLinkTracker::Disconnect" ); 
+
+  // If we need TDATA, request server to stop streaming.
+  if ( std::string( this->MessageType ).compare( "TDATA" ) == 0 )
+  {
+    igtl::StopTrackingDataMessage::Pointer stpMsg = igtl::StopTrackingDataMessage::New();
+    stpMsg->SetDeviceName("");
+    stpMsg->Pack();
+
+    int retValue = 0;
+    RETRY_UNTIL_TRUE( 
+      (retValue = this->ClientSocket->Send( stpMsg->GetPackPointer(), stpMsg->GetPackSize() ))!=0,
+      this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
+
+    if ( retValue == 0 )
+    {
+      LOG_ERROR("Failed to send STP_TDATA message to server!"); 
+      return PLUS_FAIL; 
+    } 
+  }
+
   this->ClientSocket->CloseSocket(); 
   return this->StopRecording(); 
 }
@@ -259,6 +300,60 @@ PlusStatus vtkOpenIGTLinkTracker::InternalUpdate()
     toolMatrix->SetElement(0,3, position[0]); 
     toolMatrix->SetElement(1,3, position[1]); 
     toolMatrix->SetElement(2,3, position[2]); 
+  }
+  else if (strcmp( headerMsg->GetDeviceType(), "TDATA" ) == 0 )
+  {
+    igtl::TrackingDataMessage::Pointer tdataMsg = igtl::TrackingDataMessage::New();
+    tdataMsg->SetMessageHeader( headerMsg );
+    tdataMsg->AllocatePack();
+
+    this->ClientSocket->Receive( tdataMsg->GetPackBodyPointer(), tdataMsg->GetPackBodySize() );
+    int c = tdataMsg->Unpack( this->IgtlMessageCrcCheckEnabled );
+    if ( ! ( c & igtl::MessageHeader::UNPACK_BODY ) )
+    {
+      LOG_ERROR( "Couldn't receive TDATA message from server!" );
+      return PLUS_FAIL;
+    }
+
+    for ( int i = 0; i < tdataMsg->GetNumberOfTrackingDataElements(); ++ i )
+    {
+      igtl::TrackingDataElement::Pointer tdataElem = igtl::TrackingDataElement::New();
+      tdataMsg->GetTrackingDataElement( i, tdataElem );
+
+      igtl::Matrix4x4 igtlMatrix;
+      tdataElem->GetMatrix(igtlMatrix);
+      vtkSmartPointer<vtkMatrix4x4> toolMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
+      // convert igtl matrix to vtk matrix 
+      for ( int r = 0; r < 4; r++ )
+      {
+        for ( int c = 0; c < 4; c++ )
+        {
+          toolMatrix->SetElement(r,c, igtlMatrix[r][c]); 
+        }
+      }
+
+      // Get timestamp 
+      igtl::TimeStamp::Pointer igtlTimestamp = igtl::TimeStamp::New(); 
+      tdataMsg->GetTimeStamp(igtlTimestamp); 
+      unfilteredTimestampUtc = igtlTimestamp->GetTimeStamp();  
+
+      // Get igtl transform name 
+      igtlTransformName = tdataElem->GetName();
+
+      // Set internal transform name
+      PlusTransformName transformName(igtlTransformName.c_str(), "Reference");
+
+      // for now just use system time, all coordinates will be sequential.
+      double unfilteredTimestamp = vtkAccurateTimer::GetSystemTime();
+
+      double filteredTimestamp = unfilteredTimestamp; // No need to filter already filtered timestamped items received over OpenIGTLink 
+      if ( this->ToolTimeStampedUpdateWithoutFiltering(transformName.From().c_str(), toolMatrix, TOOL_OK, unfilteredTimestamp, filteredTimestamp) != PLUS_SUCCESS )
+      {
+        LOG_ERROR("ToolTimeStampedUpdate failed for tool: " << transformName.From() << " with timestamp: " << std::fixed << unfilteredTimestamp); 
+        // DO NOT return here: we want to update the other tools.
+      }
+    }
+    return PLUS_SUCCESS;
   }
   else
   {
