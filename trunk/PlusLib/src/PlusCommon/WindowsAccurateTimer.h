7858b6d0f697a35d57179655a6dcbfac5c60b922
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2013-05-22 18:51:07 +0000

    test #762 avoiding race condition by forcing instantiation of plus logger earlier in startup sequence

--- trunk/PlusLib/src/PlusCommon/WindowsAccurateTimer.h
+++ trunk/PlusLib/src/PlusCommon/WindowsAccurateTimer.h
@@ -214,6 +214,7 @@ private:
     m_HalfMsec.QuadPart = m_Freq.QuadPart / 2000;
     m_Freq.QuadPart /= 1000;   // convert to msecs
     InitializeCriticalSection(&m_CriticalSection);
+    vtkPlusLogger::Instance();
     m_Timer = timeSetEvent(1, 0, TimerFunc, (DWORD_PTR)this, TIME_PERIODIC);
     m_Initialized=false;
   }
