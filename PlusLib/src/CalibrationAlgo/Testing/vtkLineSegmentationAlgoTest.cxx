05d9525ddcf46664b802f8961b6938aa0909afb5
Andras Lasso <lasso@queensu.ca>
2013-06-03 13:34:21 +0000

    re #772: Cleaned up vtkLineSegmentationAlgoTest (separated results reading, writing, and comparison)

--- PlusLib/src/CalibrationAlgo/Testing/vtkLineSegmentationAlgoTest.cxx
+++ PlusLib/src/CalibrationAlgo/Testing/vtkLineSegmentationAlgoTest.cxx
@@ -23,6 +23,184 @@ compares the results to a baseline
 const double MAX_ORIGIN_DISTANCE_PIXEL=10;
 const double MAX_LINE_ANGLE_DIFFERENCE_DEG=3;
 
+//----------------------------------------------------------------------------
+void WriteLineSegmentationResultsToFile(const std::string& resultSaveFilename, const std::vector<vtkLineSegmentationAlgo::LineParameters> &lineParameters)
+{
+  std::ofstream outFile; 
+  outFile.open(resultSaveFilename.c_str());	
+  outFile << "<LineSegmentationResults>" << std::endl;
+  for ( unsigned int frameIndex = 0; frameIndex < lineParameters.size(); ++frameIndex )
+  {
+    outFile << "  <Frame Index=\"" <<frameIndex<<"\" SegmentationStatus=\"";
+    if (lineParameters[frameIndex].lineDetected)
+    {
+      outFile << "OK\" "
+        << std::fixed << std::setprecision(8)
+        << "LineOriginPointPx=\""<<lineParameters[frameIndex].lineOriginPoint_Image[0]<<" "<<lineParameters[frameIndex].lineOriginPoint_Image[1]<<"\" "
+        << "LineDirectionVectorPx=\""<<lineParameters[frameIndex].lineDirectionVector_Image[0]<<" "<<lineParameters[frameIndex].lineDirectionVector_Image[1]<<"\" ";
+    }
+    else
+    {
+      outFile << "Failed\"";
+    }
+    outFile << " />" << std::endl;      
+  }
+  outFile << "</LineSegmentationResults>" << std::endl;  
+  outFile.close(); 
+}
+
+//----------------------------------------------------------------------------
+PlusStatus ReadLineSegmentationResultsFromFile(const std::string& resultSaveFilename, std::vector<vtkLineSegmentationAlgo::LineParameters> &lineParameters)
+{
+  lineParameters.clear();
+  if (resultSaveFilename.empty())
+  {
+    LOG_ERROR("Cannot read line segmentation results, filename is empty");
+    return PLUS_FAIL;
+  }
+  vtkSmartPointer<vtkXMLDataElement> resultsElem = vtkSmartPointer<vtkXMLDataElement>::Take(vtkXMLUtilities::ReadElementFromFile(resultSaveFilename.c_str()));      
+  if ( resultsElem == NULL )
+  {
+    LOG_ERROR("Failed to read baseline file: "<< resultSaveFilename);
+    return PLUS_FAIL;
+  }
+  if (resultsElem->GetName()==NULL || STRCASECMP( resultsElem->GetName(), "LineSegmentationResults" ) != 0)
+  {
+    LOG_ERROR("Unable to find LineSegmentationResults XML data element in baseline: " << resultSaveFilename); 
+    return PLUS_FAIL;
+  }
+  for ( int childElemIndex = 0; childElemIndex < resultsElem->GetNumberOfNestedElements(); ++childElemIndex)
+  {
+    vtkXMLDataElement* frameElem = resultsElem->GetNestedElement(childElemIndex); 
+    if ( frameElem==NULL || frameElem->GetName()==NULL || STRCASECMP( frameElem->GetName(), "Frame" ) != 0 )
+    {
+      LOG_ERROR("Invalid child element in LineSegmentationResults: #"<<childElemIndex);
+      return PLUS_FAIL;
+    }
+    int frameIndex=0;
+    if ( !frameElem->GetScalarAttribute( "Index", frameIndex) )
+    {
+      LOG_ERROR("Unable to find Index element in LineSegmentationResults: child index "<<childElemIndex);
+      return PLUS_FAIL;
+    }
+
+    vtkLineSegmentationAlgo::LineParameters currentLineParams;
+
+    const char* baselineSegmentationStatusString = frameElem->GetAttribute("SegmentationStatus");  
+    if (baselineSegmentationStatusString==NULL)
+    {
+      LOG_ERROR("SegmentationStatus is not available in the baseline for frame "<<frameIndex);
+      return PLUS_FAIL;
+    }
+    currentLineParams.lineDetected=!STRCASECMP(baselineSegmentationStatusString, "OK");
+
+    if (currentLineParams.lineDetected)
+    {
+      double baselineLineOriginPoint_Image[2]={0,0};
+      if ( !frameElem->GetVectorAttribute( "LineOriginPointPx", 2, currentLineParams.lineOriginPoint_Image) )
+      {
+        LOG_ERROR("Unable to find LineOriginPointPx element in LineSegmentationResults for frame "<<frameIndex);
+        return PLUS_FAIL;
+      }
+      if ( !frameElem->GetVectorAttribute( "LineDirectionVectorPx", 2, currentLineParams.lineDirectionVector_Image) )
+      {
+        LOG_ERROR("Unable to find LineDirectionVectorPx element in LineSegmentationResults for frame "<<frameIndex);
+        return PLUS_FAIL;
+      }
+    }
+    else
+    {
+      currentLineParams.lineOriginPoint_Image[0]=0;
+      currentLineParams.lineOriginPoint_Image[1]=0;
+      currentLineParams.lineDirectionVector_Image[0]=0;
+      currentLineParams.lineDirectionVector_Image[1]=1;
+    }
+
+    if (frameIndex>=lineParameters.size())
+    {
+      // expand the results array to be able to store the results
+      vtkLineSegmentationAlgo::LineParameters nonDetectedLineParams;
+      nonDetectedLineParams.lineDetected=false;
+      nonDetectedLineParams.lineOriginPoint_Image[0]=0;
+      nonDetectedLineParams.lineOriginPoint_Image[1]=0;
+      nonDetectedLineParams.lineDirectionVector_Image[0]=0;
+      nonDetectedLineParams.lineDirectionVector_Image[1]=1;
+      lineParameters.insert( lineParameters.end(), frameIndex-lineParameters.size()+1, nonDetectedLineParams);
+    }
+    lineParameters[frameIndex]=currentLineParams;
+  }
+
+  return PLUS_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+int CompareLineSegmentationResults(const std::vector<vtkLineSegmentationAlgo::LineParameters> &lineParameters, const std::vector<vtkLineSegmentationAlgo::LineParameters> &baselineLineParameters)
+{
+  int numberOfFailures=0;
+
+  for ( int frameIndex = 0; frameIndex < lineParameters.size(); ++frameIndex)
+  {
+    LOG_DEBUG("Comparing frame "<<frameIndex);
+    if ( frameIndex>=baselineLineParameters.size() )
+    {
+      LOG_ERROR("Unable to find frame "<<frameIndex<<" in LineSegmentationResults baseline");
+      numberOfFailures++;
+      continue;
+    }
+
+    vtkLineSegmentationAlgo::LineParameters currentParam=lineParameters[frameIndex];
+    vtkLineSegmentationAlgo::LineParameters baselineParam=baselineLineParameters[frameIndex];
+
+    if (currentParam.lineDetected!=baselineParam.lineDetected)
+    {
+      LOG_ERROR("SegmentationStatus mismatch in Frame #" << frameIndex << ": current=" << currentParam.lineDetected << ", baseline=" << baselineParam.lineDetected);
+      ++numberOfFailures;
+      continue;
+    }
+
+    LOG_DEBUG(" Line detection status: "<<(currentParam.lineDetected?"detected":"not detected"));
+    if (!currentParam.lineDetected)
+    {
+      // no segmentation data
+      continue;
+    }
+
+    // Compare origin to baseline 
+    double baselineOrigin3d[3]={baselineParam.lineOriginPoint_Image[0],baselineParam.lineOriginPoint_Image[1],0};
+    double currentLinePoint1[3]={currentParam.lineOriginPoint_Image[0],currentParam.lineOriginPoint_Image[1],0};
+    const double lineLen=50; // pick a second point along the line at this distance, any positive number would do
+    double currentLinePoint2[3]={currentLinePoint1[0]+currentParam.lineDirectionVector_Image[0]*lineLen,
+      currentLinePoint1[1]+currentParam.lineDirectionVector_Image[1]*lineLen,0};
+    double distanceOfBaselineOriginFromCurrentLinePx=PlusMath::ComputeDistanceLinePoint(currentLinePoint1, currentLinePoint2, baselineOrigin3d);
+    if (distanceOfBaselineOriginFromCurrentLinePx>MAX_ORIGIN_DISTANCE_PIXEL)
+    {
+      LOG_ERROR("Line position mismatch in Frame #" << frameIndex << ": baseline origin point distance from current line is "<<distanceOfBaselineOriginFromCurrentLinePx<<" pixels");
+      numberOfFailures++;
+    }
+    else
+    {
+      LOG_DEBUG(" Line distance: "<<distanceOfBaselineOriginFromCurrentLinePx<<" pixels");
+    }
+
+    // Compare direction to baseline 
+    double baselineVec3d[3]={baselineParam.lineDirectionVector_Image[0],baselineParam.lineDirectionVector_Image[1],0};
+    double currentVec3d[3]={currentParam.lineDirectionVector_Image[0],currentParam.lineDirectionVector_Image[1],0};
+    double angleDeg = acos(vtkMath::Dot(baselineVec3d,currentVec3d));
+    if (angleDeg>MAX_LINE_ANGLE_DIFFERENCE_DEG)
+    {
+      LOG_ERROR("Line angle mismatch in Frame #" << frameIndex << ": angle difference is "<<angleDeg<<" deg, vector coordinates: "
+        <<"current=(" << std::fixed << currentParam.lineDirectionVector_Image[0] << ", " << currentParam.lineDirectionVector_Image[1] << ") "
+        <<"baseline=(" << baselineParam.lineDirectionVector_Image[0] << ", " << baselineParam.lineDirectionVector_Image[1] << ").");
+      numberOfFailures++;
+    }
+    else
+    {
+      LOG_DEBUG(" Line angle difference is "<<angleDeg<<" deg");
+    }
+  }
+
+  return numberOfFailures;
+}
 
 //----------------------------------------------------------------------------
 int main(int argc, char **argv)
@@ -70,6 +248,7 @@ int main(int argc, char **argv)
     exit(EXIT_FAILURE);
   }  
 
+  LOG_DEBUG("Read input sequence");
   vtkSmartPointer<vtkTrackedFrameList> trackedFrameList = vtkSmartPointer<vtkTrackedFrameList>::New(); 
   if ( trackedFrameList->ReadFromSequenceMetafile(inputSequenceMetafile.c_str()) != PLUS_SUCCESS )
   {
@@ -96,6 +275,7 @@ int main(int argc, char **argv)
   lineSegmenter->SetSaveIntermediateImages(saveImages);
   lineSegmenter->SetIntermediateFilesOutputDirectory(vtkPlusConfig::GetInstance()->GetOutputDirectory());
 
+  LOG_DEBUG("Segment lines");
   if ( lineSegmenter->Update() != PLUS_SUCCESS )
   {
     LOG_ERROR("Failed to get line positions from video frames");
@@ -104,146 +284,26 @@ int main(int argc, char **argv)
   std::vector<vtkLineSegmentationAlgo::LineParameters> lineParameters;
   lineSegmenter->GetDetectedLineParameters(lineParameters);
 
-  //*********************************************************************
   // Save results to file
-
   std::string resultSaveFilename=vtkPlusConfig::GetInstance()->GetOutputPath("LineSegmentationResults.xml");
   LOG_INFO("Save calibration results to XML file: "<<resultSaveFilename); 
-  std::ofstream outFile; 
-  outFile.open(resultSaveFilename.c_str());	
-  outFile << "<LineSegmentationResults>" << std::endl;
-  for ( unsigned int frameIndex = 0; frameIndex < lineParameters.size(); ++frameIndex )
-  {
-    outFile << "  <Frame Index=\"" <<frameIndex<<"\" SegmentationStatus=\"";
-    if (lineParameters[frameIndex].lineDetected)
-    {
-      outFile << "OK\" "
-        << std::fixed << std::setprecision(8)
-        << "LineOriginPointPx=\""<<lineParameters[frameIndex].lineOriginPoint_Image[0]<<" "<<lineParameters[frameIndex].lineOriginPoint_Image[1]<<"\" "
-        << "LineDirectionVectorPx=\""<<lineParameters[frameIndex].lineDirectionVector_Image[0]<<" "<<lineParameters[frameIndex].lineDirectionVector_Image[1]<<"\" ";
+  WriteLineSegmentationResultsToFile(resultSaveFilename, lineParameters);
 
-    }
-    else
-    {
-      outFile << "Failed\"";
-    }
-    outFile << " />" << std::endl;      
-  }
-  outFile << "</LineSegmentationResults>" << std::endl;  
-  outFile.close(); 
-
-  //*********************************************************************
   // Compare result to baseline
-
   if (!inputBaselineFileName.empty())
   {
     LOG_INFO("Comparing result with baseline..."); 
-
-    vtkSmartPointer<vtkXMLDataElement> resultsElem = vtkSmartPointer<vtkXMLDataElement>::Take(vtkXMLUtilities::ReadElementFromFile(inputBaselineFileName.c_str()));      
-    if ( resultsElem == NULL )
+    std::vector<vtkLineSegmentationAlgo::LineParameters> baselineLineParameters;
+    if (ReadLineSegmentationResultsFromFile(inputBaselineFileName, baselineLineParameters)!=PLUS_SUCCESS)
     {
-      LOG_ERROR("Failed to read baseline file: "<< inputBaselineFileName);
+      LOG_ERROR("Failed to read baseline data file");
       exit(EXIT_FAILURE);
     }
-    if (resultsElem->GetName()==NULL || STRCASECMP( resultsElem->GetName(), "LineSegmentationResults" ) != 0)
+    int numberOfFailures=CompareLineSegmentationResults(lineParameters, baselineLineParameters);
+    if (numberOfFailures>0)
     {
-      LOG_ERROR("Unable to find LineSegmentationResults XML data element in baseline: " << inputBaselineFileName); 
+      LOG_ERROR("Number of differences compared to baseline: "<<numberOfFailures<<". Test failed!");
       exit(EXIT_FAILURE);
-    }
-
-    for ( int childElemIndex = 0; childElemIndex < resultsElem->GetNumberOfNestedElements(); ++childElemIndex)
-    {
-      vtkXMLDataElement* baselineFrameElem = resultsElem->GetNestedElement(childElemIndex); 
-      if ( baselineFrameElem==NULL || baselineFrameElem->GetName()==NULL || STRCASECMP( baselineFrameElem->GetName(), "Frame" ) != 0 )
-      {
-        LOG_ERROR("Invalid child element in LineSegmentationResults: #"<<childElemIndex);
-        ++numberOfFailures;
-        continue;
-      }
-
-      int baselineFrameIndex=0;
-      if ( !baselineFrameElem->GetScalarAttribute( "Index", baselineFrameIndex) )
-      {
-        LOG_ERROR("Unable to find Index element in LineSegmentationResults: child index "<<childElemIndex);
-        numberOfFailures++; 
-        continue;
-      }
-
-      const char* baselineSegmentationStatusString = baselineFrameElem->GetAttribute("SegmentationStatus");  
-      if (baselineSegmentationStatusString ==NULL)
-      {
-        LOG_ERROR("SegmentationStatus is not available in the baseline for frame "<<baselineFrameIndex);
-        numberOfFailures++; 
-        continue;
-      }
-      bool baselineSegmentationStatus=!STRCASECMP(baselineSegmentationStatusString, "OK");
-      if (baselineSegmentationStatus!=lineParameters[baselineFrameIndex].lineDetected)
-      {
-        LOG_ERROR("SegmentationStatus mismatch in Frame #" << baselineFrameIndex 
-          << ": current=" << lineParameters[baselineFrameIndex].lineDetected 
-          << ", baseline=" << baselineSegmentationStatus);
-        ++numberOfFailures;
-        continue;
-      }
-
-      if (!baselineSegmentationStatus)
-      {
-        // no segmentation data
-        continue;
-      }
-
-      // Compare origin to baseline 
-      double baselineLineOriginPoint_Image[2]={0,0};
-      if ( !baselineFrameElem->GetVectorAttribute( "LineOriginPointPx", 2, baselineLineOriginPoint_Image) )
-      {
-        LOG_ERROR("Unable to find LineOriginPointPx element in LineSegmentationResults: frame "<<baselineFrameIndex);
-        numberOfFailures++; 
-      }
-      else
-      {
-
-        double baselineOrigin3d[3]={baselineLineOriginPoint_Image[0],baselineLineOriginPoint_Image[1],0};
-        double currentLinePoint1[3]={lineParameters[baselineFrameIndex].lineOriginPoint_Image[0],lineParameters[baselineFrameIndex].lineOriginPoint_Image[1],0};
-        const double lineLen=50; // any positive number would do, just pick a second point along the line at this distance
-        double currentLinePoint2[3]={currentLinePoint1[0]+lineParameters[baselineFrameIndex].lineDirectionVector_Image[0]*lineLen,
-          currentLinePoint1[1]+lineParameters[baselineFrameIndex].lineDirectionVector_Image[1]*lineLen,0};
-        double dist=PlusMath::ComputeDistanceLinePoint(currentLinePoint1, currentLinePoint2, baselineOrigin3d);
-        if (dist>MAX_ORIGIN_DISTANCE_PIXEL)
-        {
-          LOG_ERROR("LineOriginPointPx differ from baseline for frame "<<baselineFrameIndex<<": distance="<<dist<<" pixels "
-            <<"current=(" << std::fixed << lineParameters[baselineFrameIndex].lineOriginPoint_Image[0] << ", " << lineParameters[baselineFrameIndex].lineOriginPoint_Image[1] << ") "
-            <<"baseline=(" << baselineLineOriginPoint_Image[0] << ", " << baselineLineOriginPoint_Image[1] << ").");
-          numberOfFailures++;
-        }
-      }
-
-      // Compare direction to baseline 
-      double baselineLineDirectionVector_Image[2]={0,1};
-      if ( !baselineFrameElem->GetVectorAttribute( "LineDirectionVectorPx", 2, baselineLineDirectionVector_Image) )
-      {
-        LOG_ERROR("Unable to find LineDirectionVectorPx element in LineSegmentationResults: frame "<<baselineFrameIndex);
-        numberOfFailures++; 
-      }
-      else
-      {
-        double baselineVec3d[3]={baselineLineDirectionVector_Image[0],baselineLineDirectionVector_Image[1],0};
-        double currentVec3d[3]={lineParameters[baselineFrameIndex].lineDirectionVector_Image[0],lineParameters[baselineFrameIndex].lineDirectionVector_Image[1],0};
-        double angleDeg = acos(vtkMath::Dot(baselineVec3d,currentVec3d));
-        if (angleDeg>MAX_LINE_ANGLE_DIFFERENCE_DEG)
-        {
-          LOG_ERROR("LineDirectionVectorPx differ from baseline for frame "<<baselineFrameIndex<<": angle difference is "<<angleDeg<<" deg, vector coordinates: "
-            <<"current=(" << std::fixed << lineParameters[baselineFrameIndex].lineDirectionVector_Image[0] << ", " << lineParameters[baselineFrameIndex].lineDirectionVector_Image[1] << ") "
-            <<"baseline=(" << baselineLineDirectionVector_Image[0] << ", " << baselineLineDirectionVector_Image[1] << ").");
-          numberOfFailures++;
-        }
-      }
-
-    }
-
-    if ( numberOfFailures > 0 ) 
-    {
-      LOG_INFO("Test failed!"); 
-      return EXIT_FAILURE; 
     }
   }
 
