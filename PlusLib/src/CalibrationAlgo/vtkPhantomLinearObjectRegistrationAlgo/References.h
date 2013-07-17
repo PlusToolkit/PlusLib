ee65deb5146f585d1174ef8cd625752ec91cfb09
RyanWalsh <RyanWalsh@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2013-07-17 21:34:26 +0000

    re #787: added concept of reference points to vtkLinearObjectRegistrationAlgo and the fCal 2.0 config files to port Matthew's current algorithm
    future plans are to eliminate reference points from the algorithm entirely

---
+++ PlusLib/src/CalibrationAlgo/vtkPhantomLinearObjectRegistrationAlgo/References.h
@@ -0,0 +1,34 @@
+/*=Plus=header=begin======================================================
+Program: Plus
+Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+See License.txt for details.
+=========================================================Plus=header=end*/ 
+
+#ifndef REFERENCES_H
+#define REFERENCES_H
+
+#include "Reference.h"
+#include <vector>
+
+class References
+{
+public:
+  References();
+  ~References();
+
+  void Reset();
+  void InsertReference(const Reference& newReference);
+  void RemoveReference(int index);
+  const Reference& GetReference(int index);
+  int GetNumberOfReferences();
+
+  std::vector<Reference>::const_iterator ReferencesBegin() const;
+  std::vector<Reference>::const_iterator ReferencesEnd() const;
+
+  References& operator=(const References& rhs);
+
+protected:
+  std::vector<Reference> m_ReferenceList;
+};
+
+#endif
