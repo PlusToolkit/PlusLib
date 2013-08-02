e158b6c0011a4b7f6c2f9fc976e1331616d6a410
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2013-08-02 16:00:52 +0000

    re #707: Optimized the matlab mha file reader to decrease file loading time by about 50%

--- trunk/sandbox/MatlabUtils/mha_read_transforms.m
+++ trunk/sandbox/MatlabUtils/mha_read_transforms.m
@@ -34,61 +34,65 @@ while 1
     break
   end
 
-  token=regexp(tline, 'DimSize = (.*)', 'tokens');
+  % Detect ...Transform and ...TransformStatus fields with one regexp to
+  % reduce parsing time
+  token=regexp(tline, 'Seq_Frame([0-9]*)_(.*)Transform((|Status)) = (.*)', 'tokens');
   if (length(token)>0)
-    dimSize=str2num(token{1}{1});
-    numberOfFrames=dimSize(3);
-    % Initialize arrays to their final sizes, because concatenation for each frame would take a very long time
-    timestamps=zeros(numberOfFrames,1);
-    unfilteredTimestamps=zeros(numberOfFrames,1);
+      frameIndex=sscanf(token{1}{1},'%d');
+      transformName=token{1}{2};
+      if (isempty(token{1}{3}))
+        % Transform      
+          transformFieldName=[transformName 'TransformMatrix'];
+          if isfield(transforms,transformFieldName)==0
+            % Initialize the array to its final size, because concatenation for each frame would take a very long time
+            transforms.(transformFieldName)=zeros(4,4,numberOfFrames);
+          end
+          transforms.(transformFieldName)(:,:,frameIndex+1)=reshape(sscanf(token{1}{4},'%f'),4,4)';
+      else
+        % TransformStatus
+        transformStatusFieldName=[transformName 'TransformStatus'];
+        transformStatus=strtrim(token{1}{4});
+        if (strcmp(transformStatus,'OK'))
+          statusCode=1;
+        else
+          statusCode=0;
+        end
+        if isfield(transforms,transformStatusFieldName)==0
+            % Initialize the array to its final size, because concatenation for each frame would take a very long time
+            transforms.(transformStatusFieldName)=zeros(numberOfFrames,1);
+        end
+        transforms.(transformStatusFieldName)(frameIndex+1)=statusCode;          
+      end
+      continue;      
   end
 
   token=regexp(tline, 'Seq_Frame([0-9]*)_UnfilteredTimestamp = (.*)', 'tokens');
   if (length(token)>0)
-    frameIndex=str2num(token{1}{1});
-    n=str2num(token{1}{2});
+    frameIndex=sscanf(token{1}{1},'%d');
+    n=sscanf(token{1}{2},'%f');
     unfilteredTimestamps(frameIndex+1)=n;
+    continue;
   end
 
   token=regexp(tline, 'Seq_Frame([0-9]*)_Timestamp = (.*)', 'tokens');
   if (length(token)>0)
-    frameIndex=str2num(token{1}{1});
-    n=str2num(token{1}{2});
+    frameIndex=sscanf(token{1}{1},'%d');
+    n=sscanf(token{1}{2},'%f');
     timestamps(frameIndex+1)=n;
     if verbose && (mod(frameIndex,100)==0 || frameIndex+1==numberOfFrames)
       disp(['Reading... (' num2str(round(frameIndex/numberOfFrames*100)) '%)']);
     end
+    continue;
   end
-
-  token=regexp(tline, 'Seq_Frame([0-9]*)_(.*)Transform = (.*)', 'tokens');
+  
+  token=regexp(tline, 'DimSize = (.*)', 'tokens');
   if (length(token)>0)
-      frameIndex=str2num(token{1}{1});
-      transformName=token{1}{2};
-      transformMatrix=reshape(str2num(token{1}{3}),4,4)';
-      transformFieldName=[transformName 'TransformMatrix'];
-      if isfield(transforms,transformFieldName)==0
-        % Initialize the array to its final size, because concatenation for each frame would take a very long time
-        transforms.(transformFieldName)=zeros(4,4,numberOfFrames);
-      end
-      transforms.(transformFieldName)(:,:,frameIndex+1)=transformMatrix;
-  end
-
-  token=regexp(tline, 'Seq_Frame([0-9]*)_(.*)TransformStatus = (.*)', 'tokens');
-  if (length(token)>0)
-    frameIndex=str2num(token{1}{1});
-    transformName=token{1}{2};
-    transformStatusFieldName=[transformName 'TransformStatus'];
-    transformStatus=strtrim(token{1}{3});
-    if (strcmp(transformStatus,'OK'))
-      statusCode=1;
-    else
-      statusCode=0;
-    end
-    if isfield(transforms,transformStatusFieldName)==0
-        % Initialize the array to its final size, because concatenation for each frame would take a very long time
-        transforms.(transformStatusFieldName)=zeros(numberOfFrames,1);
-    end
-    transforms.(transformStatusFieldName)(frameIndex+1)=statusCode;
+    dimSize=sscanf(token{1}{1},'%d');
+    numberOfFrames=dimSize(3);
+    % Initialize arrays to their final sizes, because concatenation for each frame would take a very long time
+    timestamps=zeros(numberOfFrames,1);
+    unfilteredTimestamps=zeros(numberOfFrames,1);
+    continue;
   end
 
   if (regexp(tline, 'ElementDataFile')>0)
@@ -98,4 +102,3 @@ while 1
 end
 
 fclose(fid);
-
