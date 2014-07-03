17009839f0d16a6ecbaf7776c56cb0987c0fe8f9
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2014-07-03 17:17:33 +0000

    re #912: Moved common parts of OpenIGTLink tracker and video acquisition devices to a common vtkOpenIGTLinkDevice class

--- trunk/PlusLib/src/DataCollection/OpenIGTLink/vtkOpenIGTLinkVideoSource.cxx
+++ trunk/PlusLib/src/DataCollection/OpenIGTLink/vtkOpenIGTLinkVideoSource.cxx
@@ -7,11 +7,9 @@ See License.txt for details.
 #include "PlusConfigure.h"
 #include "vtkOpenIGTLinkVideoSource.h"
 
+#include "igtlImageMessage.h"
 #include "PlusVideoFrame.h"
 #include "TrackedFrame.h"
-#include "igtlImageMessage.h"
-#include "igtlMessageHeader.h"
-#include "igtlPlusClientInfoMessage.h"
 #include "vtkImageData.h"
 #include "vtkObjectFactory.h"
 #include "vtkPlusBuffer.h"
@@ -19,110 +17,25 @@ See License.txt for details.
 #include "vtkPlusDataSource.h"
 #include "vtkPlusIgtlMessageCommon.h"
 
-#include "vtksys/SystemTools.hxx"
-
-#include <vector>
-#include <string>
-
-static const int CLIENT_SOCKET_TIMEOUT_MSEC = 500; 
-
 vtkStandardNewMacro(vtkOpenIGTLinkVideoSource);
-
-#ifdef _WIN32
-  #include <Winsock2.h>
-#endif
 
 //----------------------------------------------------------------------------
 vtkOpenIGTLinkVideoSource::vtkOpenIGTLinkVideoSource()
-: MessageType(NULL)
-, ServerAddress(NULL)
-, ServerPort(-1)
-, NumberOfRetryAttempts(10)
-, DelayBetweenRetryAttemptsSec(0.100) // there is already a delay with a CLIENT_SOCKET_TIMEOUT_MSEC timeout, so we just add a little extra idle delay
-, IgtlMessageCrcCheckEnabled(0)
-, ClientSocket(igtl::ClientSocket::New())
-, ReconnectOnReceiveTimeout(true)
-, UseReceivedTimestamps(true)
 {
   this->RequireImageOrientationInConfiguration = true;
 
-  // No callback function provided by the device, so the data capture thread will be used to poll the hardware and add new items to the buffer
-  this->StartThreadForInternalUpdates=true;
-  this->AcquisitionRate = 30;
 }
 
 //----------------------------------------------------------------------------
 vtkOpenIGTLinkVideoSource::~vtkOpenIGTLinkVideoSource()
 {
-  if ( this->Recording )
-  {
-    this->StopRecording();
-  }
-  this->ClientSocket = NULL;
 }
 
 //----------------------------------------------------------------------------
 void vtkOpenIGTLinkVideoSource::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
-  if ( this->ServerAddress )
-  {
-    os << indent << "Server address: " << this->ServerAddress << "\n";
-  }
-  os << indent << "Server port: " << this->ServerPort << "\n";
-  if ( this->MessageType )
-  {
-    os << indent << "Message type: " << this->ServerAddress << "\n";
-  }
-
 }
-//----------------------------------------------------------------------------
-std::string vtkOpenIGTLinkVideoSource::GetSdkVersion()
-{
-  std::ostringstream version; 
-  version << "OpenIGTLink v" << OPENIGTLINK_VERSION; 
-  return version.str(); 
-}
-
-//----------------------------------------------------------------------------
-PlusStatus vtkOpenIGTLinkVideoSource::InternalConnect()
-{
-  LOG_TRACE( "vtkOpenIGTLinkVideoSource::InternalConnect" );
-
-  // Clear buffers on connect
-  this->ClearAllBuffers();   
-
-  if ( this->ClientSocket->GetConnected() )
-  {
-    return PLUS_SUCCESS; 
-  }
-
-  return ClientSocketReconnect();
-}
-
-//----------------------------------------------------------------------------
-PlusStatus vtkOpenIGTLinkVideoSource::InternalDisconnect()
-{
-  LOG_TRACE( "vtkOpenIGTLinkVideoSource::Disconnect" ); 
-
-  this->ClientSocket->CloseSocket(); 
-  return this->StopRecording();
-}
-
-//----------------------------------------------------------------------------
-PlusStatus vtkOpenIGTLinkVideoSource::Probe()
-{
-  LOG_TRACE( "vtkOpenIGTLinkVideoSource::Probe" ); 
-
-  PlusStatus status = PLUS_FAIL; 
-  if ( this->Connect() == PLUS_SUCCESS )
-  {
-    status = PLUS_SUCCESS; 
-    this->Disconnect(); 
-  }
-  
-  return status; 
-} 
 
 //----------------------------------------------------------------------------
 PlusStatus vtkOpenIGTLinkVideoSource::InternalUpdate()
@@ -135,31 +48,11 @@ PlusStatus vtkOpenIGTLinkVideoSource::InternalUpdate()
   }
 
   igtl::MessageHeader::Pointer headerMsg;
-  PlusStatus socketStatus=ReceiveMessageHeader(headerMsg);
-  if (socketStatus==PLUS_FAIL)
-  {
-    // There is a socket error
-    if (this->GetReconnectOnReceiveTimeout())
-    {
-      LOG_ERROR("Socket error in device "<<this->GetDeviceId()<<": failed to receive OpenIGTLink transforms. Attempt to reconnect.");
-      ClientSocketReconnect();
-    }
-    else
-    {
-      LOG_ERROR("Socket error in device "<<this->GetDeviceId()<<": failed to receive OpenIGTLink transforms");
-    }
-  }
+  ReceiveMessageHeaderWithErrorHandling(headerMsg);
+  
   if (headerMsg.IsNull())
   {
-    if (this->GetReconnectOnReceiveTimeout())
-    {
-      LOG_WARNING("No OpenIGTLink message has been received in device "<<this->GetDeviceId()<<": failed to receive OpenIGTLink transforms. Attempt to reconnect.");
-      ClientSocketReconnect();
-    }
-    else
-    {
-      LOG_WARNING("No OpenIGTLink message has been received in device "<<this->GetDeviceId());
-    }
+    OnReceiveTimeout();
     return PLUS_FAIL;
   }
 
@@ -235,156 +128,12 @@ PlusStatus vtkOpenIGTLinkVideoSource::InternalUpdate()
 
   return status;
 }
-//----------------------------------------------------------------------------
-PlusStatus vtkOpenIGTLinkVideoSource::ClientSocketReconnect()
-{ 
-  LOG_DEBUG("Attempt to connect to client socket in device "<<this->GetDeviceId());
-  
-  if ( this->ClientSocket->GetConnected() )
-  {
-    this->ClientSocket->CloseSocket();  
-  }
-
-  if ( this->ServerAddress == NULL )
-  {
-    LOG_ERROR("Unable to connect OpenIGTLink server - server address is undefined" ); 
-    return PLUS_FAIL; 
-  }
-
-  if ( this->ServerPort < 0 )
-  {
-    LOG_ERROR("Unable to connect OpenIGTLink server - server port is invalid: " << this->ServerPort ); 
-    return PLUS_FAIL; 
-  }
-
-  int errorCode = 0; // 0 means success
-  RETRY_UNTIL_TRUE( 
-    (errorCode = this->ClientSocket->ConnectToServer( this->ServerAddress, this->ServerPort ))==0,
-    this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
-
-  if ( errorCode != 0 )
-  {
-    LOG_ERROR( "Cannot connect to the server (" << this->ServerAddress << ":" << this->ServerPort << ")." );
-    return PLUS_FAIL;
-  }
-  else
-  {
-    LOG_DEBUG( "Client successfully connected to server (" << this->ServerAddress << ":" << this->ServerPort << ")."  );
-  }
-
-  this->ClientSocket->SetTimeout(CLIENT_SOCKET_TIMEOUT_MSEC);
-
-  // If we specified message type, try to send it to the server
-  if ( this->MessageType != NULL )
-  {
-    // Send client info request to the server
-    PlusIgtlClientInfo clientInfo; 
-    // Set message type
-    clientInfo.IgtlMessageTypes.push_back(this->MessageType); 
-
-    // We need the following tool names from the server 
-    for ( DataSourceContainerConstIterator it = this->GetToolIteratorBegin(); it != this->GetToolIteratorEnd(); ++it )
-    {
-      PlusTransformName tName( it->second->GetSourceId(), this->GetToolReferenceFrameName() ); 
-      clientInfo.TransformNames.push_back( tName ); 
-    }
-
-    // Pack client info message 
-    igtl::PlusClientInfoMessage::Pointer clientInfoMsg = igtl::PlusClientInfoMessage::New(); 
-    clientInfoMsg->SetClientInfo(clientInfo); 
-    clientInfoMsg->Pack(); 
-
-    // Send message to server 
-    int retValue = 0;
-    RETRY_UNTIL_TRUE( 
-      (retValue = this->ClientSocket->Send( clientInfoMsg->GetPackPointer(), clientInfoMsg->GetPackSize() ))!=0,
-      this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
-
-    if ( retValue == 0 )
-    {
-      LOG_ERROR("Failed to send PlusClientInfo message to server!"); 
-      return PLUS_FAIL; 
-    }
-  }
-
-  return PLUS_SUCCESS;
-}
-
-//----------------------------------------------------------------------------
-PlusStatus vtkOpenIGTLinkVideoSource::ReceiveMessageHeader(igtl::MessageHeader::Pointer &headerMsg)
-{
-  headerMsg = igtl::MessageHeader::New();
-  headerMsg->InitPack();
-
-  int numOfBytesReceived = 0;
-  RETRY_UNTIL_TRUE(
-    (numOfBytesReceived = this->ClientSocket->Receive( headerMsg->GetPackPointer(), headerMsg->GetPackSize() ))!=0,
-    this->NumberOfRetryAttempts, this->DelayBetweenRetryAttemptsSec);
-
-  if ( numOfBytesReceived > 0)
-  {
-    // Data is received
-    if ( numOfBytesReceived != headerMsg->GetPackSize() )
-    {
-      // Received data is not as we expected
-      LOG_ERROR("Couldn't receive data from OpenIGTLink video source (unexpected header size)"); 
-      headerMsg=NULL;
-      return PLUS_FAIL; 
-    }
-    return PLUS_SUCCESS;
-  }
-   
-  // No data has been received
-  headerMsg=NULL; // this will indicate the caller that no data has been read
-
-  bool socketError = numOfBytesReceived<0; /* -1 == SOCKET_ERROR */
-#ifdef _WIN32
-  // On Windows try to get some more details about the socket error
-  if (socketError)
-  {     
-    int socketErrorCode=WSAGetLastError();
-    if (socketErrorCode==WSAETIMEDOUT)
-    {
-      // timeout, it just means that no data was received, no need to reconnect
-      LOG_TRACE("No data coming from OpenIGTLink video source (timeout)");
-      socketError=false;
-    }
-    else
-    {
-      LPTSTR errorMsgPtr = 0;
-      if(FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, socketErrorCode, 0, (LPTSTR)&errorMsgPtr, 0, NULL) != 0)
-      {
-        LOG_DEBUG("No data coming from OpenIGTLink video source (socket error: "<<errorMsgPtr<<")");
-      }
-      else
-      {
-        LOG_DEBUG("No data coming from OpenIGTLink video source (unknown socket error)");
-      }
-    }
-  }
-  else
-  {
-    LOG_DEBUG("No data coming from OpenIGTLink video source");
-  }
-#else
-  LOG_DEBUG("No data coming from OpenIGTLink video source");
-#endif  
-   
-  return socketError ? PLUS_FAIL : PLUS_SUCCESS;
-}
-
-
 
 //-----------------------------------------------------------------------------
 PlusStatus vtkOpenIGTLinkVideoSource::ReadConfiguration(vtkXMLDataElement* rootConfigElement)
 {
   XML_FIND_DEVICE_ELEMENT_REQUIRED_FOR_READING(deviceConfig, rootConfigElement);
-  XML_READ_STRING_ATTRIBUTE_REQUIRED(ServerAddress, deviceConfig);
-  XML_READ_SCALAR_ATTRIBUTE_REQUIRED(int, ServerPort, deviceConfig);
-  XML_READ_STRING_ATTRIBUTE_OPTIONAL(MessageType, deviceConfig);
-  XML_READ_BOOL_ATTRIBUTE_OPTIONAL(IgtlMessageCrcCheckEnabled, deviceConfig);  
   XML_READ_STRING_ATTRIBUTE_OPTIONAL(ImageMessageEmbeddedTransformName, deviceConfig);
-  XML_READ_BOOL_ATTRIBUTE_OPTIONAL(UseReceivedTimestamps, deviceConfig);
   return PLUS_SUCCESS;
 }
 
@@ -392,15 +141,7 @@ PlusStatus vtkOpenIGTLinkVideoSource::ReadConfiguration(vtkXMLDataElement* rootC
 PlusStatus vtkOpenIGTLinkVideoSource::WriteConfiguration(vtkXMLDataElement* rootConfigElement)
 {
   XML_FIND_DEVICE_ELEMENT_REQUIRED_FOR_WRITING(deviceConfig, rootConfigElement);
-
-  deviceConfig->SetAttribute("MessageType", this->MessageType);
-  deviceConfig->SetAttribute("ServerAddress", this->ServerAddress);
-  deviceConfig->SetIntAttribute("ServerPort", this->ServerPort);
   deviceConfig->SetAttribute("ImageMessageEmbeddedTransformName", this->ImageMessageEmbeddedTransformName.GetTransformName().c_str());
-  deviceConfig->SetAttribute("UseReceivedTimestamps", this->UseReceivedTimestamps?"true":"false");
-  deviceConfig->SetAttribute("ReconnectOnReceiveTimeout", this->ReconnectOnReceiveTimeout?"true":"false");
-  deviceConfig->SetAttribute("IgtlMessageCrcCheckEnabled", this->IgtlMessageCrcCheckEnabled?"true":"false");
-  
   return PLUS_SUCCESS;
 }
 
