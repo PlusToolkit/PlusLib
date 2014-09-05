7244187068f52dcf961997f7c22b7982ce8693bf
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2014-09-05 19:28:40 +0000

    re #491: Added Ultrasonix SDK 6.1 support to PlusBuilds

--- trunk/PlusBuild/Modules/FindULTRASONIX_SDK.cmake
+++ trunk/PlusBuild/Modules/FindULTRASONIX_SDK.cmake
@@ -9,19 +9,27 @@ IF ( NOT "${ULTRASONIX_SDK_VERSION}" STREQUAL "${PLUS_ULTRASONIX_SDK_MAJOR_VERSI
 ENDIF()
 
 SET( ULTRASONIX_SDK_PATH_HINTS 
-    ../Ultrasonix
-    ../PLTools/Ultrasonix
-    ../../PLTools/Ultrasonix
-    ../PLTools/trunk/Ultrasonix
-    ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Ultrasonix
-    )
-
-FIND_PATH(ULTRASONIX_SDK_DIR bin/ulterius${CMAKE_SHARED_LIBRARY_SUFFIX}
-  PATH_SUFFIXES 
-    sdk-${ULTRASONIX_SDK_VERSION}
-    Ulterius-${ULTRASONIX_SDK_VERSION}/
-  PATHS ${ULTRASONIX_SDK_PATH_HINTS} 
+  ../Ultrasonix
+  ../PLTools/Ultrasonix
+  ../../PLTools/Ultrasonix
+  ../PLTools/trunk/Ultrasonix
+  ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Ultrasonix
   )
+
+IF (PLUS_ULTRASONIX_SDK_MAJOR_VERSION EQUAL 6)
+  FIND_PATH(ULTRASONIX_SDK_DIR bin/ulterius_old${CMAKE_SHARED_LIBRARY_SUFFIX}
+    PATH_SUFFIXES 
+      sdk-${ULTRASONIX_SDK_VERSION}
+    PATHS ${ULTRASONIX_SDK_PATH_HINTS} 
+    )
+ELSE()
+  FIND_PATH(ULTRASONIX_SDK_DIR bin/ulterius${CMAKE_SHARED_LIBRARY_SUFFIX}
+    PATH_SUFFIXES 
+      sdk-${ULTRASONIX_SDK_VERSION}
+      Ulterius-${ULTRASONIX_SDK_VERSION}
+    PATHS ${ULTRASONIX_SDK_PATH_HINTS} 
+    )
+ENDIF()
 
 # handle the QUIETLY and REQUIRED arguments and set ULTRASONIX_SDK_FOUND to TRUE if 
 # all listed variables are TRUE
