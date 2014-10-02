48e64140c00b11a109732db71a6a27abf09aa05b
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2014-10-02 13:10:40 +0000

    re #41: Fixed build error

--- trunk/PlusLib/src/CalibrationAlgo/Testing/vtkTRUSCalibrationTest.cxx
+++ trunk/PlusLib/src/CalibrationAlgo/Testing/vtkTRUSCalibrationTest.cxx
@@ -80,7 +80,7 @@ int main (int argc, char* argv[])
   vtkPlusConfig::GetInstance()->SetDeviceSetConfigurationData(configRootElement);
 
   FidPatternRecognition patternRecognition;
-  PatternRecognitionError error;
+  FidPatternRecognition::PatternRecognitionError error;
   patternRecognition.ReadConfiguration(configRootElement);
 
   LOG_INFO("Reading probe rotation data from sequence metafile..."); 
