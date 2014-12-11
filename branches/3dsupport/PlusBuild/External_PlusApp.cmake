5498a0108786102ea429772f4ea7ab716eca9779
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2014-12-11 19:00:04 +0000

    re #955 pointing to new branched projects

--- branches/3dsupport/PlusBuild/External_PlusApp.cmake
+++ branches/3dsupport/PlusBuild/External_PlusApp.cmake
@@ -24,7 +24,7 @@ ExternalProject_Add(PlusApp
   #--Download step--------------
   SVN_USERNAME ${PLUSBUILD_ASSEMBLA_USERNAME}
   SVN_PASSWORD ${PLUSBUILD_ASSEMBLA_PASSWORD}
-  SVN_REPOSITORY https://subversion.assembla.com/svn/plus/trunk/PlusApp
+  SVN_REPOSITORY https://subversion.assembla.com/svn/plus/branches/3dsupport/PlusApp
   ${PLUSBUILD_SVN_REVISION_ARGS}
   #--Configure step-------------
   CMAKE_ARGS 
