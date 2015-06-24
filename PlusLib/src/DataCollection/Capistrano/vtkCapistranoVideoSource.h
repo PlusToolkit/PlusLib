551eff8d3d3da89650ad29f704691634003cfa35
matt.mccormick <matt.mccormick@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2015-06-24 20:12:14 +0000

    re: #1007: Add PlusLib CMake configuration for Capistrano.

---
+++ PlusLib/src/DataCollection/Capistrano/vtkCapistranoVideoSource.h
@@ -0,0 +1,10 @@
+/*=Plus=header=begin======================================================
+  Program: Plus
+  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+  See License.txt for details.
+=========================================================Plus=header=end*/
+
+#ifndef __vtkCapistranoVideoSource_h
+#define __vtkCapistranoVideoSource_h
+
+#endif
