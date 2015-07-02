55e7a461cfcc66a1898dc2b214c5191829ee4e18
matt.mccormick <matt.mccormick@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2015-07-02 17:21:25 +0000

    re: #1007 Add vtkCapistranoVideoSourceTest skeleton.

--- PlusLib/src/DataCollection/Capistrano/vtkCapistranoVideoSource.h
+++ PlusLib/src/DataCollection/Capistrano/vtkCapistranoVideoSource.h
@@ -7,4 +7,35 @@
 #ifndef __vtkCapistranoVideoSource_h
 #define __vtkCapistranoVideoSource_h
 
+#include "vtkDataCollectionExport.h"
+
+#include "vtkPlusDevice.h"
+
+/*!
+ \class vtkCapistranoVideoSource
+ \brief Class for acquiring ultrasound images from Capistrano Labs USB
+ ultrasound systems.
+
+ Requires PLUS_USE_CAPISTRANO_VIDEO option in CMake.
+ Requires the Capistrano cSDK2013 (SDK provided by Capistrano Labs).
+
+ \ingroup PlusLibDataCollection.
+*/
+class vtkDataCollectionExport vtkCapistranoVideoSource: public vtkPlusDevice
+{
+public:
+  static vtkCapistranoVideoSource * New();
+  vtkTypeMacro(vtkCapistranoVideoSource, vtkPlusDevice);
+
+  void PrintSelf(ostream& os, vtkIndent indent);   
+
+protected:
+  vtkCapistranoVideoSource();
+  ~vtkCapistranoVideoSource();
+
+private:
+  vtkCapistranoVideoSource(const vtkCapistranoVideoSource &); // Not implemented
+  void operator=(const vtkCapistranoVideoSource &); // Not implemented
+};
+
 #endif
