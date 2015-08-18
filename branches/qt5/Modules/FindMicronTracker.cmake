002b8b036b90b914a12a096458ea09210b19ec91
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2015-08-18 20:48:05 +0000

    re #1020 branching plusbuild with qt5 changes

---
+++ branches/qt5/Modules/FindMicronTracker.cmake
@@ -0,0 +1,102 @@
+# Find the Claron MicronTracker SDK 
+# This module defines
+# MICRONTRACKER_FOUND - MicronTracker SDK has been found on this system
+# MICRONTRACKER_INCLUDE_DIR - where to find the header files
+# MICRONTRACKER_LIBRARY - libraries to be linked
+# MICRONTRACKER_BINARY_DIR - shared libraries to be installed
+
+# By default prefer MTC_3.6 as MTC_3.7 requires installation of VS2013 redistributable package
+OPTION(PLUSBUILD_PREFER_MICRONTRACKER_36 "Plus prefers MicronTracker SDK version MTC_3.6 instead of MTC_3.7" ON)
+MARK_AS_ADVANCED(PLUSBUILD_PREFER_MICRONTRACKER_36)
+
+# If PLUSBUILD_PREFER_MICRONTRACKER_36 is defined: try to find MTC_3.6 first and fall back to MTC_3.7,
+# otherwise try to find MTC_3.7 and fall back to MTC_3.6.
+IF (PLUSBUILD_PREFER_MICRONTRACKER_36)
+  SET( MicronTracker_PATH_HINTS
+    ../Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ../PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ../../PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ../trunk/PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker  
+    ../Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ../PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ../../PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ../trunk/PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ../Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ../PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ../../PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ../trunk/PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    )
+ELSE()
+  SET( MicronTracker_PATH_HINTS
+    ../Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ../PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ../../PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ../trunk/PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Claron/MTC_3.7.6.8_x86_win/MicronTracker
+    ../Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ../PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ../../PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ../trunk/PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker
+    ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Claron/MTC_3.6.5.4_x86_win/MicronTracker  
+    ../Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ../PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ../../PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ../trunk/PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    ${CMAKE_CURRENT_BINARY_DIR}/PLTools/Claron/MTC_3.6.1.6_x86_win/MicronTracker
+    )
+ENDIF()
+
+SET( MicronTracker_PATH_HINTS ${MicronTracker_PATH_HINTS}
+  "c:/Program Files (x86)/Claron Technology/MicronTracker"
+  "c:/Program Files/Claron Technology/MicronTracker"
+  )
+
+  IF(CMAKE_HOST_WIN32 AND CMAKE_CL_64)
+    SET(PLATFORM_SUFFIX "64")
+  ELSE()
+    SET(PLATFORM_SUFFIX "")
+  ENDIF()
+  
+FIND_PATH(MicronTracker_BASE_INCLUDE_DIR MTC.h 
+  PATH_SUFFIXES
+    Dist${PLATFORM_SUFFIX}
+    inc	
+  DOC "MicronTracker include directory (contains MTC.h)"
+  PATHS ${MicronTracker_PATH_HINTS} 
+  )
+
+FIND_LIBRARY(MicronTracker_BASE_LIBRARY 
+  NAMES MTC${CMAKE_STATIC_LIBRARY_SUFFIX}
+  PATH_SUFFIXES
+    Dist${PLATFORM_SUFFIX}
+    lib
+  DOC "Path to MicronTracker base library (MTC.lib)"
+  PATHS ${MicronTracker_PATH_HINTS} 
+  )
+
+FIND_PATH(MicronTracker_BASE_BINARY_DIR MTC${CMAKE_SHARED_LIBRARY_SUFFIX}
+  PATH_SUFFIXES 
+    Dist${PLATFORM_SUFFIX}
+    bin
+  PATHS ${MicronTracker_PATH_HINTS} 
+  DOC "Path to MicronTracker base shared library (MTC.dll)"
+  NO_DEFAULT_PATH # avoid finding installed DLLs in the system folders
+  )
+
+# handle the QUIETLY and REQUIRED arguments and set MicronTracker_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MicronTracker DEFAULT_MSG 
+  MicronTracker_BASE_LIBRARY
+  MicronTracker_BASE_INCLUDE_DIR
+  MicronTracker_BASE_BINARY_DIR
+  )
+
+IF(MICRONTRACKER_FOUND)
+  SET( MICRONTRACKER_LIBRARY ${MicronTracker_BASE_LIBRARY} )
+  SET( MICRONTRACKER_INCLUDE_DIR ${MicronTracker_BASE_INCLUDE_DIR} )
+  SET( MICRONTRACKER_BINARY_DIR ${MicronTracker_BASE_BINARY_DIR} )
+ENDIF()
