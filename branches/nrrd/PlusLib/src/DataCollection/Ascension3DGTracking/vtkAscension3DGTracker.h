a6ec270691f5e3907bbca4bc055ab38670a00981
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-01-11 18:02:01 +0000

    re #1059 Deleting nrrd branch now that it is complete

--- branches/nrrd/PlusLib/src/DataCollection/Ascension3DGTracking/vtkAscension3DGTracker.h
+++
@@ -1,8 +0,0 @@
-#ifndef __vtkAscension3DGTracker_h
-#define __vtkAscension3DGTracker_h
-
-#define vtkAscension3DGTrackerBase vtkAscension3DGTracker
-#include "vtkAscension3DGTrackerBase.h"
-#undef vtkAscension3DGTrackerBase
-
-#endif
