a5c0bec602faed13dd1fec986bc5e81206797145
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-01-11 18:02:57 +0000

    re #1059 removing 3dsupport branch now that trunk has shown stable 3d capabilities

--- branches/3dsupport/PlusLib/src/DataCollection/Ascension3DGTracking/vtkAscension3DGmTracker.h
+++
@@ -1,8 +0,0 @@
-#ifndef __vtkAscension3DGmTracker_h
-#define __vtkAscension3DGmTracker_h
-
-#define vtkAscension3DGTrackerBase vtkAscension3DGmTracker
-#include "vtkAscension3DGTrackerBase.h"
-#undef vtkAscension3DGTrackerBase
-
-#endif
