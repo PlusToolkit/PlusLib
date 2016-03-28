d344b0c4ef4bfb2ec2e77f577a0ddda1d5290b6a
lassoan <lassoan@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-03-28 12:16:55 +0000

    re #1071: Updated Windows SDK path detection and ImagingControls SDK version to support VS2013

--- trunk/PlusBuild/Modules/TestWindowsSDK.cmake
+++ trunk/PlusBuild/Modules/TestWindowsSDK.cmake
@@ -1,18 +1,14 @@
-MESSAGE(STATUS "Checking if the latest Windows SDK is compatible (${WINDOWSSDK_LATEST_DIR})")
+MESSAGE(STATUS "Checking if the latest Windows SDK is compatible (${WINDOWS_SDK_ROOT_DIR})")
 
-IF( ${CMAKE_GENERATOR} MATCHES Win64 )
-  SET( MF_LIB_DIR ${WINDOWSSDK_LATEST_DIR}/Lib/x64 )
-ELSE()
-  SET( MF_LIB_DIR ${WINDOWSSDK_LATEST_DIR}/Lib )
-ENDIF()  
 TRY_COMPILE(PLUS_WINDOWS_SDK_IS_COMPATIBLE
             "${CMAKE_BINARY_DIR}/CMakeTmp/WindowsSdkCompatibility"
-            "${CMAKE_CURRENT_SOURCE_DIR}/Modules/PlusTestWindowsSdkCompatible.cxx"              
+            "${CMAKE_CURRENT_SOURCE_DIR}/Modules/PlusTestWindowsSdkCompatible.cxx"
             CMAKE_FLAGS
-              "-DINCLUDE_DIRECTORIES:STRING=${WINDOWSSDK_LATEST_DIR}/Include"
-              "-DLINK_LIBRARIES:STRING=${MF_LIB_DIR}/mfuuid.lib"
+              "-DINCLUDE_DIRECTORIES:STRING=${WINDOWS_SDK_INCLUDE_DIRS}"
+              "-DLINK_DIRECTORIES:STRING=${WINDOWS_SDK_LIBRARY_DIRS}"
+              "-DLINK_LIBRARIES:STRING=mfuuid.lib"
             OUTPUT_VARIABLE OUTPUT)
-            
+
 IF(PLUS_WINDOWS_SDK_IS_COMPATIBLE)
   MESSAGE(STATUS "Checking if the latest Windows SDK is compatible -- yes")
   SET(PLUS_WINDOWS_SDK_IS_COMPATIBLE 1 CACHE INTERNAL "Windows SDK is compatible with Plus")
