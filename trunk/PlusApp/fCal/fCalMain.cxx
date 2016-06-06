0642ac764cb113ed464d99bc4ae1c973dc049b52
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-06-06 17:36:32 +0000

    re #1072 missing rename in fCal Qt Resource file

--- trunk/PlusApp/fCal/fCalMain.cxx
+++ trunk/PlusApp/fCal/fCalMain.cxx
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-  Q_INIT_RESOURCE(CommonWidgets);
+  Q_INIT_RESOURCE(PlusCommonWidgets);
 
 	fCalMainWindow mainWindow;
 	mainWindow.show();
