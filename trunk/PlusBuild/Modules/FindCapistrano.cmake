658cdce0d7d817bcb3d53e34fb1eda22e8217d20
dzenanz <dzenanz@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-07-19 13:52:31 +0000

    fixed #1098 Simplifying Capistrano SDK usage

--- trunk/PlusBuild/Modules/FindCapistrano.cmake
+++ trunk/PlusBuild/Modules/FindCapistrano.cmake
@@ -1,6 +1,6 @@
 # Find the Capistrano iSDK
 # This module defines
-# CAPISTRANO_FOUND - MicronTracker SDK has been found on this system
+# CAPISTRANO_FOUND - Capistrano SDK has been found on this system
 # CAPISTRANO_INCLUDE_DIR - where to find the header files
 # CAPISTRANO_LIBRARY_DIR - libraries to be linked
 # CAPISTRANO_BINARY_DIR - common shared libraries to be installed
@@ -18,19 +18,18 @@ FIND_PATH(Capistrano_INCLUDE_DIR usbprobedll_net.h
   PATHS ${Capistrano_PATH_HINTS} 
   )
 
-FIND_PATH(Capistrano_LIBRARY_DIR USBprobeDLL${CMAKE_STATIC_LIBRARY_SUFFIX}
+FIND_PATH(Capistrano_WIN32_LIBRARY_DIR USBprobeDLL${CMAKE_STATIC_LIBRARY_SUFFIX}
   PATH_SUFFIXES
     "lib"
-  DOC "Capistrano library directory (contains USBprobeDLL.lib)"
+  DOC "Capistrano 32-bit library directory (contains USBprobeDLL.lib)"
   PATHS ${Capistrano_PATH_HINTS} 
   )
   
-FIND_PATH(Capistrano_BINARY_DIR BmodeUSB${CMAKE_SHARED_LIBRARY_SUFFIX}
-  PATH_SUFFIXES 
-    "bin"
-  DOC "Path to Capistrano common binary directory (contains BmodeUSB.dll)"
-  PATHS ${Capistrano_PATH_HINTS}
-  NO_DEFAULT_PATH # avoid finding installed DLLs in the system folders
+FIND_PATH(Capistrano_WIN64_LIBRARY_DIR USBprobeDLL${CMAKE_STATIC_LIBRARY_SUFFIX}
+  PATH_SUFFIXES
+    "lib/x64"
+  DOC "Capistrano 64-bit library directory (contains USBprobeDLL.lib)"
+  PATHS ${Capistrano_PATH_HINTS} 
   )
   
 FIND_PATH(Capistrano_WIN32_BINARY_DIR USBprobe32${CMAKE_SHARED_LIBRARY_SUFFIX}
@@ -49,6 +48,14 @@ FIND_PATH(Capistrano_WIN64_BINARY_DIR USBprobe${CMAKE_SHARED_LIBRARY_SUFFIX}
   NO_DEFAULT_PATH # avoid finding installed DLLs in the system folders
   )  
 
+if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+  set( Capistrano_BINARY_DIR ${Capistrano_WIN64_BINARY_DIR} )
+  set( Capistrano_LIBRARY_DIR ${Capistrano_WIN64_LIBRARY_DIR} )  
+else( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+  set( Capistrano_BINARY_DIR ${Capistrano_WIN32_BINARY_DIR} )
+  set( Capistrano_LIBRARY_DIR ${Capistrano_WIN32_LIBRARY_DIR} )
+endif( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+  
 # handle the QUIETLY and REQUIRED arguments and set Capistrano_FOUND to TRUE if 
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
@@ -56,14 +63,10 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(Capistrano DEFAULT_MSG
   Capistrano_LIBRARY_DIR
   Capistrano_INCLUDE_DIR
   Capistrano_BINARY_DIR
-  Capistrano_WIN32_BINARY_DIR
-  Capistrano_WIN64_BINARY_DIR
   )
 
 IF(CAPISTRANO_FOUND)
   SET( CAPISTRANO_LIBRARY_DIR ${Capistrano_LIBRARY_DIR} )
   SET( CAPISTRANO_INCLUDE_DIR ${Capistrano_INCLUDE_DIR} )
   SET( CAPISTRANO_BINARY_DIR ${Capistrano_BINARY_DIR} )
-  SET( CAPISTRANO_WIN32_BINARY_DIR ${Capistrano_WIN32_BINARY_DIR} )
-  SET( CAPISTRANO_WIN64_BINARY_DIR ${Capistrano_WIN64_BINARY_DIR} )
 ENDIF()
