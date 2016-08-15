b20e26a7c807fdc0a47e07f4b9ab27bbf484932b
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-08-15 15:50:31 +0000

    re #1089 reducing warning by correcting calculation bug

--- trunk/PlusLib/src/PlusDataCollection/Haptics/vtkPlusPolydataForce.cxx
+++ trunk/PlusLib/src/PlusDataCollection/Haptics/vtkPlusPolydataForce.cxx
@@ -13,7 +13,7 @@ See License.txt for details.
 
 //----------------------------------------------------------------------------
 
-vtkStandardNewMacro(vtkPlusPolydataForce)
+vtkStandardNewMacro( vtkPlusPolydataForce )
 
 //----------------------------------------------------------------------------
 vtkPlusPolydataForce::vtkPlusPolydataForce()
@@ -27,9 +27,9 @@ vtkPlusPolydataForce::vtkPlusPolydataForce()
 }
 
 //----------------------------------------------------------------------------
-void vtkPlusPolydataForce::PrintSelf(ostream& os, vtkIndent indent)
+void vtkPlusPolydataForce::PrintSelf( ostream& os, vtkIndent indent )
 {
-  this->Superclass::PrintSelf(os, indent.GetNextIndent());
+  this->Superclass::PrintSelf( os, indent.GetNextIndent() );
   os << indent.GetNextIndent() << "Gamma Sigmoid: " << this->gammaSigmoid << endl;
 }
 
@@ -40,46 +40,46 @@ vtkPlusPolydataForce::~vtkPlusPolydataForce()
 }
 
 //----------------------------------------------------------------------------
-void vtkPlusPolydataForce::SetInput(vtkPolyData * poly)
+void vtkPlusPolydataForce::SetInput( vtkPolyData* poly )
 {
   this->poly = poly;
 }
 
 //----------------------------------------------------------------------------
-int vtkPlusPolydataForce::GenerateForce(vtkMatrix4x4 * transformMatrix, double force[3])
+int vtkPlusPolydataForce::GenerateForce( vtkMatrix4x4* transformMatrix, double force[3] )
 {
   double distance;
-  distance = CalculateDistance(transformMatrix->GetElement(0,3), transformMatrix->GetElement(1,3), transformMatrix->GetElement(2,3));
+  distance = CalculateDistance( transformMatrix->GetElement( 0, 3 ), transformMatrix->GetElement( 1, 3 ), transformMatrix->GetElement( 2, 3 ) );
 
-  if (distance <= 5)
+  if ( distance <= 5 )
   {
-    CalculateForce(transformMatrix->GetElement(0,3), transformMatrix->GetElement(1,3), transformMatrix->GetElement(2,3), force);
+    CalculateForce( transformMatrix->GetElement( 0, 3 ), transformMatrix->GetElement( 1, 3 ), transformMatrix->GetElement( 2, 3 ), force );
   }
   else
   {
-    force[0] = (0);
-    force[1] = (0);
-    force[2] = (0);
+    force[0] = ( 0 );
+    force[1] = ( 0 );
+    force[2] = ( 0 );
   }
   cout <<  " FORCE: " << force[0] << ",  " << force[1] << ",  " << force[2] << endl;
   return 1;
 }
 
 //----------------------------------------------------------------------------
-double vtkPlusPolydataForce::CalculateDistance(double x, double y, double z)
+double vtkPlusPolydataForce::CalculateDistance( double x, double y, double z )
 {
   int pointID;
   double distance;
-  pointID = this->poly->FindPoint(x,y,z);
-  this->poly->GetPoint(pointID, this->lastPos);
+  pointID = this->poly->FindPoint( x, y, z );
+  this->poly->GetPoint( pointID, this->lastPos );
 
-  double tmp = pow((x-lastPos[0]),2) + pow((y-lastPos[1]),2) + pow((z-lastPos[2]),2);
-  distance = sqrt(tmp);
+  double tmp = pow( ( x - lastPos[0] ), 2 ) + pow( ( y - lastPos[1] ), 2 ) + pow( ( z - lastPos[2] ), 2 );
+  distance = sqrt( tmp );
   return distance;
 }
 
 //----------------------------------------------------------------------------
-int vtkPlusPolydataForce::SetGamma(double gamma)
+int vtkPlusPolydataForce::SetGamma( double gamma )
 {
   gammaSigmoid = gamma;
 
@@ -87,39 +87,39 @@ int vtkPlusPolydataForce::SetGamma(double gamma)
 }
 
 //----------------------------------------------------------------------------
-void vtkPlusPolydataForce::CalculateForce(double x, double y, double z, double force[3])
+void vtkPlusPolydataForce::CalculateForce( double x, double y, double z, double force[3] )
 {
   double vector[3];
 
-  vector[0] = fabs(x - this->lastPos[0]);
-  vector[1] = fabs(y - this->lastPos[1]);
-  vector[2] = fabs(z - this->lastPos[2]);
+  vector[0] = fabs( x - this->lastPos[0] );
+  vector[1] = fabs( y - this->lastPos[1] );
+  vector[2] = fabs( z - this->lastPos[2] );
 
   cout << "vector[0]: " << vector[0] << endl;
   cout << "vector[1]: " << vector[1] << endl;
   cout << "vector[2]: " << vector[2] << endl;
 
-  for (int i=0; i<=3; i++)
+  for ( int i = 0; i < 3; i++ )
   {
-    if (vector[i] > 0)
+    if ( vector[i] > 0 )
     {
-      force[i] = (0.1/(vector[i]*vector[i]))*.6;
+      force[i] = ( 0.1 / ( vector[i] * vector[i] ) ) * .6;
     }
   }
   cout << "X: " << force[0] << endl;
   cout << "Y: " << force[1] << endl;
   cout << "Z: " << force[2] << endl;
 
-  if (force[0] > 1)
+  if ( force[0] > 1 )
   {
-    force[0]=.6;
+    force[0] = .6;
   }
-  if (force[1] > 1)
+  if ( force[1] > 1 )
   {
-    force[1]=.6;
+    force[1] = .6;
   }
-  if (force[2] > 1)
+  if ( force[2] > 1 )
   {
-    force[2]=.6;
+    force[2] = .6;
   }
 }
