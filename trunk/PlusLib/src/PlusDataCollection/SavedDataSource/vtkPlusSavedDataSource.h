ddebc69b1ce74cd69cd0dfcca6a59d8bdfe66772
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-08-24 23:55:30 +0000

    test #950 storing exact value of source filename and only using absolute path to load the data

--- trunk/PlusLib/src/PlusDataCollection/SavedDataSource/vtkPlusSavedDataSource.h
+++ trunk/PlusLib/src/PlusDataCollection/SavedDataSource/vtkPlusSavedDataSource.h
@@ -11,12 +11,12 @@ See License.txt for details.
 
 #include "vtkPlusDevice.h"
 
-class vtkPlusBuffer; 
+class vtkPlusBuffer;
 
 class vtkPlusDataCollectionExport vtkPlusSavedDataSource;
 
 /*!
-\class vtkPlusSavedDataSource 
+\class vtkPlusSavedDataSource
 \brief Class for providing VTK video input interface from sequence file
 \ingroup PlusLibDataCollection
 
@@ -24,57 +24,57 @@ Attributes:
 \li SequenceFile: source file to read the image/transform data from
 \li UseData: specifies what data to be use from the source file (IMAGE|IMAGE_AND_TRANSFORM|TRANSFORM)
 \li RepeatEnabled: if true then the sequence is played repeateadly (TRUE|FALSE)
-\li UseOriginalTimestamps: if true then the original timestamps (recorded originally in the source file) 
-  will be replayed exactly, otherwise only the timestamp difference will be replayed exactly, 
+\li UseOriginalTimestamps: if true then the original timestamps (recorded originally in the source file)
+  will be replayed exactly, otherwise only the timestamp difference will be replayed exactly,
   starting from the current time (TRUE|FALSE)
 
 */
 class vtkPlusDataCollectionExport vtkPlusSavedDataSource : public vtkPlusDevice
 {
 public:
-  vtkTypeMacro(vtkPlusSavedDataSource,vtkPlusDevice);
-  void PrintSelf(ostream& os, vtkIndent indent);   
+  vtkTypeMacro( vtkPlusSavedDataSource,vtkPlusDevice );
+  void PrintSelf( ostream& os, vtkIndent indent );
   static vtkPlusSavedDataSource* New();
 
   /*! Read configuration from xml data */
-  virtual PlusStatus ReadConfiguration(vtkXMLDataElement* config); 
+  virtual PlusStatus ReadConfiguration( vtkXMLDataElement* config );
   /*! Write configuration to xml data */
-  virtual PlusStatus WriteConfiguration(vtkXMLDataElement* config);
+  virtual PlusStatus WriteConfiguration( vtkXMLDataElement* config );
 
   /*! Set SequenceMetafile name with path with tracking buffer data  */
-  vtkSetStringMacro(SequenceFile);
+  vtkSetStringMacro( SequenceFile );
   /*! Get SequenceMetafile name with path with tracking buffer data  */
-  vtkGetStringMacro(SequenceFile);
+  vtkGetStringMacro( SequenceFile );
 
   /*! Set the time range of the loaded buffer that will be replayed */
-  void SetLoopTimeRange(double loopStartTime, double loopStopTime); 
-  
+  void SetLoopTimeRange( double loopStartTime, double loopStopTime );
+
   /*! Get the time range of the loaded buffer that will be replayed. It is initialized to the full range of the loaded data set on Connect(). */
-  void GetLoopTimeRange(double& loopStartTime, double& loopStopTime); 
-    
+  void GetLoopTimeRange( double& loopStartTime, double& loopStopTime );
+
   /*! Set flag to to enable saved dataset looping /sa RepeatEnabled */
-  vtkGetMacro(RepeatEnabled, bool);
+  vtkGetMacro( RepeatEnabled, bool );
   /*! Get flag to to enable saved dataset looping /sa RepeatEnabled */
-  vtkSetMacro(RepeatEnabled, bool);
+  vtkSetMacro( RepeatEnabled, bool );
   /*! Get flag to to enable saved dataset looping /sa RepeatEnabled */
-  vtkBooleanMacro(RepeatEnabled, bool);
+  vtkBooleanMacro( RepeatEnabled, bool );
 
   /*! Read all the frame fields from the file and provide them in the output */
-  vtkGetMacro(UseAllFrameFields, bool);
+  vtkGetMacro( UseAllFrameFields, bool );
   /*! Read all the frame fields from the file and provide them in the output */
-  vtkSetMacro(UseAllFrameFields, bool);
+  vtkSetMacro( UseAllFrameFields, bool );
   /*! Read all the frame fields from the file and provide them in the output */
-  vtkBooleanMacro(UseAllFrameFields, bool);
+  vtkBooleanMacro( UseAllFrameFields, bool );
 
   /*! Read the timestamps from the file and use provide them in the output (instead of the current time) */
-  vtkGetMacro(UseOriginalTimestamps, bool);
+  vtkGetMacro( UseOriginalTimestamps, bool );
   /*! Read the timestamps from the file and use provide them in the output (instead of the current time) */
-  vtkSetMacro(UseOriginalTimestamps, bool);
+  vtkSetMacro( UseOriginalTimestamps, bool );
   /*! Read the timestamps from the file and use provide them in the output (instead of the current time) */
-  vtkBooleanMacro(UseOriginalTimestamps, bool);
+  vtkBooleanMacro( UseOriginalTimestamps, bool );
 
   /*! Get local video buffer */
-  vtkGetObjectMacro(LocalVideoBuffer, vtkPlusBuffer); 
+  vtkGetObjectMacro( LocalVideoBuffer, vtkPlusBuffer );
 
   virtual bool IsTracker() const;
 
@@ -96,10 +96,10 @@ protected:
   virtual PlusStatus InternalConnect();
 
   /*! Connect to device, in case the output is a video stream */
-  virtual PlusStatus InternalConnectVideo(vtkPlusTrackedFrameList* savedDataBuffer);
+  virtual PlusStatus InternalConnectVideo( vtkPlusTrackedFrameList* savedDataBuffer );
 
   /*! Connect to device, in case the output is a tracker stream */
-  virtual PlusStatus InternalConnectTracker(vtkPlusTrackedFrameList* savedDataBuffer);
+  virtual PlusStatus InternalConnectTracker( vtkPlusTrackedFrameList* savedDataBuffer );
 
   /*! Disconnect from device */
   virtual PlusStatus InternalDisconnect();
@@ -108,26 +108,26 @@ protected:
   PlusStatus InternalUpdate();
 
   /*! Internal update, called when NOT the original timestamps are used */
-  PlusStatus InternalUpdateCurrentTimestamp(BufferItemUidType frameToBeAddedUid, int frameToBeAddedLoopIndex);
+  PlusStatus InternalUpdateCurrentTimestamp( BufferItemUidType frameToBeAddedUid, int frameToBeAddedLoopIndex );
 
   /*! Internal update, called when the original timestamps are used */
-  PlusStatus InternalUpdateOriginalTimestamp(BufferItemUidType frameToBeAddedUid, int frameToBeAddedLoopIndex);
+  PlusStatus InternalUpdateOriginalTimestamp( BufferItemUidType frameToBeAddedUid, int frameToBeAddedLoopIndex );
 
-  BufferItemUidType GetClosestFrameUidWithinTimeRange(double time_Local, double startTime_Local, double stopTime_Local);
+  BufferItemUidType GetClosestFrameUidWithinTimeRange( double time_Local, double startTime_Local, double stopTime_Local );
 
   /*! Get local tracker buffer */
-  vtkPlusBuffer* GetLocalTrackerBuffer(); 
+  vtkPlusBuffer* GetLocalTrackerBuffer();
 
   /*! Get the output data source (the video buffer or the first active tool buffer of the current stream) */
   vtkPlusDataSource* GetOutputDataSource();
 
-  /*! 
-    Get local tracker buffer, it returns tracker buffer if the output is a tracker stream, and 
-    returns the video buffer if the output is a video stream 
+  /*!
+    Get local tracker buffer, it returns tracker buffer if the output is a tracker stream, and
+    returns the video buffer if the output is a video stream
   */
   vtkPlusBuffer* GetLocalBuffer();
 
-  void DeleteLocalBuffers(); 
+  void DeleteLocalBuffers();
 
 protected:
   /*! Byte alignment of each row in the framebuffer */
@@ -137,23 +137,23 @@ protected:
   char* SequenceFile;
 
   /*! Flag to to enable saved dataset looping. If it's enabled, the video source will continuously play saved data (starts playing from the beginning when the end is reached). */
-  bool RepeatEnabled; 
+  bool RepeatEnabled;
 
-  /*! 
+  /*!
     Loop start time (in local buffer time). The first acquired frame (at system time = 0) will be the frame
     that has the closest timestamp to the start time.
     ItemTimestamp_Local = loopStartTime_Local + (actualTimestamp_Sys - startTimestamp_Sys) % (loopStopTime-LooStartTime)
   */
-  double LoopStartTime_Local; 
+  double LoopStartTime_Local;
 
   /*! Loop stop time (in local buffer time) */
-  double LoopStopTime_Local; 
+  double LoopStopTime_Local;
 
   /*! Local video buffer */
-  vtkPlusBuffer* LocalVideoBuffer; 
+  vtkPlusBuffer* LocalVideoBuffer;
 
   /*! Local buffer for each tracker tool, used for storing data read from sequence metafile */
-  std::map<std::string, vtkPlusBuffer*> LocalTrackerBuffers; 
+  std::map<std::string, vtkPlusBuffer*> LocalTrackerBuffers;
 
   /*! Read all the frame fields from the file and provide them in the output */
   bool UseAllFrameFields;
@@ -169,7 +169,7 @@ protected:
 
   /*! Frames before this item (identified by the buffer item UID) in the local buffer are ignored, not replayed */
   BufferItemUidType LoopFirstFrameUid;
-  
+
   /*! Frames after this item (identified by the buffer item UID) in the local buffer are ignored, not replayed */
   BufferItemUidType LoopLastFrameUid;
 
@@ -183,8 +183,8 @@ protected:
 
 private:
   static vtkPlusSavedDataSource* Instance;
-  vtkPlusSavedDataSource(const vtkPlusSavedDataSource&);  // Not implemented.
-  void operator=(const vtkPlusSavedDataSource&);  // Not implemented.
+  vtkPlusSavedDataSource( const vtkPlusSavedDataSource& ); // Not implemented.
+  void operator=( const vtkPlusSavedDataSource& ); // Not implemented.
 };
 
 #endif
