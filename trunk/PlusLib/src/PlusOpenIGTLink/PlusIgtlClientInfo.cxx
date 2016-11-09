e4230e55935da71989fdc1a37843f22b2a93ba20
rankin <rankin@c04e96f8-ac36-4ff2-8b34-4a5e34ef045f>
2016-11-09 00:04:53 +0000

    re #1010 enabling default TDATARequested set to on from config file

--- trunk/PlusLib/src/PlusOpenIGTLink/PlusIgtlClientInfo.cxx
+++ trunk/PlusLib/src/PlusOpenIGTLink/PlusIgtlClientInfo.cxx
@@ -12,229 +12,234 @@ See License.txt for details.
 
 //----------------------------------------------------------------------------
 PlusIgtlClientInfo::PlusIgtlClientInfo()
-  : ClientHeaderVersion( IGTL_HEADER_VERSION_1 )
-  , Resolution( 0 )
-  , TDATARequested( false )
-  , LastTDATASentTimeStamp( -1 )
+  : ClientHeaderVersion(IGTL_HEADER_VERSION_1)
+  , Resolution(0)
+  , TDATARequested(false)
+  , LastTDATASentTimeStamp(-1)
 {
 
 }
 
 //----------------------------------------------------------------------------
-PlusStatus PlusIgtlClientInfo::SetClientInfoFromXmlData( const char* strXmlData )
+PlusStatus PlusIgtlClientInfo::SetClientInfoFromXmlData(const char* strXmlData)
 {
-  if ( strXmlData == NULL )
+  if (strXmlData == NULL)
   {
-    LOG_ERROR( "Failed to set ClientInfo - input xml data string is invalid" );
+    LOG_ERROR("Failed to set ClientInfo - input xml data string is invalid");
     return PLUS_FAIL;
   }
 
-  vtkSmartPointer<vtkXMLDataElement> xmldata = vtkSmartPointer<vtkXMLDataElement>::Take( vtkXMLUtilities::ReadElementFromString( strXmlData ) );
-  if ( xmldata == NULL )
+  vtkSmartPointer<vtkXMLDataElement> xmldata = vtkSmartPointer<vtkXMLDataElement>::Take(vtkXMLUtilities::ReadElementFromString(strXmlData));
+  if (xmldata == NULL)
   {
-    LOG_ERROR( "Failed to set ClientInfo - invalid xml data string: " << strXmlData );
+    LOG_ERROR("Failed to set ClientInfo - invalid xml data string: " << strXmlData);
     return PLUS_FAIL;
   }
 
-  return SetClientInfoFromXmlData( xmldata );
+  return SetClientInfoFromXmlData(xmldata);
 }
 
 
 //----------------------------------------------------------------------------
-PlusStatus PlusIgtlClientInfo::SetClientInfoFromXmlData( vtkXMLDataElement* xmldata )
+PlusStatus PlusIgtlClientInfo::SetClientInfoFromXmlData(vtkXMLDataElement* xmldata)
 {
-  if ( xmldata == NULL )
+  if (xmldata == NULL)
   {
-    LOG_ERROR( "Failed to set ClientInfo - invalid xml data string!" );
+    LOG_ERROR("Failed to set ClientInfo - invalid xml data string!");
     return PLUS_FAIL;
   }
 
   PlusIgtlClientInfo clientInfo;
 
-  // Get message types
-  vtkXMLDataElement* messageTypes = xmldata->FindNestedElementWithName( "MessageTypes" );
-  if ( messageTypes != NULL )
+  if (xmldata->GetAttribute("TDATARequested") != NULL)
   {
-    for ( int i = 0; i < messageTypes->GetNumberOfNestedElements(); ++i )
+    clientInfo.TDATARequested = STRCASECMP(xmldata->GetAttribute("TDATARequested"), "TRUE") == 0;
+  }
+
+  // Get message types
+  vtkXMLDataElement* messageTypes = xmldata->FindNestedElementWithName("MessageTypes");
+  if (messageTypes != NULL)
+  {
+    for (int i = 0; i < messageTypes->GetNumberOfNestedElements(); ++i)
     {
-      const char* name = messageTypes->GetNestedElement( i )->GetName();
-      if ( name == NULL || STRCASECMP( name, "Message" ) != 0 )
+      const char* name = messageTypes->GetNestedElement(i)->GetName();
+      if (name == NULL || STRCASECMP(name, "Message") != 0)
       {
         continue;
       }
-      const char* type = messageTypes->GetNestedElement( i )->GetAttribute( "Type" );
-      if ( type != NULL )
+      const char* type = messageTypes->GetNestedElement(i)->GetAttribute("Type");
+      if (type != NULL)
       {
-        clientInfo.IgtlMessageTypes.push_back( type );
+        clientInfo.IgtlMessageTypes.push_back(type);
       }
     }
   }
 
   // Get transform names
-  vtkXMLDataElement* transformNames = xmldata->FindNestedElementWithName( "TransformNames" );
-  if ( transformNames != NULL )
+  vtkXMLDataElement* transformNames = xmldata->FindNestedElementWithName("TransformNames");
+  if (transformNames != NULL)
   {
-    for ( int i = 0; i < transformNames->GetNumberOfNestedElements(); ++i )
+    for (int i = 0; i < transformNames->GetNumberOfNestedElements(); ++i)
     {
-      const char* transform = transformNames->GetNestedElement( i )->GetName();
-      if ( transform == NULL || STRCASECMP( transform, "Transform" ) != 0 )
+      const char* transform = transformNames->GetNestedElement(i)->GetName();
+      if (transform == NULL || STRCASECMP(transform, "Transform") != 0)
       {
         continue;
       }
-      const char* name = transformNames->GetNestedElement( i )->GetAttribute( "Name" );
-      if ( name == NULL )
+      const char* name = transformNames->GetNestedElement(i)->GetAttribute("Name");
+      if (name == NULL)
       {
-        LOG_WARNING( "In TransformNames child Transform #" << i << " definition is incomplete: required Name attribute is missing" );
+        LOG_WARNING("In TransformNames child Transform #" << i << " definition is incomplete: required Name attribute is missing");
         continue;
       }
       PlusTransformName tName;
-      if ( tName.SetTransformName( name ) != PLUS_SUCCESS )
+      if (tName.SetTransformName(name) != PLUS_SUCCESS)
       {
-        LOG_WARNING( "Invalid transform name: " << name );
+        LOG_WARNING("Invalid transform name: " << name);
         continue;
       }
-      clientInfo.TransformNames.push_back( tName );
+      clientInfo.TransformNames.push_back(tName);
     } // transformNames
   }
 
   // Get image streams
-  vtkXMLDataElement* imageNames = xmldata->FindNestedElementWithName( "ImageNames" );
-  if ( imageNames != NULL )
+  vtkXMLDataElement* imageNames = xmldata->FindNestedElementWithName("ImageNames");
+  if (imageNames != NULL)
   {
-    for ( int i = 0; i < imageNames->GetNumberOfNestedElements(); ++i )
+    for (int i = 0; i < imageNames->GetNumberOfNestedElements(); ++i)
     {
-      const char* image = imageNames->GetNestedElement( i )->GetName();
-      if ( image == NULL || STRCASECMP( image, "Image" ) != 0 )
+      const char* image = imageNames->GetNestedElement(i)->GetName();
+      if (image == NULL || STRCASECMP(image, "Image") != 0)
       {
         continue;
       }
-      const char* embeddedTransformToFrame = imageNames->GetNestedElement( i )->GetAttribute( "EmbeddedTransformToFrame" );
-      if ( embeddedTransformToFrame == NULL )
+      const char* embeddedTransformToFrame = imageNames->GetNestedElement(i)->GetAttribute("EmbeddedTransformToFrame");
+      if (embeddedTransformToFrame == NULL)
       {
-        LOG_WARNING( "EmbeddedTransformToFrame attribute of ImageNames/Image element is missing. This element will be ignored." );
+        LOG_WARNING("EmbeddedTransformToFrame attribute of ImageNames/Image element is missing. This element will be ignored.");
         continue;
       }
-      const char* name = imageNames->GetNestedElement( i )->GetAttribute( "Name" );
-      if ( name == NULL )
+      const char* name = imageNames->GetNestedElement(i)->GetAttribute("Name");
+      if (name == NULL)
       {
-        LOG_WARNING( "Name attribute of ImageNames/Image element is missing. This element will be ignored." );
+        LOG_WARNING("Name attribute of ImageNames/Image element is missing. This element will be ignored.");
         continue;
       }
 
       ImageStream stream;
       stream.EmbeddedTransformToFrame = embeddedTransformToFrame;
       stream.Name = name;
-      clientInfo.ImageStreams.push_back( stream );
+      clientInfo.ImageStreams.push_back(stream);
     }
   }
 
   // Get string names
-  vtkXMLDataElement* stringNames = xmldata->FindNestedElementWithName( "StringNames" );
-  if ( stringNames != NULL )
+  vtkXMLDataElement* stringNames = xmldata->FindNestedElementWithName("StringNames");
+  if (stringNames != NULL)
   {
-    for ( int i = 0; i < stringNames->GetNumberOfNestedElements(); ++i )
+    for (int i = 0; i < stringNames->GetNumberOfNestedElements(); ++i)
     {
-      const char* string = stringNames->GetNestedElement( i )->GetName();
-      if ( string == NULL || STRCASECMP( string, "String" ) != 0 )
+      const char* string = stringNames->GetNestedElement(i)->GetName();
+      if (string == NULL || STRCASECMP(string, "String") != 0)
       {
         continue;
       }
-      const char* name = stringNames->GetNestedElement( i )->GetAttribute( "Name" );
-      if ( name == NULL )
+      const char* name = stringNames->GetNestedElement(i)->GetAttribute("Name");
+      if (name == NULL)
       {
-        LOG_WARNING( "In StringNames child element #" << i << " definition is incomplete: required Name attribute is missing" );
+        LOG_WARNING("In StringNames child element #" << i << " definition is incomplete: required Name attribute is missing");
         continue;
       }
-      clientInfo.StringNames.push_back( name );
+      clientInfo.StringNames.push_back(name);
     }
   }
 
   // Copy over the new client info
-  ( *this ) = clientInfo;
+  (*this) = clientInfo;
 
   return PLUS_SUCCESS;
 }
 
 //----------------------------------------------------------------------------
-void PlusIgtlClientInfo::GetClientInfoInXmlData( std::string& strXmlData )
+void PlusIgtlClientInfo::GetClientInfoInXmlData(std::string& strXmlData)
 {
   vtkSmartPointer<vtkXMLDataElement> xmldata = vtkSmartPointer<vtkXMLDataElement>::New();
-  xmldata->SetName( "ClientInfo" );
+  xmldata->SetName("ClientInfo");
 
   vtkSmartPointer<vtkXMLDataElement> messageTypes = vtkSmartPointer<vtkXMLDataElement>::New();
-  messageTypes->SetName( "MessageTypes" );
-  for ( unsigned int i = 0; i < IgtlMessageTypes.size(); ++i )
+  messageTypes->SetName("MessageTypes");
+  for (unsigned int i = 0; i < IgtlMessageTypes.size(); ++i)
   {
     vtkSmartPointer<vtkXMLDataElement> message = vtkSmartPointer<vtkXMLDataElement>::New();
-    message->SetName( "Message" );
-    message->SetAttribute( "Type", IgtlMessageTypes[i].c_str() );
-    messageTypes->AddNestedElement( message );
+    message->SetName("Message");
+    message->SetAttribute("Type", IgtlMessageTypes[i].c_str());
+    messageTypes->AddNestedElement(message);
   }
-  xmldata->AddNestedElement( messageTypes );
+  xmldata->AddNestedElement(messageTypes);
 
   vtkSmartPointer<vtkXMLDataElement> transformNames = vtkSmartPointer<vtkXMLDataElement>::New();
-  transformNames->SetName( "TransformNames" );
-  for ( unsigned int i = 0; i < TransformNames.size(); ++i )
+  transformNames->SetName("TransformNames");
+  for (unsigned int i = 0; i < TransformNames.size(); ++i)
   {
-    if ( ! TransformNames[i].IsValid() )
+    if (! TransformNames[i].IsValid())
     {
       std::string transformName;
-      TransformNames[i].GetTransformName( transformName );
-      LOG_ERROR( "Failed to add transform name to client info - transform name is invalid (" << transformName << ")." );
+      TransformNames[i].GetTransformName(transformName);
+      LOG_ERROR("Failed to add transform name to client info - transform name is invalid (" << transformName << ").");
       continue;
     }
     vtkSmartPointer<vtkXMLDataElement> transform = vtkSmartPointer<vtkXMLDataElement>::New();
-    transform->SetName( "Transform" );
+    transform->SetName("Transform");
     std::string tname;
-    TransformNames[i].GetTransformName( tname );
-    transform->SetAttribute( "Name", tname.c_str() );
-    transformNames->AddNestedElement( transform );
+    TransformNames[i].GetTransformName(tname);
+    transform->SetAttribute("Name", tname.c_str());
+    transformNames->AddNestedElement(transform);
   }
-  xmldata->AddNestedElement( transformNames );
+  xmldata->AddNestedElement(transformNames);
 
   vtkSmartPointer<vtkXMLDataElement> stringNames = vtkSmartPointer<vtkXMLDataElement>::New();
-  stringNames->SetName( "StringNames" );
-  for ( unsigned int i = 0; i < StringNames.size(); ++i )
+  stringNames->SetName("StringNames");
+  for (unsigned int i = 0; i < StringNames.size(); ++i)
   {
-    if ( StringNames[i].empty() )
+    if (StringNames[i].empty())
     {
-      LOG_ERROR( "Failed to add string name to client info - string name is empty" );
+      LOG_ERROR("Failed to add string name to client info - string name is empty");
       continue;
     }
     vtkSmartPointer<vtkXMLDataElement> stringElem = vtkSmartPointer<vtkXMLDataElement>::New();
-    stringElem->SetName( "String" );
-    stringElem->SetAttribute( "Name", StringNames[i].c_str() );
-    stringNames->AddNestedElement( stringElem );
+    stringElem->SetName("String");
+    stringElem->SetAttribute("Name", StringNames[i].c_str());
+    stringNames->AddNestedElement(stringElem);
   }
-  xmldata->AddNestedElement( stringNames );
+  xmldata->AddNestedElement(stringNames);
 
   vtkSmartPointer<vtkXMLDataElement> imageNames = vtkSmartPointer<vtkXMLDataElement>::New();
-  imageNames->SetName( "ImageNames" );
-  for ( unsigned int i = 0; i < ImageStreams.size(); ++i )
+  imageNames->SetName("ImageNames");
+  for (unsigned int i = 0; i < ImageStreams.size(); ++i)
   {
     vtkSmartPointer<vtkXMLDataElement> image = vtkSmartPointer<vtkXMLDataElement>::New();
-    image->SetName( "Image" );
-    image->SetAttribute( "Name", ImageStreams[i].Name.c_str() );
-    image->SetAttribute( "EmbeddedTransformToFrame", ImageStreams[i].EmbeddedTransformToFrame.c_str() );
-    imageNames->AddNestedElement( image );
+    image->SetName("Image");
+    image->SetAttribute("Name", ImageStreams[i].Name.c_str());
+    image->SetAttribute("EmbeddedTransformToFrame", ImageStreams[i].EmbeddedTransformToFrame.c_str());
+    imageNames->AddNestedElement(image);
   }
-  xmldata->AddNestedElement( imageNames );
+  xmldata->AddNestedElement(imageNames);
 
   std::ostringstream os;
-  PlusCommon::PrintXML( os, vtkIndent( 0 ), xmldata );
+  PlusCommon::PrintXML(os, vtkIndent(0), xmldata);
   strXmlData = os.str();
 }
 
 //----------------------------------------------------------------------------
-void PlusIgtlClientInfo::PrintSelf( ostream& os, vtkIndent indent )
+void PlusIgtlClientInfo::PrintSelf(ostream& os, vtkIndent indent)
 {
   os << indent << "IGTL version: " << this->ClientHeaderVersion;
   os << indent << "Message types: ";
-  if ( !this->IgtlMessageTypes.empty() )
+  if (!this->IgtlMessageTypes.empty())
   {
-    for ( unsigned int i = 0; i < this->IgtlMessageTypes.size(); ++i )
+    for (unsigned int i = 0; i < this->IgtlMessageTypes.size(); ++i)
     {
-      if ( i > 0 )
+      if (i > 0)
       {
         os << ", ";
       }
@@ -247,16 +252,16 @@ void PlusIgtlClientInfo::PrintSelf( ostream& os, vtkIndent indent )
   }
 
   os << ". Transforms: ";
-  if ( !this->TransformNames.empty() )
+  if (!this->TransformNames.empty())
   {
-    for ( unsigned int i = 0; i < this->TransformNames.size(); ++i )
+    for (unsigned int i = 0; i < this->TransformNames.size(); ++i)
     {
-      if ( i > 0 )
+      if (i > 0)
       {
         os << ", ";
       }
       std::string tn;
-      this->TransformNames[i].GetTransformName( tn );
+      this->TransformNames[i].GetTransformName(tn);
       os << tn;
     }
   }
@@ -266,12 +271,12 @@ void PlusIgtlClientInfo::PrintSelf( ostream& os, vtkIndent indent )
   }
 
   os << ". Strings: ";
-  if ( !this->StringNames.empty() )
+  if (!this->StringNames.empty())
   {
     std::ostringstream stringNames;
-    for ( unsigned int i = 0; i < this->StringNames.size(); ++i )
+    for (unsigned int i = 0; i < this->StringNames.size(); ++i)
     {
-      if ( i > 0 )
+      if (i > 0)
       {
         os << ", ";
       }
@@ -284,12 +289,12 @@ void PlusIgtlClientInfo::PrintSelf( ostream& os, vtkIndent indent )
   }
 
   os << ". Images: ";
-  if ( !this->ImageStreams.empty() )
+  if (!this->ImageStreams.empty())
   {
     std::ostringstream imageNames;
-    for ( unsigned int i = 0; i < this->ImageStreams.size(); ++i )
+    for (unsigned int i = 0; i < this->ImageStreams.size(); ++i)
     {
-      if ( i > 0 )
+      if (i > 0)
       {
         os << ", ";
       }
@@ -309,7 +314,7 @@ int PlusIgtlClientInfo::GetClientHeaderVersion() const
 }
 
 //----------------------------------------------------------------------------
-void PlusIgtlClientInfo::SetClientHeaderVersion( int version )
+void PlusIgtlClientInfo::SetClientHeaderVersion(int version)
 {
   this->ClientHeaderVersion = version;
 }
