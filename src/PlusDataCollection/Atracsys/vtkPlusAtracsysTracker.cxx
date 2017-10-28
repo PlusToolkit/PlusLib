04e2e3fe75238a42a55475217fc5cce1aa47d5c3
Mark Asselin <mark.asselin95@gmail.com>
2017-10-28 17:56:33 -0400

    re #241: Added loading of Atracsys geometry files

--- src/PlusDataCollection/Atracsys/vtkPlusAtracsysTracker.cxx
+++ src/PlusDataCollection/Atracsys/vtkPlusAtracsysTracker.cxx
@@ -16,6 +16,7 @@ See License.txt for details.
 // System includes
 #include <string>
 #include <map>
+#include <fstream>
 
 // Atracsys includes
 #include "ftkErrors.h"
@@ -25,8 +26,255 @@ See License.txt for details.
 #include "ftkPlatform.h"
 #include "ftkTypes.h"
 
+#define ENABLE_ONBOARD_PROCESSING_OPTION 6000
+#define SENDING_IMAGES_OPTION 6003
+
+
 vtkStandardNewMacro(vtkPlusAtracsysTracker);
 
+namespace
+{
+  class IniFile
+  {
+  protected:
+
+    long findEOL(char& c, char* addr, size_t size)
+    {
+      for (long u = 0; u < (long)size; u++)
+      {
+        c = addr[u];
+        if (c == 0 || c == '\n') // note that MAX may only have a '\r'
+        {
+          return u;
+        }
+      }
+      return -1;
+    }
+    bool parseLine(std::string& line)
+    {
+      size_t first_bracket = line.find_first_of("["),
+        last_bracket = line.find_last_of("]"),
+        equal = line.find_first_of("=");
+
+      if (first_bracket != std::string::npos &&
+        last_bracket != std::string::npos)
+      {
+        // Found section
+        _currentSection = line.substr(first_bracket + 1,
+          last_bracket - first_bracket - 1);
+        sections[_currentSection] = KeyValues();
+      }
+      else
+        if (equal != std::string::npos && _currentSection != "")
+        {
+          // Found property in a section
+          std::string key = line.substr(0, equal),
+            val = line.substr(equal + 1);
+          sections[_currentSection][key] = val;
+        }
+        else
+        {
+          // If the line is empty, just skip it, if not and is a comment, just
+          // skip it
+          // as well, otherwise the parsing cannot be done.
+          line.erase(remove_if(line.begin(),
+            line.end(), isspace), line.end());
+          if (!line.empty() && line.substr(0, 1) != ";")
+          {
+            return false;
+          }
+        }
+      return true;
+    }
+    std::string _currentSection;
+
+  public:
+
+    typedef std::map< std::string, std::string > KeyValues;
+    typedef std::map< std::string, KeyValues > Sections;
+
+    Sections sections;
+
+    bool parse(char* addr, size_t size)
+    {
+      sections.clear();
+      _currentSection = "";
+
+      std::string strLine;
+
+      while (size)
+      {
+        char c;
+        long lineSize = findEOL(c, addr, size);
+
+        if (lineSize != 0)
+        {
+          if (lineSize > 0)
+          {
+            strLine = std::string(addr, lineSize);
+          }
+          else
+          {
+            strLine = std::string(addr);
+          }
+
+          strLine.erase(remove(strLine.begin(),
+            strLine.end(), '\r'), strLine.end());
+          if (!parseLine(strLine))
+          {
+            return false;
+          }
+
+          if (lineSize < 0)
+          {
+            return true; // EOF at the end of the line
+          }
+        }
+        if (c == 0 || size == size_t(lineSize))
+        {
+          return true; // !!! eof not reached
+        }
+        addr += lineSize + 1;
+        size -= lineSize + 1;
+      }
+      return true;
+    } // Return false in case of syntax error
+    bool save(std::string str)
+    {
+      FILE* file = fopen(str.c_str(), "wb");
+      if (!file)
+      {
+        return false;
+      }
+
+      Sections::iterator iterS = sections.begin();
+
+      while (iterS != sections.end())
+      {
+        fprintf(file, "[%s]\n", iterS->first.c_str());
+
+        KeyValues& kw = iterS->second;
+        KeyValues::iterator iterK = kw.begin();
+
+        while (iterK != kw.end())
+        {
+          fprintf(file, "%s=%s\n",
+            iterK->first.c_str(), iterK->second.c_str());
+          iterK++;
+        }
+        iterS++;
+      }
+      fclose(file);
+      return true;
+    }
+    bool toBuffer(char** out, size_t& outSize)
+    {
+      if (!out)
+      {
+        return false;
+      }
+
+      std::string buffer;
+      char temp[1024];
+
+      Sections::iterator iterS = sections.begin();
+
+      while (iterS != sections.end())
+      {
+        sprintf(temp, "[%s]\n", iterS->first.c_str());
+        buffer += temp;
+
+        KeyValues& kw = iterS->second;
+        KeyValues::iterator iterK = kw.begin();
+
+        while (iterK != kw.end())
+        {
+          sprintf(temp, "%s=%s\n",
+            iterK->first.c_str(), iterK->second.c_str());
+          buffer += temp;
+          iterK++;
+        }
+        iterS++;
+      }
+
+      outSize = buffer.length() + 1;
+      char* str = new (std::nothrow) char[outSize];
+
+      if (!str)
+      {
+        return false;
+      }
+
+      strncpy(str, buffer.c_str(), outSize - 1);
+      str[outSize - 1] = 0;
+      *out = str;
+
+      return true;
+    } // Buffer must be unallocated by user
+  };
+
+  bool checkSection(IniFile& p, const std::string& section)
+  {
+    if (p.sections.find(section) == p.sections.end())
+    {
+      std::cerr << "Cannot find section \"" << section << "\"" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  // ----------------------------------------------------------------------------
+
+  bool checkKey(IniFile& p, const std::string& section, const std::string& key)
+  {
+    if (p.sections[section].find(key) == p.sections[section].end())
+    {
+      std::cerr << "Cannot find key \"" << key << "\" in section \""
+        << section << "\"" << std::endl;
+      return false;
+    }
+
+    return true;
+  }
+
+  // ----------------------------------------------------------------------------
+
+  bool assignUint32(IniFile& p, const std::string& section,
+    const std::string& key,
+    uint32* variable)
+  {
+    if (!checkKey(p, section, key))
+    {
+      return false;
+    }
+
+    char* pEnd;
+    std::string val(p.sections[section][key]);
+
+    *variable = uint32(strtol(val.c_str(), &pEnd, 10));
+
+    return true;
+  }
+
+  // ----------------------------------------------------------------------------
+
+  bool assignFloatXX(IniFile& p, const std::string& section,
+    const std::string& key,
+    floatXX* variable)
+  {
+    if (!checkKey(p, section, key))
+    {
+      return false;
+    }
+
+    char* pEnd;
+
+    *variable =
+      floatXX(strtod(p.sections[section][key].c_str(), &pEnd));
+
+    return true;
+  }
+}
 //----------------------------------------------------------------------------
 class vtkPlusAtracsysTracker::vtkInternal
 {
@@ -42,8 +290,6 @@ public:
   {
   }
 
-
-
   // matches tool id to .ini geometry file names/paths
   std::map<std::string, std::string> IdMappedToGeometryFilename;
 
@@ -58,8 +304,8 @@ public:
 
   std::string GetFtkErrorString();
 
-  //DeviceData Device;
-  //void DeviceEnumCallback(uint64 sn, void* user, ftkDeviceType type);
+  PlusStatus LoadFtkGeometry(const std::string& filename, ftkGeometry& geom);
+  bool LoadIniFile(std::ifstream& is, ftkGeometry& geometry);
 };
 
 //----------------------------------------------------------------------------
@@ -82,6 +328,115 @@ struct DeviceData
   uint64 SerialNumber;
   ftkDeviceType Type;
 };
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusAtracsysTracker::vtkInternal::LoadFtkGeometry(const std::string& filename, ftkGeometry& geom)
+{
+  std::ifstream input;
+  input.open(filename.c_str());
+
+  if (!input.fail() && this->LoadIniFile(input, geom))
+  {
+    return PLUS_FAIL;
+  }
+  else
+  {
+    ftkBuffer buffer;
+    buffer.reset();
+    if (ftkGetData(this->ftkLib, this->TrackerSN, FTK_OPT_DATA_DIR, &buffer) != FTK_OK || buffer.size < 1u)
+    {
+      return PLUS_FAIL;
+    }
+
+    std::string fullFile(reinterpret_cast< char* >(buffer.data));
+    fullFile += "\\" + filename;
+
+    input.open(fullFile.c_str());
+
+    if (!input.fail() && this->LoadIniFile(input, geom))
+    {
+      return PLUS_SUCCESS;
+    }
+  }
+
+  return PLUS_FAIL;
+}
+
+//----------------------------------------------------------------------------
+bool vtkPlusAtracsysTracker::vtkInternal::LoadIniFile(std::ifstream& is, ftkGeometry& geometry)
+{
+  std::string line, fileContent("");
+
+  while (!is.eof())
+  {
+    getline(is, line);
+    fileContent += line + "\n";
+  }
+
+  IniFile parser;
+
+  if (!parser.parse(const_cast< char* >(fileContent.c_str()),
+    fileContent.size()))
+  {
+    return false;
+  }
+
+  if (!checkSection(parser, "geometry"))
+  {
+    return false;
+  }
+
+  uint32 tmp;
+
+  if (!assignUint32(parser, "geometry", "count", &tmp))
+  {
+    return false;
+  }
+  geometry.version = 0u;
+  geometry.pointsCount = tmp;
+  if (!assignUint32(parser, "geometry", "id", &geometry.geometryId))
+  {
+    return false;
+  }
+
+  std::cout << "Loading geometry " << geometry.geometryId << ", composed of "
+    << geometry.pointsCount << " fiducials" << std::endl;
+
+  char sectionName[10u];
+
+  for (uint32 i(0u); i < geometry.pointsCount; ++i)
+  {
+    sprintf(sectionName, "fiducial%u", i);
+
+    if (!checkSection(parser, sectionName))
+    {
+      return false;
+    }
+
+    if (!assignFloatXX(parser, sectionName, "x",
+      &geometry.positions[i].x))
+    {
+      return false;
+    }
+    if (!assignFloatXX(parser, sectionName, "y",
+      &geometry.positions[i].y))
+    {
+      return false;
+    }
+    if (!assignFloatXX(parser, sectionName, "z",
+      &geometry.positions[i].z))
+    {
+      return false;
+    }
+
+    std::cout << "Loaded fiducial " << i << " ("
+      << geometry.positions[i].x << ", "
+      << geometry.positions[i].y << ", "
+      << geometry.positions[i].z << ")" << std::endl;
+  }
+
+  return true;
+}
 
 //----------------------------------------------------------------------------
 void fusionTrackEnumerator(uint64 sn, void* user, ftkDeviceType devType)
@@ -156,7 +511,7 @@ PlusStatus vtkPlusAtracsysTracker::ReadConfiguration(vtkXMLDataElement* rootConf
     }
     else
     {
-      LOG_ERROR("Failed to initialize Atracsys tool " << toolId << ": GeometryFile is missing.")
+LOG_ERROR("Failed to initialize Atracsys tool " << toolId << ": GeometryFile is missing.")
     }
   }
 
@@ -226,10 +581,43 @@ PlusStatus vtkPlusAtracsysTracker::InternalConnect()
   }
 
   this->Internal->TrackerSN = device.SerialNumber;
-  
+
   LOG_INFO("Detected " << deviceType << " with serial number 0x" << std::setw(16u)
     << std::setfill('0') << std::hex << device.SerialNumber << std::dec
     << std::endl << std::setfill('\0'));
+
+  // set spryTrack to do onboard image processing
+  if (DEV_SPRYTRACK_180 == device.Type)
+  {
+    cout << "Enable onboard processing" << endl;
+    if (ftkSetInt32(this->Internal->ftkLib, this->Internal->TrackerSN, ENABLE_ONBOARD_PROCESSING_OPTION, 1) != FTK_OK)
+    {
+      LOG_ERROR("Cannot process data directly on the SpryTrack.");
+      return PLUS_FAIL;
+    }
+
+    cout << "Disable images sending" << endl;
+    if (ftkSetInt32(this->Internal->ftkLib, this->Internal->TrackerSN, SENDING_IMAGES_OPTION, 0) != FTK_OK)
+    {
+      LOG_ERROR("Cannot disable images sending on the SpryTrack.");
+      return PLUS_FAIL;
+    }
+  }
+
+  // load geometries onto Atracsys
+  std::map<std::string, std::string>::iterator it;
+  for (it = begin(this->Internal->IdMappedToGeometryFilename); it != end(this->Internal->IdMappedToGeometryFilename); it++)
+  {
+    ftkGeometry geom;
+    std::string geomFilePath = vtkPlusConfig::GetInstance()->GetDeviceSetConfigurationPath(it->second);
+    this->Internal->LoadFtkGeometry(geomFilePath, geom);
+    if (ftkSetGeometry(this->Internal->ftkLib, this->Internal->TrackerSN, &geom) != FTK_OK)
+    {
+      LOG_ERROR("Failed to load geometry " << it->second);
+      LOG_ERROR(this->Internal->GetFtkErrorString());
+    }
+    //cout << it->first << " " << it->second;
+  }
 
   return PLUS_SUCCESS;
 }
