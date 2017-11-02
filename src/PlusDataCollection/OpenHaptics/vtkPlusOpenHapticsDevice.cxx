510f45f332896eff55f16725afe14fdef04227c5
Sam Horvath <sam.horvath@kitware.com>
2017-11-02 15:03:32 -0400

    ENH:  Added forces to Open Haptics Device

--- src/PlusDataCollection/OpenHaptics/vtkPlusOpenHapticsDevice.cxx
+++ src/PlusDataCollection/OpenHaptics/vtkPlusOpenHapticsDevice.cxx
@@ -7,7 +7,6 @@ See License.txt for details.
 
 // Local includes
 #include "PlusConfigure.h"
-#include "PlusVideoFrame.h"
 #include "vtkPlusDataSource.h"
 #include "vtkPlusOpenHapticsDevice.h"
 
@@ -17,16 +16,12 @@ See License.txt for details.
 #include <HD/hd.h>
 
 // VTK includes
-#include <vtkImageData.h>
-#include <vtkImageImport.h>
 #include <vtkMatrix4x4.h>
-#include <vtkObjectFactory.h>
 #include <vtkTransform.h>
 
 // STL includes
 #include <fstream>
 #include <iostream>
-#include <set>
 #include <sstream>
 
 vtkStandardNewMacro(vtkPlusOpenHapticsDevice);
@@ -95,6 +90,23 @@ PlusStatus vtkPlusOpenHapticsDevice::InternalConnect()
     return PLUS_SUCCESS;
   }
 
+  if (this->InputChannels.empty())
+  {
+    LOG_WARNING("No force input has been provided");
+  }
+  else
+  {
+    if (this->InputChannels.size() > 1)
+    {
+      LOG_WARNING("Multiple input channels present.  Only first channel will be checked for force data");
+    }
+    vtkPlusDataSource* forceInput;
+    if (!this->InputChannels[0]->GetOwnerDevice()->GetToolByPortName("Force", forceInput))
+    {
+      LOG_WARNING("No Force tool in input channel.  Forces will not be available");
+    }
+  }
+
   HDErrorInfo errorFlush;
   while (HD_DEVICE_ERROR(errorFlush = hdGetError())) {}
 
@@ -107,6 +119,10 @@ PlusStatus vtkPlusOpenHapticsDevice::InternalConnect()
     DeviceHandle = -1;
     return PLUS_FAIL;
   }
+
+  // Enable forces
+  hdEnable(HD_FORCE_OUTPUT);
+  hdEnable(HD_FORCE_RAMPING);
 
   this->isHapticDeviceInitialized = true;
   LOG_INFO("Phantom initialized: " << this->DeviceName)
@@ -130,7 +146,6 @@ PlusStatus vtkPlusOpenHapticsDevice::InternalDisconnect()
 PlusStatus vtkPlusOpenHapticsDevice::ReadConfiguration(vtkXMLDataElement* rootConfigElement)
 {
   XML_FIND_DEVICE_ELEMENT_REQUIRED_FOR_READING(deviceConfig, rootConfigElement);
-
   XML_READ_CSTRING_ATTRIBUTE_REQUIRED(DeviceName,deviceConfig);
   return PLUS_SUCCESS;
 }
@@ -170,26 +185,73 @@ vtkPlusOpenHapticsDevice::positionCallback(void* pData)
   const double unfilteredTimestamp = vtkPlusAccurateTimer::GetSystemTime();
   ++client->FrameNumber;
 
+  HDdouble force[3];
+  force[0] = 0;
+  force[1] = 0;
+  force[2] = 0;
+
+  //Transform to RAS coordinate system
+  vtkSmartPointer<vtkMatrix4x4> rasCorrection = vtkSmartPointer<vtkMatrix4x4>::New();
+  rasCorrection->Identity();
+  rasCorrection->SetElement(0, 0, -1);
+  rasCorrection->SetElement(1, 1, 0);
+  rasCorrection->SetElement(1, 2, 1);
+  rasCorrection->SetElement(2, 2, 0);
+  rasCorrection->SetElement(2, 1, 1);
+
+  //assemble force data
+  auto inputChannels = client->InputChannels;
+  vtkPlusDataSource* forceInput;
+  if (!inputChannels.empty() && inputChannels[0]->GetOwnerDevice()->GetToolByPortName("Force", forceInput))
+  {
+    StreamBufferItem* item = new StreamBufferItem();
+    if (forceInput->GetLatestItemUidInBuffer() > 0)
+    {
+      forceInput->GetLatestStreamBufferItem(item);
+      vtkSmartPointer<vtkMatrix4x4> forceMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
+      item->GetMatrix(forceMatrix);
+      vtkMatrix4x4::Multiply4x4(rasCorrection, forceMatrix, forceMatrix);
+      force[0] = forceMatrix->GetElement(0,3);
+      force[1] = forceMatrix->GetElement(1,3);
+      force[2] = forceMatrix->GetElement(2,3);
+    }
+    delete item;
+  }
+  else
+  {
+    LOG_TRACE("No force data available");
+  }
+
   //Current position data is pulled here
   HDdouble trans[16];
   HDdouble pos[3];  
+  HDdouble vel[3];
   hdBeginFrame(handle);
   hdMakeCurrentDevice(handle);  
   hdGetDoublev(HD_CURRENT_TRANSFORM, trans);
   hdGetDoublev(HD_CURRENT_POSITION, pos);
+  hdGetDoublev(HD_CURRENT_VELOCITY, vel);
+  hdSetDoublev(HD_CURRENT_FORCE, force);
   hdEndFrame(handle);
 
-  vtkPlusDataSource* tool = NULL;
+  vtkPlusDataSource* stylus = NULL;
+  vtkPlusDataSource* velocity = NULL;
 
-  if (client->GetToolByPortName("Stylus", tool) != PLUS_SUCCESS)
+  if (client->GetToolByPortName("Stylus", stylus) != PLUS_SUCCESS)
   {
     LOG_ERROR("Stylus not found!");    
   }
 
+  if (client->GetToolByPortName("StylusVelocity", velocity) != PLUS_SUCCESS)
+  {
+    LOG_ERROR("Stylus velocity not found!");
+  }
+
   vtkSmartPointer<vtkTransform> transform = vtkSmartPointer<vtkTransform>::New();
-  vtkSmartPointer<vtkMatrix4x4> finalMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
-  vtkSmartPointer<vtkMatrix4x4> rasCorrection = vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkSmartPointer<vtkMatrix4x4> toolMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
   vtkSmartPointer<vtkTransform> rotation = vtkSmartPointer<vtkTransform>::New();
+  vtkSmartPointer<vtkTransform> velTrans = vtkSmartPointer<vtkTransform>::New();
+  vtkSmartPointer<vtkMatrix4x4> velMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
   double orient[3];
 
   //Arrange transformations in correct order
@@ -200,20 +262,14 @@ vtkPlusOpenHapticsDevice::positionCallback(void* pData)
   transform->RotateX(-1* orient[0] + 180);
   transform->RotateY(orient[1]);
   transform->RotateZ(orient[2]);  
-  transform->GetMatrix(finalMatrix);
+  transform->GetMatrix(toolMatrix);
+  velTrans->Translate(vel);
+  velTrans->GetMatrix(velMatrix);
+  vtkMatrix4x4::Multiply4x4(rasCorrection, velMatrix, velMatrix);
+  vtkMatrix4x4::Multiply4x4(rasCorrection,toolMatrix,toolMatrix);
 
-  //Transform to RAS coordinate system
-  rasCorrection->Identity();
-  rasCorrection->SetElement(0,0,-1);
-  rasCorrection->SetElement(1,1,0);
-  rasCorrection->SetElement(1,2,1);
-  rasCorrection->SetElement(2,2,0);
-  rasCorrection->SetElement(2,1,1);
-  vtkMatrix4x4::Multiply4x4(rasCorrection,finalMatrix,finalMatrix);
-
-  
-  client->ToolTimeStampedUpdate(tool->GetId(), finalMatrix, TOOL_OK, client->FrameNumber,unfilteredTimestamp);
-
+  client->ToolTimeStampedUpdate(velocity->GetId(), velMatrix, TOOL_OK, client->FrameNumber, unfilteredTimestamp);
+  client->ToolTimeStampedUpdate(stylus->GetId(), toolMatrix, TOOL_OK, client->FrameNumber,unfilteredTimestamp);
   return HD_CALLBACK_DONE;
 }
 
