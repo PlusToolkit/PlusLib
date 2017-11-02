646f3d3689f3159178cd7e75a8743ff9f8685994
Ben Church <benchurch628@gmail.com>
2017-11-02 17:05:18 -0400

    re #259 Changed LOG_ERROR message under help call to std::cout, like in the rest of the tools' source files, help text no longer returned as error

--- src/PlusImageProcessing/Tools/EnhanceUsTrpSequence.cxx
+++ src/PlusImageProcessing/Tools/EnhanceUsTrpSequence.cxx
@@ -41,8 +41,8 @@ int main(int argc, char **argv)
 
   if (printHelp)
   {
-    //std::cout << args.GetHelp() << std::endl;
-    LOG_ERROR(args.GetHelp());
+    std::cout << args.GetHelp() << std::endl;
+    //LOG_ERROR(args.GetHelp());
     return EXIT_SUCCESS;
   }
 
