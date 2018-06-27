fcd5f2b9e6f89e1b040387a02f1649c29c9ec8b4
Ole Vegard Solberg <OleVegard.Solberg@sintef.no>
2018-06-27 11:38:32 +0100

    Test code setting igtio US parameter values as meta data. Actually this quick solutuion sends all parameters in vtkPlusUsDevice::FrameFields as meta data in the image message.

--- src/PlusOpenIGTLink/vtkPlusIgtlMessageFactory.cxx
+++ src/PlusOpenIGTLink/vtkPlusIgtlMessageFactory.cxx
@@ -192,6 +192,26 @@ PlusStatus vtkPlusIgtlMessageFactory::PackMessages(const PlusIgtlClientInfo& cli
           numberOfErrors++;
           continue;
         }
+
+
+        // Test code for sending US image parameters as meta data.
+        // This will put all string messages into the image meta data, in addition to sending them as separate string messages.
+        // We could probably just create these parameters as a separate list instead.
+        // The parameters could probably be added to vtkPlusUsImagingParameters of that makes more sense.
+        for (std::vector< std::string >::const_iterator stringNameIterator = clientInfo.StringNames.begin(); stringNameIterator != clientInfo.StringNames.end(); ++stringNameIterator)
+        {
+          const char* stringName = stringNameIterator->c_str();
+          const char* stringValue = trackedFrame.GetCustomFrameField(stringName);
+          if (stringValue == NULL)
+          {
+            // no value is available, do not send anything
+            continue;
+          }
+          imageMessage->SetMetaDataElement(stringName, IANA_TYPE_US_ASCII, stringValue);
+        }
+
+
+
         igtlMessages.push_back(imageMessage.GetPointer());
       }
     }
