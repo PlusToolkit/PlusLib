3abb184e2799b42c79c98cd16fd7450e0043a3a0
Adam Rankin <adam.rankin@gmail.com>
2018-11-27 12:12:16 -0500

    Adding version info macro that attaches details to libraries and applications built under MSVC (#447)
    Resolves #113

--- CMake/PlusLibMacros.cmake
+++ CMake/PlusLibMacros.cmake
@@ -14,3 +14,34 @@ MACRO(PlusLibInstallLibrary _target_name _variable_root)
       )
   ENDIF()
 ENDMACRO()
+
+MACRO(PlusLibAddVersionInfo target_name description internal_name product_name)
+  IF(MSVC)
+    IF(NOT TARGET ${target_name})
+      MESSAGE(FATAL_ERROR PlusLibAddVersionInfo called but target parameter does not exist)
+    ENDIF()
+
+    # Configure file does not see these variables unless we re-set them locally
+    set(target_name ${target_name})
+    set(description ${description})
+    set(internal_name ${internal_name})
+    set(product_name ${product_name})
+
+    CONFIGURE_FILE(
+      ${CMAKE_SOURCE_DIR}/CMake/MSVCVersion.rc.in 
+      ${CMAKE_CURRENT_BINARY_DIR}/${target_name}MSVCVersion.rc
+      )
+
+    GET_TARGET_PROPERTY(_target_type ${target_name} TYPE)
+    IF(${_target_type} STREQUAL "EXECUTABLE")
+      TARGET_SOURCES(${target_name} PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/${target_name}MSVCVersion.rc)
+    ELSE()
+      TARGET_SOURCES(${target_name} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/${target_name}MSVCVersion.rc)
+    ENDIF()
+
+    # If this macro was called from the target they're currently configuring
+    IF(${PROJECT_NAME} STREQUAL ${target_name} OR vtk${PROJECT_NAME} STREQUAL ${target_name})
+      SOURCE_GROUP(Resources FILES ${CMAKE_CURRENT_BINARY_DIR}/${target_name}MSVCVersion.rc)
+    ENDIF()
+  ENDIF()
+ENDMACRO()
