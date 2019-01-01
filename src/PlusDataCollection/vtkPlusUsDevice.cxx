2503fe99eb875a5cc9bc9eac9d6b262df9b05fdb
Adam Rankin <adam.rankin@gmail.com>
2019-01-01 12:37:02 -0500

    Added generic parameter (string, string) map to vtkPlusDevice (#479)
    * Added generic parameter (string, string) map to vtkPlusDevice
    
    Catch US imaging devices in vtkPlusUsDevice before they go to the base class in order to re-use existing system
    
    Minor cleanup of code
    
    * Update vtkPlusUsDevice.cxx

--- src/PlusDataCollection/vtkPlusUsDevice.cxx
+++ src/PlusDataCollection/vtkPlusUsDevice.cxx
@@ -20,8 +20,8 @@ vtkStandardNewMacro(vtkPlusUsDevice);
 //----------------------------------------------------------------------------
 vtkPlusUsDevice::vtkPlusUsDevice()
   : vtkPlusDevice()
-  , ImagingParameters( vtkPlusUsImagingParameters::New() )
-  , ImageToTransducerTransformName( NULL )
+  , ImagingParameters(vtkPlusUsImagingParameters::New())
+  , ImageToTransducerTransformName("")
 {
   this->CurrentTransducerOriginPixels[0] = 0;
   this->CurrentTransducerOriginPixels[1] = 0;
@@ -43,7 +43,8 @@ void vtkPlusUsDevice::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
   os << indent << "Imaging parameters: " << std::endl;
-  this->ImagingParameters->PrintSelf( os, indent );
+  this->ImagingParameters->PrintSelf(os, indent);
+  os << indent << "ImageToTransducerTransformName: " << this->ImageToTransducerTransformName << std::endl;
 }
 
 //----------------------------------------------------------------------------
@@ -56,18 +57,17 @@ PlusStatus vtkPlusUsDevice::ReadConfiguration(vtkXMLDataElement* rootConfigEleme
     return PLUS_FAIL;
   }
 
-  XML_READ_CSTRING_ATTRIBUTE_OPTIONAL(ImageToTransducerTransformName, deviceConfig);
-  const char* transformName = deviceConfig->GetAttribute("ImageToTransducerTransformName");
-  if (transformName != NULL && this->ImageToTransducerTransform.SetTransformName(transformName) != PLUS_SUCCESS)
+  XML_READ_STRING_ATTRIBUTE_OPTIONAL(ImageToTransducerTransformName, deviceConfig);
+  if (!this->ImageToTransducerTransformName.empty() && this->ImageToTransducerTransform.SetTransformName(this->ImageToTransducerTransformName) != PLUS_SUCCESS)
   {
     LOG_ERROR("Transform name is not properly formatted. It should be of the format <From>ToTransducer.");
     this->SetImageToTransducerTransformName(NULL);
   }
-  XML_FIND_NESTED_ELEMENT_OPTIONAL(imagingParams, deviceConfig, vtkPlusUsImagingParameters::XML_ELEMENT_TAG);
 
+  XML_FIND_NESTED_ELEMENT_OPTIONAL(imagingParams, deviceConfig, vtkPlusUsImagingParameters::XML_ELEMENT_TAG);
   if (imagingParams != NULL)
   {
-    this->ImagingParameters->ReadConfiguration( deviceConfig );
+    this->ImagingParameters->ReadConfiguration(deviceConfig);
 
     if (this->InternalApplyImagingParameterChange() == PLUS_FAIL)
     {
@@ -84,15 +84,15 @@ PlusStatus vtkPlusUsDevice::WriteConfiguration(vtkXMLDataElement* deviceConfig)
 {
   XML_FIND_NESTED_ELEMENT_CREATE_IF_MISSING(imagingParams, deviceConfig, vtkPlusUsImagingParameters::XML_ELEMENT_TAG);
 
-  XML_WRITE_CSTRING_ATTRIBUTE_IF_NOT_NULL(ImageToTransducerTransformName, deviceConfig);
+  XML_WRITE_STRING_ATTRIBUTE_IF_NOT_EMPTY(ImageToTransducerTransformName, deviceConfig);
 
-  return this->ImagingParameters->WriteConfiguration( deviceConfig );
+  return this->ImagingParameters->WriteConfiguration(deviceConfig);
 }
 
 //----------------------------------------------------------------------------
 PlusStatus vtkPlusUsDevice::SetNewImagingParameters(const vtkPlusUsImagingParameters& newImagingParameters)
 {
-  if( this->ImagingParameters->DeepCopy( newImagingParameters ) == PLUS_FAIL )
+  if (this->ImagingParameters->DeepCopy(newImagingParameters) == PLUS_FAIL)
   {
     LOG_ERROR("Unable to deep copy new imaging parameters");
     return PLUS_FAIL;
@@ -166,6 +166,109 @@ PlusStatus vtkPlusUsDevice::AddVideoItemToVideoSources(const std::vector<vtkPlus
   }
 
   return Superclass::AddVideoItemToVideoSources(videoSources, imageDataPtr, usImageOrientation, frameSizeInPx, pixelType, numberOfScalarComponents, imageType, numberOfBytesToSkip, frameNumber, unfilteredTimestamp, filteredTimestamp, &localCustomFields);
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusUsDevice::SetParameter(const std::string& key, const std::string& value)
+{
+  // Catch any vtkPlusUsImagingParameters known params before sending to base class
+  if (this->IsKnownKey(key))
+  {
+    return this->ImagingParameters->SetValue(key, value);
+  }
+
+  return Superclass::SetParameter(key, value);
+}
+
+//----------------------------------------------------------------------------
+std::string vtkPlusUsDevice::GetParameter(const std::string& key) const
+{
+  if (this->IsKnownKey(key))
+  {
+    std::string value;
+    if (this->ImagingParameters->GetValue<std::string>(key, value) != PLUS_SUCCESS)
+    {
+      return "";
+    }
+    return value;
+  }
+
+  return Superclass::GetParameter(key);
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusUsDevice::GetParameter(const std::string& key, std::string& outValue) const
+{
+  if (this->IsKnownKey(key))
+  {
+    return this->ImagingParameters->GetValue<std::string>(key, outValue);
+  }
+
+  return Superclass::GetParameter(key, outValue);
+}
+
+//----------------------------------------------------------------------------
+bool vtkPlusUsDevice::IsKnownKey(const std::string& queryKey) const
+{
+  if (igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_FREQUENCY) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_DEPTH) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_DYNRANGE) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_GAIN) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_TGC) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_INTENSITY) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_CONTRAST) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_SECTOR) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_ZOOM) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_SOUNDVELOCITY) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_VOLTAGE) ||
+      igsioCommon::IsEqualInsensitive(queryKey, vtkPlusUsImagingParameters::KEY_IMAGESIZE))
+  {
+    return true;
+  }
+
+  return false;
+}
+
+//----------------------------------------------------------------------------
+IGTLIO_PROBE_TYPE vtkPlusUsDevice::GetProbeType()
+{
+  return UNKNOWN;
+}
+
+//----------------------------------------------------------------------------
+std::vector<double> vtkPlusUsDevice::CalculateOrigin()
+{
+  return std::vector<double>();
+}
+
+//----------------------------------------------------------------------------
+std::vector<double> vtkPlusUsDevice::CalculateAngles()
+{
+  return std::vector<double>();
+}
+
+//----------------------------------------------------------------------------
+std::vector<double> vtkPlusUsDevice::CalculateBoundingBox()
+{
+  return std::vector<double>();
+}
+
+//----------------------------------------------------------------------------
+std::vector<double> vtkPlusUsDevice::CalculateDepths()
+{
+  return std::vector<double>();
+}
+
+//----------------------------------------------------------------------------
+double vtkPlusUsDevice::CalculateLinearWidth()
+{
+  return 0;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusUsDevice::InternalApplyImagingParameterChange()
+{
+  return PLUS_FAIL;
 }
 
 //----------------------------------------------------------------------------
