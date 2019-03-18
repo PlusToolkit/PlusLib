feba7a942373bd219f4e7e42e16d5ef372b02960
zoez526 <zoezhao1024@gmail.com>
2019-03-18 15:08:21 -0700

    bug fixing, compiled

--- src/PlusDataCollection/Testing/vtkClariusTest.cxx
+++ src/PlusDataCollection/Testing/vtkClariusTest.cxx
@@ -5,13 +5,8 @@
 =========================================================Plus=header=end*/
 
 /*!
-  \file vtkTelemedVideoSourceTest.cxx
-  \brief Test basic connection to the Telemed ultrasound probe
-
-  If the --rendering-off switch is defined then the connection is established, images are
-  transferred for a few seconds, then the connection is closed (useful for automatic testing).
-  If the --rendering-off switch is not defined then the live ultrasound image is displayed
-  in a window (useful for quick interactive testing of the image transfer).
+  \file vtkClariusTest.cxx
+  \brief Test basic connection to the Clarius ultrasound probe
   \ingroup PlusLibDataCollection
 */
 #include "PlusConfigure.h"
@@ -42,8 +37,8 @@
 
 enum DisplayMode
 {
-  SHOW_IMAGE,
-  SHOW_PLOT
+	SHOW_IMAGE,
+	SHOW_PLOT
 };
 
 //----------------------------------------------------------------------------
@@ -51,87 +46,87 @@ enum DisplayMode
 class vtkExtractImageRow : public vtkTableAlgorithm
 {
 public:
-  static vtkExtractImageRow* New();
-  vtkTypeMacro(vtkExtractImageRow,vtkTableAlgorithm);
-  void PrintSelf(ostream& os, vtkIndent indent)
-  {
-    this->Superclass::PrintSelf(os, indent);
-  }
+	static vtkExtractImageRow* New();
+	vtkTypeMacro(vtkExtractImageRow, vtkTableAlgorithm);
+	void PrintSelf(ostream& os, vtkIndent indent)
+	{
+		this->Superclass::PrintSelf(os, indent);
+	}
 
-  // Description:
-  // Specify the first vtkGraph input and the second vtkSelection input.
-  int FillInputPortInformation(int port, vtkInformation* info)
-  {
-    if (port == 0)
-    {
-      info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkImageData");
-      return 1;
-    }
-    return 0;
-  }
+	// Description:
+	// Specify the first vtkGraph input and the second vtkSelection input.
+	int FillInputPortInformation(int port, vtkInformation* info)
+	{
+		if (port == 0)
+		{
+			info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkImageData");
+			return 1;
+		}
+		return 0;
+	}
 
 protected:
-  vtkExtractImageRow()
-  {
-    this->SetNumberOfInputPorts(1);
-  }
-  virtual ~vtkExtractImageRow()
-  {
-  }
+	vtkExtractImageRow()
+	{
+		this->SetNumberOfInputPorts(1);
+	}
+	virtual ~vtkExtractImageRow()
+	{
+	}
 
-  int RequestData(vtkInformation* vtkNotUsed(request), vtkInformationVector** inputVector, vtkInformationVector* outputVector)
-  {
-    vtkImageData* inputImage = vtkImageData::GetData(inputVector[0]);
-    vtkInformation* outInfo = outputVector->GetInformationObject(0);
-    vtkTable* outputTable = vtkTable::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
+	int RequestData(vtkInformation* vtkNotUsed(request), vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+	{
+		vtkImageData* inputImage = vtkImageData::GetData(inputVector[0]);
+		vtkInformation* outInfo = outputVector->GetInformationObject(0);
+		vtkTable* outputTable = vtkTable::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
 
-    if(!inputImage)
-    {
-      LOG_ERROR("No input image is available");
-      return 0;
-    }
+		if (!inputImage)
+		{
+			LOG_ERROR("No input image is available");
+			return 0;
+		}
 
-    // Create the tables if they haven't been created yet
-    if (outputTable->GetColumnByName("time")==NULL)
-    {
-      vtkSmartPointer<vtkFloatArray> arrXnew = vtkSmartPointer<vtkFloatArray>::New();
-      arrXnew->SetName("time");
-      outputTable->AddColumn(arrXnew);
-    }
-    if (outputTable->GetColumnByName("RF value")==NULL)
-    {
-      vtkSmartPointer<vtkFloatArray> arrRfValNew = vtkSmartPointer<vtkFloatArray>::New();
-      arrRfValNew->SetName("RF value");
-      outputTable->AddColumn(arrRfValNew);
-    }
+		// Create the tables if they haven't been created yet
+		if (outputTable->GetColumnByName("time") == NULL)
+		{
+			vtkSmartPointer<vtkFloatArray> arrXnew = vtkSmartPointer<vtkFloatArray>::New();
+			arrXnew->SetName("time");
+			outputTable->AddColumn(arrXnew);
+		}
+		if (outputTable->GetColumnByName("RF value") == NULL)
+		{
+			vtkSmartPointer<vtkFloatArray> arrRfValNew = vtkSmartPointer<vtkFloatArray>::New();
+			arrRfValNew->SetName("RF value");
+			outputTable->AddColumn(arrRfValNew);
+		}
 
-    if (inputImage->GetScalarType()!=VTK_SHORT)
-    {
-      LOG_ERROR("Plotting is only supported for signed short data");
-      return 0;
-    }
-    int rowCount=inputImage->GetDimensions()[1]; // number of transducer crystals
-    int numPoints=inputImage->GetDimensions()[0]; // number of data points (RF data values) recorded for one crystal
-    int selectedRow=rowCount/2; // plot the center column of the image
-    short* pixelBuffer=reinterpret_cast<short*>(inputImage->GetScalarPointer())+selectedRow*numPoints;
+		if (inputImage->GetScalarType() != VTK_SHORT)
+		{
+			LOG_ERROR("Plotting is only supported for signed short data");
+			return 0;
+		}
+		int rowCount = inputImage->GetDimensions()[1]; // number of transducer crystals
+		int numPoints = inputImage->GetDimensions()[0]; // number of data points (RF data values) recorded for one crystal
+		int selectedRow = rowCount / 2; // plot the center column of the image
+		short* pixelBuffer = reinterpret_cast<short*>(inputImage->GetScalarPointer()) + selectedRow * numPoints;
 
-    outputTable->SetNumberOfRows(numPoints);
-    int timeIndex=numPoints-1; // the RF data set starts with the latest time
-    for (int i = 0; i < numPoints; ++i)
-    {
-      outputTable->SetValue(i, 0, timeIndex);
-      short value=*pixelBuffer;
-      outputTable->SetValue(i, 1, value);
-      pixelBuffer++;
-      timeIndex--;
-    }
+		outputTable->SetNumberOfRows(numPoints);
+		int timeIndex = numPoints - 1; // the RF data set starts with the latest time
+		for (int i = 0; i < numPoints; ++i)
+		{
+			outputTable->SetValue(i, 0, timeIndex);
+			short value = *pixelBuffer;
+			outputTable->SetValue(i, 1, value);
+			pixelBuffer++;
+			timeIndex--;
+		}
 
-    return 1;
-  }
+		return 1;
+	}
 
 private:
-  vtkExtractImageRow(const vtkExtractImageRow&); // Not implemented
-  void operator=(const vtkExtractImageRow&);   // Not implemented
+	vtkExtractImageRow(const vtkExtractImageRow&); // Not implemented
+	void operator=(const vtkExtractImageRow&);   // Not implemented
 };
 
 vtkStandardNewMacro(vtkExtractImageRow);
@@ -141,72 +136,72 @@ vtkStandardNewMacro(vtkExtractImageRow);
 class vtkMyPlotCallback : public vtkCommand
 {
 public:
-  static vtkMyPlotCallback *New()	{ return new vtkMyPlotCallback; }
+	static vtkMyPlotCallback *New() { return new vtkMyPlotCallback; }
 
-  virtual void Execute(vtkObject *caller, unsigned long eventId, void* callData)
-  {
-    if (eventId==vtkCommand::KeyPressEvent)
-    {
-      if (m_Interactor->GetKeyCode()=='q')
-      {
-        m_Interactor->ExitCallback();
-      }
-      return;
-    }
+	virtual void Execute(vtkObject *caller, unsigned long eventId, void* callData)
+	{
+		if (eventId == vtkCommand::KeyPressEvent)
+		{
+			if (m_Interactor->GetKeyCode() == 'q')
+			{
+				m_Interactor->ExitCallback();
+			}
+			return;
+		}
 
-    m_ImageToTableAdaptor->Update();
-    m_Viewer->Render();
-    //update the timer so it will trigger again
-    m_Interactor->CreateTimer(VTKI_TIMER_UPDATE);
-  }
+		m_ImageToTableAdaptor->Update();
+		m_Viewer->Render();
+		//update the timer so it will trigger again
+		m_Interactor->CreateTimer(VTKI_TIMER_UPDATE);
+	}
 
-  vtkRenderWindowInteractor *m_Interactor;
-  vtkContextView *m_Viewer;
-  vtkExtractImageRow *m_ImageToTableAdaptor;
+	vtkRenderWindowInteractor *m_Interactor;
+	vtkContextView *m_Viewer;
+	vtkExtractImageRow *m_ImageToTableAdaptor;
 
 private:
 
-  vtkMyPlotCallback()
-  {
-    m_Interactor=NULL;
-    m_Viewer=NULL;
-    m_ImageToTableAdaptor=NULL;
-  }
+	vtkMyPlotCallback()
+	{
+		m_Interactor = NULL;
+		m_Viewer = NULL;
+		m_ImageToTableAdaptor = NULL;
+	}
 };
 
 //----------------------------------------------------------------------------
 void TestLinePlot(vtkPlusClarius *ClariusDevice)
 {
-  // Set up a 2D scene, add an XY chart to it
-  vtkSmartPointer<vtkContextView> view = vtkSmartPointer<vtkContextView>::New();
-  view->GetRenderer()->SetBackground(1.0, 1.0, 1.0);
-  view->GetRenderWindow()->SetSize(640, 480);
-  vtkSmartPointer<vtkChartXY> chart = vtkSmartPointer<vtkChartXY>::New();
-  view->GetScene()->AddItem(chart);
+	// Set up a 2D scene, add an XY chart to it
+	vtkSmartPointer<vtkContextView> view = vtkSmartPointer<vtkContextView>::New();
+	view->GetRenderer()->SetBackground(1.0, 1.0, 1.0);
+	view->GetRenderWindow()->SetSize(640, 480);
+	vtkSmartPointer<vtkChartXY> chart = vtkSmartPointer<vtkChartXY>::New();
+	view->GetScene()->AddItem(chart);
 
-  vtkSmartPointer<vtkExtractImageRow> imageToTableAdaptor=vtkSmartPointer<vtkExtractImageRow>::New();
-  imageToTableAdaptor->SetInputConnection(ClariusDevice->GetOutputPort());
-  imageToTableAdaptor->Update();
+	vtkSmartPointer<vtkExtractImageRow> imageToTableAdaptor = vtkSmartPointer<vtkExtractImageRow>::New();
+	imageToTableAdaptor->SetInputConnection(ClariusDevice->GetOutputPort());
+	imageToTableAdaptor->Update();
 
-  // Add multiple line plots, setting the colors etc
-  vtkPlot *line = chart->AddPlot(vtkChart::LINE);
-  line->SetInputData(imageToTableAdaptor->GetOutput(), 0, 1);
-  line->SetColor(0, 255, 0, 255);
-  line->SetWidth(1.0);
+	// Add multiple line plots, setting the colors etc
+	vtkPlot *line = chart->AddPlot(vtkChart::LINE);
+	line->SetInputData(imageToTableAdaptor->GetOutput(), 0, 1);
+	line->SetColor(0, 255, 0, 255);
+	line->SetWidth(1.0);
 
-  vtkSmartPointer<vtkMyPlotCallback> call = vtkSmartPointer<vtkMyPlotCallback>::New();
-  call->m_Interactor=view->GetInteractor();
-  call->m_Viewer=view;
-  call->m_ImageToTableAdaptor=imageToTableAdaptor;
+	vtkSmartPointer<vtkMyPlotCallback> call = vtkSmartPointer<vtkMyPlotCallback>::New();
+	call->m_Interactor = view->GetInteractor();
+	call->m_Viewer = view;
+	call->m_ImageToTableAdaptor = imageToTableAdaptor;
 
-  view->GetInteractor()->Initialize();
+	view->GetInteractor()->Initialize();
 
-  view->GetInteractor()->AddObserver(vtkCommand::TimerEvent, call);
-  view->GetInteractor()->CreateTimer(VTKI_TIMER_FIRST);
+	view->GetInteractor()->AddObserver(vtkCommand::TimerEvent, call);
+	view->GetInteractor()->CreateTimer(VTKI_TIMER_FIRST);
 
-  view->GetInteractor()->AddObserver(vtkCommand::KeyPressEvent, call);
+	view->GetInteractor()->AddObserver(vtkCommand::KeyPressEvent, call);
 
-  view->GetInteractor()->Start();
+	view->GetInteractor()->Start();
 
 }
 
@@ -215,154 +210,133 @@ void TestLinePlot(vtkPlusClarius *ClariusDevice)
 class vtkMyCallback : public vtkCommand
 {
 public:
-  static vtkMyCallback *New()	{ return new vtkMyCallback; }
+	static vtkMyCallback *New() { return new vtkMyCallback; }
 
-  virtual void Execute(vtkObject *caller, unsigned long, void*)
-  {
-    m_Viewer->Render();
+	virtual void Execute(vtkObject *caller, unsigned long, void*)
+	{
+		m_Viewer->Render();
 
-    //update the timer so it will trigger again
-    m_Interactor->CreateTimer(VTKI_TIMER_UPDATE);
-  }
+		//update the timer so it will trigger again
+		m_Interactor->CreateTimer(VTKI_TIMER_UPDATE);
+	}
 
-  vtkRenderWindowInteractor *m_Interactor;
-  vtkImageViewer *m_Viewer;
+	vtkRenderWindowInteractor *m_Interactor;
+	vtkImageViewer *m_Viewer;
 
 private:
 
-  vtkMyCallback()
-  {
-    m_Interactor=NULL;
-    m_Viewer=NULL;
-  }
+	vtkMyCallback()
+	{
+		m_Interactor = NULL;
+		m_Viewer = NULL;
+	}
 };
 
 //-------------------------------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
-  bool printHelp(false);
-  // bool renderingOff(false);
-  bool printParams(true);
-  std::string inputConfigFile = "Testing/PlusDeviceSet_DataCollectionOnly_Clarius.xml";
+	bool printHelp(false);
+	// bool renderingOff(false);
+	bool printParams(true);
+	std::string inputConfigFile = "Testing/PlusDeviceSet_DataCollectionOnly_Clarius.xml";
 
-  std::string acqMode("B");
+	std::string acqMode("B");
 
-  vtksys::CommandLineArguments args;
-  args.Initialize(argc, argv);
+	vtksys::CommandLineArguments args;
+	args.Initialize(argc, argv);
 
-  int verboseLevel = vtkPlusLogger::LOG_LEVEL_UNDEFINED;
+	int verboseLevel = vtkPlusLogger::LOG_LEVEL_UNDEFINED;
 
-  args.AddArgument("--help", vtksys::CommandLineArguments::NO_ARGUMENT, &printHelp, "Print this help.");
-  args.AddArgument("--config-file", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputConfigFile, "Config file containing the device configuration.");
-  args.AddArgument("--verbose", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &verboseLevel, "Verbose level 1=error only, 2=warning, 3=info, 4=debug, 5=trace)");
+	args.AddArgument("--help", vtksys::CommandLineArguments::NO_ARGUMENT, &printHelp, "Print this help.");
+	args.AddArgument("--config-file", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &inputConfigFile, "Config file containing the device configuration.");
+	args.AddArgument("--verbose", vtksys::CommandLineArguments::EQUAL_ARGUMENT, &verboseLevel, "Verbose level 1=error only, 2=warning, 3=info, 4=debug, 5=trace)");
 
-  if ( !args.Parse() )
-  {
-    std::cerr << "Problem parsing arguments" << std::endl;
-    std::cout << "\n\nvtkPlusSonixVideoSourceTest1 help:" << args.GetHelp() << std::endl;
-    exit(EXIT_FAILURE);
-  }
+	if (!args.Parse())
+	{
+		std::cerr << "Problem parsing arguments" << std::endl;
+		std::cout << "\n\nvtkPlusSonixVideoSourceTest1 help:" << args.GetHelp() << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
-  vtkPlusLogger::Instance()->SetLogLevel(5);
+	vtkPlusLogger::Instance()->SetLogLevel(5);
 
-  
-  vtkSmartPointer<vtkPlusClarius> ClariusDevice = vtkSmartPointer<vtkPlusClarius>::New();
-  ClariusDevice->SetDeviceId("VideoDevice");
 
-  // Read config file
-  if (STRCASECMP(inputConfigFile.c_str(), "")!=0)
-  {
-    LOG_DEBUG("Reading config file...");
-    vtkSmartPointer<vtkXMLDataElement> configRead = vtkSmartPointer<vtkXMLDataElement>::Take(::vtkXMLUtilities::ReadElementFromFile(inputConfigFile.c_str()));
-    LOG_DEBUG("Reading config file finished.");
-    if ( configRead != NULL )
-    {
-      ClariusDevice->ReadConfiguration(configRead);
-    }
-  }
+	vtkSmartPointer<vtkPlusClarius> ClariusDevice = vtkSmartPointer<vtkPlusClarius>::New();
+	ClariusDevice->SetDeviceId("VideoDevice");
 
-  ClariusDevice->CreateDefaultOutputChannel(NULL, true);
-  vtkPlusDataSource* videoSource=NULL;
-  if (ClariusDevice->GetFirstActiveOutputVideoSource(videoSource) != PLUS_SUCCESS )
-  {
-    LOG_ERROR("Unable to retrieve the video source.");
-    exit(EXIT_FAILURE);
-  }
-  videoSource->SetInputImageOrientation(US_IMG_ORIENT_UN);
+	// Read config file
+	if (STRCASECMP(inputConfigFile.c_str(), "") != 0)
+	{
+		LOG_DEBUG("Reading config file...");
+		vtkSmartPointer<vtkXMLDataElement> configRead = vtkSmartPointer<vtkXMLDataElement>::Take(::vtkXMLUtilities::ReadElementFromFile(inputConfigFile.c_str()));
+		LOG_DEBUG("Reading config file finished.");
+		if (configRead != NULL)
+		{
+			ClariusDevice->ReadConfiguration(configRead);
+		}
+	}
 
-  DisplayMode displayMode=SHOW_IMAGE;
-  //DisplayMode displayMode=SHOW_PLOT;
+	ClariusDevice->CreateDefaultOutputChannel(NULL, true);
+	vtkPlusDataSource* videoSource = NULL;
+	if (ClariusDevice->GetFirstActiveOutputVideoSource(videoSource) != PLUS_SUCCESS)
+	{
+		LOG_ERROR("Unable to retrieve the video source.");
+		exit(EXIT_FAILURE);
+	}
+	videoSource->SetInputImageOrientation(US_IMG_ORIENT_UN);
 
-  if (STRCASECMP(acqMode.c_str(), "B")==0)
-  {
-    LOG_DEBUG("Acquisition mode: B");
-    //ClariusDevice->SetImagingMode(BMode);
-    displayMode=SHOW_IMAGE;
-  }
-  
-  else
-  {
-    LOG_ERROR("Unsupported AcquisitionDataType requested: "<<acqMode);
-    exit(EXIT_FAILURE);
-  }
+	DisplayMode displayMode = SHOW_IMAGE;
+	//DisplayMode displayMode=SHOW_PLOT;
 
-  if ( ClariusDevice->Connect()!=PLUS_SUCCESS )
-  {
-    LOG_ERROR("Unable to connect to Telemed device");
-    exit(EXIT_FAILURE);
-  }
+	if (STRCASECMP(acqMode.c_str(), "B") == 0)
+	{
+		LOG_DEBUG("Acquisition mode: B");
+		//ClariusDevice->SetImagingMode(BMode);
+		displayMode = SHOW_IMAGE;
+	}
 
-  // ClariusDevice->StartRecording();				
-  //start recording frame from the video
+	else
+	{
+		LOG_ERROR("Unsupported AcquisitionDataType requested: " << acqMode);
+		exit(EXIT_FAILURE);
+	}
 
-  /*
-  if (renderingOff)
-  {
-    // just run the recording for  a few seconds then exit
-    LOG_DEBUG("Rendering disabled. Wait for just a few seconds to acquire data before exiting");
-    Sleep(5000); // no need to use accurate timer, it's just an approximate delay
-    ClariusDevice->StopRecording();
-    ClariusDevice->Disconnect();
-  }
-  */
-	/*
-	  if (displayMode==SHOW_PLOT)
-    {
-      TestLinePlot(ClariusDevice);
-    }
-	*/
-    
-    
-      // Show the live ultrasound image in a VTK renderer window
+	if (ClariusDevice->Connect() != PLUS_SUCCESS)
+	{
+		LOG_ERROR("Unable to connect to Telemed device");
+		exit(EXIT_FAILURE);
+	}
 
-      vtkSmartPointer<vtkImageViewer> viewer = vtkSmartPointer<vtkImageViewer>::New();
-      viewer->SetInputConnection(ClariusDevice->GetOutputPort());   //set image to the render and window
-      viewer->SetColorWindow(255);
-      viewer->SetColorLevel(127.5);
-      viewer->SetZSlice(0);
+	// Show the live ultrasound image in a VTK renderer window
 
-      //Create the interactor that handles the event loop
-      vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-      iren->SetRenderWindow(viewer->GetRenderWindow());
-      viewer->SetupInteractor(iren);
+	vtkSmartPointer<vtkImageViewer> viewer = vtkSmartPointer<vtkImageViewer>::New();
+	viewer->SetInputConnection(ClariusDevice->GetOutputPort());   //set image to the render and window
+	viewer->SetColorWindow(255);
+	viewer->SetColorLevel(127.5);
+	viewer->SetZSlice(0);
 
-      viewer->Render();	//must be called after iren and viewer are linked or there will be problems
+	//Create the interactor that handles the event loop
+	vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	iren->SetRenderWindow(viewer->GetRenderWindow());
+	viewer->SetupInteractor(iren);
 
-      // Establish timer event and create timer to update the live image
-      vtkSmartPointer<vtkMyCallback> call = vtkSmartPointer<vtkMyCallback>::New();
-      call->m_Interactor=iren;
-      call->m_Viewer=viewer;
-      iren->AddObserver(vtkCommand::TimerEvent, call);
-      iren->CreateTimer(VTKI_TIMER_FIRST);
+	viewer->Render();	//must be called after iren and viewer are linked or there will be problems
 
-      //iren must be initialized so that it can handle events
-      iren->Initialize();
-      iren->Start();
+	// Establish timer event and create timer to update the live image
+	vtkSmartPointer<vtkMyCallback> call = vtkSmartPointer<vtkMyCallback>::New();
+	call->m_Interactor = iren;
+	call->m_Viewer = viewer;
+	iren->AddObserver(vtkCommand::TimerEvent, call);
+	iren->CreateTimer(VTKI_TIMER_FIRST);
+
+	//iren must be initialized so that it can handle events
+	iren->Initialize();
+	iren->Start();
 
 
 
-  ClariusDevice->Disconnect();
-  return EXIT_SUCCESS;
+	ClariusDevice->Disconnect();
+	return EXIT_SUCCESS;
 }
 
