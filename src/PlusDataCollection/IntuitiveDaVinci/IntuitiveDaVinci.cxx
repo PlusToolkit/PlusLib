f710694aa5cfbfcfc6c38763cced74cb00b920dd
James Ferguson <james.m.ferguson@vanderbilt.edu>
2019-05-13 12:57:42 -0500

    Updated intuitivedavinci to do what the fake one does

--- src/PlusDataCollection/IntuitiveDaVinci/IntuitiveDaVinci.cxx
+++ src/PlusDataCollection/IntuitiveDaVinci/IntuitiveDaVinci.cxx
@@ -11,11 +11,7 @@ See License.txt for details.
 IntuitiveDaVinci::IntuitiveDaVinci()
   : mPrintStream(ISI_FALSE)
   , mQuit(ISI_FALSE)
-  , mManipIndex(ISI_PSM2)
   , mStatus(ISI_SUCCESS)
-  , mIpAddr("10.0.0.5")
-  , mPort(5002)
-  , mPassword("")
   , mRateHz(60)
 {
 
@@ -31,18 +27,8 @@ IntuitiveDaVinci::~IntuitiveDaVinci()
 //----------------------------------------------------------------------------
 bool IntuitiveDaVinci::start()
 {
-  if (isConnected())
-  {
-    ISI_STATUS status;
-    status = isi_start_stream(mRateHz);
-    if (status != ISI_SUCCESS)
-    {
-      LOG_ERROR("Unable to start streaming.");
-      return false;
-    }
-
+  if (isConnected()) 
     return true;
-  }
 
   LOG_ERROR("Not connected. Cannot start streaming.");
   return false;
@@ -52,25 +38,14 @@ bool IntuitiveDaVinci::start()
 void IntuitiveDaVinci::stop()
 {
   if (isConnected())
-  {
-    isi_stop_stream();
-    isi_disconnect();
     mConnected = false;
-  }
 }
 
 //----------------------------------------------------------------------------
 ISI_STATUS IntuitiveDaVinci::connect()
 {
-  ISI_UINT password = strtol(mPassword.c_str(), 0, 16);
-  mStatus = isi_connect_ex(mIpAddr.c_str(), mPort, password);
+  mStatus = ISI_SUCCESS;
 
-  if (mStatus != ISI_SUCCESS)
-  {
-    LOG_WARNING("IntuitiveDaVinci::connect failed to connect with arguments");
-    isi_stop_stream();
-    isi_disconnect();
-  }
   mConnected = true;
   return mStatus;
 }
@@ -86,95 +61,8 @@ ISI_STATUS IntuitiveDaVinci::disconnect()
   }
 
   stop();
-  isi_disconnect();
-  this->mConnected = false;
+  mConnected = false;
   return ISI_SUCCESS;
-}
-
-//----------------------------------------------------------------------------
-ISI_STATUS IntuitiveDaVinci::subscribe(ISI_EVENT_CALLBACK eCB, ISI_STREAM_CALLBACK sCB, void* eventUserData, void* streamUserData)
-{
-  setStreamCallback(sCB, streamUserData);
-  setEventCallback(eCB, eventUserData);
-
-  // Start streaming information at 60 Hz
-  mStatus = isi_start_stream(mRateHz);
-
-  if (mStatus != ISI_SUCCESS)
-  {
-    LOG_WARNING("IntuitiveDaVinci::subscribe failed to start stream. Disconnecting from da Vinci");
-    stop();
-  }
-  return mStatus;
-}
-
-//----------------------------------------------------------------------------
-ISI_STATUS IntuitiveDaVinci::setStreamCallback(ISI_STREAM_CALLBACK sCB, void* userData)
-{
-  // Subscribe to all fields
-  mStatus = isi_subscribe_all_stream_fields();
-  if (mStatus != ISI_SUCCESS)
-  {
-    // Propagate error message to caller
-    return mStatus;
-  }
-
-  if (sCB != NULL)
-  {
-    mStatus = isi_set_stream_callback(sCB, userData);
-  }
-  Sleep(500);
-
-  return mStatus;
-}
-
-//----------------------------------------------------------------------------
-ISI_STATUS IntuitiveDaVinci::setEventCallback(ISI_EVENT_CALLBACK eCB, void* userData)
-{
-  // Subscribe to all events
-  mStatus = isi_subscribe_all_events();
-
-  if (mStatus != ISI_SUCCESS)
-  {
-    // Propagate error message to caller
-    return mStatus;
-  }
-
-  if (eCB != NULL)
-  {
-    mStatus = isi_set_event_callback(eCB, userData);
-  }
-  Sleep(500);
-
-  return mStatus;
-}
-
-//----------------------------------------------------------------------------
-std::vector<std::string> IntuitiveDaVinci::getManipulatorNames()
-{
-  std::vector<std::string> names;
-
-  for (int i = 0; i != ISI_NUM_MANIPS; i++)
-  {
-    ISI_MANIP_INDEX index = (ISI_MANIP_INDEX) i;
-    std::string name = isi_get_manip_name(index);
-
-    if (name.size() == 0) { continue; }
-
-    names.push_back(name);
-  }
-
-  return names;
-}
-
-//----------------------------------------------------------------------------
-void IntuitiveDaVinci::getPosition(ISI_TRANSFORM* T)
-{
-  ISI_STREAM_FIELD stream_data; // Datastreams
-
-  isi_get_stream_field(mManipIndex, ISI_TIP_TRANSFORM, &stream_data);
-
-  copyTransform((ISI_TRANSFORM*) stream_data.data, T);
 }
 
 //----------------------------------------------------------------------------
@@ -201,110 +89,7 @@ void IntuitiveDaVinci::printTransform(const ISI_TRANSFORM* T)
 }
 
 //----------------------------------------------------------------------------
-void IntuitiveDaVinci::printHelp()
-{
-  static const char help[] = {"\
-								API Stream Menu \n\
-								---------------- \n\
-								Q: Quit          \n\
-								M: Set Manip ID  \n\
-								V: Print Version \n\
-								P: Print Sample  \n\
-								S: Print Stream  \n\
-								R: Record file   \n\
-								"
-                             };
-
-  LOG_INFO(help);
-}
-
-//----------------------------------------------------------------------------
-std::string IntuitiveDaVinci::getLibraryVersion()
-{
-  ISI_STATUS status;
-  ISI_SYSTEM_CONFIG config;
-
-  status = isi_get_system_config(&config);
-
-  if (status != ISI_SUCCESS)
-  {
-    return "";
-  }
-
-  return config.library_version;
-}
-
-//----------------------------------------------------------------------------
 bool IntuitiveDaVinci::isConnected()
 {
   return mConnected;
-}
-
-//----------------------------------------------------------------------------
-void IntuitiveDaVinci::printStreamState(ISI_MANIP_INDEX manipIndex)
-{
-  ISI_STREAM_FIELD stream_data;
-  int i;
-
-  LOG_INFO("Retrieving stream state for manipulator: " << isi_get_manip_name(manipIndex));
-
-  LOG_INFO("\nISI_TIP_TRANSFORM: \n");
-  isi_get_stream_field(manipIndex, ISI_TIP_TRANSFORM, &stream_data);
-  printTransform((ISI_TRANSFORM*) stream_data.data);
-
-  LOG_INFO("\nISI_JOINT_VALUES: \n");
-
-  isi_get_stream_field(manipIndex, ISI_JOINT_VALUES, &stream_data);
-  for (i = 0; i != stream_data.count; i++)
-  {
-    LOG_INFO(" " << stream_data.data[i]);
-  }
-
-  LOG_INFO("\n ISI Timestamp (seconds): " <<  stream_data.timestamp);
-}
-
-//----------------------------------------------------------------------------
-void IntuitiveDaVinci::printVersion()
-{
-  ISI_STATUS status;
-  ISI_SYSTEM_CONFIG config;
-
-  status = isi_get_system_config(&config);
-
-  if (status == ISI_SUCCESS)
-  {
-    LOG_INFO("System Name: " << config.system_name);
-    LOG_INFO("System Version: " << config.system_version);
-    LOG_INFO("Library Version: " << config.library_version);
-  }
-  else
-  {
-    LOG_WARNING("Failed to get IntuitiveDaVinci system configuration");
-  }
-}
-
-//----------------------------------------------------------------------------
-void IntuitiveDaVinci::setHostInfo(const std::string ip, const unsigned int port, const std::string pass)
-{
-  mIpAddr = ip;
-  mPort = port;
-  mPassword = pass;
-}
-
-//----------------------------------------------------------------------------
-void IntuitiveDaVinci::setIpAddr(const std::string ip)
-{
-  mIpAddr = ip;
-}
-
-//----------------------------------------------------------------------------
-void IntuitiveDaVinci::setPort(const unsigned int port)
-{
-  mPort = port;
-}
-
-//----------------------------------------------------------------------------
-void IntuitiveDaVinci::setPassword(const std::string password)
-{
-  mPassword = password;
 }
--- src/PlusDataCollection/IntuitiveDaVinci/vtkPlusIntuitiveDaVinciTracker.h
+++ src/PlusDataCollection/IntuitiveDaVinci/vtkPlusIntuitiveDaVinciTracker.h
@@ -16,17 +16,10 @@ See License.txt for details.
 
 // Intuitive includes
 #include "IntuitiveDaVinci.h"
-#include <isi_api.h>
 
 class vtkMatrix4x4;
 class IntuitiveDaVinci;
 class vtkPlusIntuitiveDaVinciTracker;
-
-namespace vtkPlusIntuitiveDaVinciTrackerUtilities
-{
-  void ISICALLBACK streamCB(void* userdata);
-  void ISICALLBACK eventCB(ISI_MANIP_INDEX mid, ISI_EVENT_ID event_id, ISI_INT args[ISI_NUM_EVENT_ARGS],  void* userdata);
-};
 
 /* This class talks with the da Vinci Surgical System. */
 class vtkPlusDataCollectionExport vtkPlusIntuitiveDaVinciTracker : public vtkPlusDevice
@@ -35,9 +28,6 @@ public:
   static vtkPlusIntuitiveDaVinciTracker* New();
   vtkTypeMacro(vtkPlusIntuitiveDaVinciTracker, vtkPlusDevice);
   virtual void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
-
-  /*! Hardware device SDK version. */
-  virtual std::string GetSdkVersion();
 
   virtual bool IsTracker() const { return true; }
 
@@ -51,15 +41,6 @@ public:
   virtual PlusStatus WriteConfiguration(vtkXMLDataElement* rootConfigElement);
 
   IntuitiveDaVinci* GetDaVinci() const;
-
-  vtkSetStdStringMacro(IpAddr);
-  vtkGetStdStringMacro(IpAddr);
-
-  vtkSetMacro(Port, unsigned int);
-  vtkGetMacro(Port, unsigned int);
-
-  vtkSetStdStringMacro(Password);
-  vtkGetStdStringMacro(Password);
 
 protected:
   vtkPlusIntuitiveDaVinciTracker();
@@ -77,38 +58,25 @@ protected:
   /*! Stop the tracking system and bring it back to its initial state. */
   virtual PlusStatus InternalStopRecording();
 
+  /*! Update method */
+  PlusStatus InternalUpdate();
+
 protected:
   /*! Pointer to the MicronTrackerInterface class instance */
   IntuitiveDaVinci*   DaVinci;
 
   /*! Index of the last frame number. This is used for providing a frame number when the tracker doesn't return any transform */
-  double              LastFrameNumber;
-
-  unsigned int        FrameNumber;
-
-#ifdef USE_DAVINCI_TIMESTAMPS
-  double              TrackerTimeToSystemTimeSec; // time_System = time_Tracker + TrackerTimeToSystemTimeSec
-  bool                TrackerTimeToSystemTimeComputed; // the time offset is always computed when the first frame is received after start tracking
-#endif
-
-  std::string         IpAddr;
-  unsigned int        Port;
-  std::string         Password;
+  unsigned long       LastFrameNumber;
+  unsigned long       FrameNumber;
 
 private:
   vtkPlusIntuitiveDaVinciTracker(const vtkPlusIntuitiveDaVinciTracker&);
   void operator=(const vtkPlusIntuitiveDaVinciTracker&);
 
-  void StreamCallback(void);
-
-  friend void vtkPlusIntuitiveDaVinciTrackerUtilities::streamCB(void* userData);
-  friend void vtkPlusIntuitiveDaVinciTrackerUtilities::eventCB(ISI_MANIP_INDEX mid, ISI_EVENT_ID event_id, ISI_INT args[ISI_NUM_EVENT_ARGS],  void* userdata);
-
-  ISI_MANIP_INDEX getManipIndexFromName(const std::string& toolName);
   void setVtkMatrixFromISITransform(vtkMatrix4x4& vtkMatrix, ISI_TRANSFORM* isiMatrix);
 
-  static const int CONNECT_RETRY_DELAY_SEC = 1.0;
-  static const int MAX_ATTEMPTS = 5;
+  vtkPlusDataSource* PSM1Tip;
+  vtkPlusDataSource* PSM2Tip;
 };
 
 #endif
