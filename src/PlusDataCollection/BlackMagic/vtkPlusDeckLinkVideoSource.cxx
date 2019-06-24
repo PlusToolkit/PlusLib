fd7d48a796d4869da3f2d2b829bf7768c37b05c7
Adam Rankin <adam.rankin@gmail.com>
2019-06-24 10:05:02 -0400

    re PlusToolkit/PlusLib#555: Added skeleton of support for BlackMagic DeckLink capture cards (#569)

---
+++ src/PlusDataCollection/BlackMagic/vtkPlusDeckLinkVideoSource.cxx
@@ -0,0 +1,151 @@
+/*=Plus=header=begin======================================================
+Program: Plus
+Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+See License.txt for details.
+=========================================================Plus=header=end*/
+
+#include "PlusConfigure.h"
+
+// Local includes
+#include "vtkPlusDeckLinkVideoSource.h"
+#include "vtkIGSIOAccurateTimer.h"
+
+// VTK includes
+
+// System includes
+#include <string>
+
+//----------------------------------------------------------------------------
+// vtkPlusDeckLinkVideoSource::vtkInternal
+//----------------------------------------------------------------------------
+
+class vtkPlusDeckLinkVideoSource::vtkInternal
+{
+public:
+  vtkPlusDeckLinkVideoSource* External;
+
+  vtkInternal(vtkPlusDeckLinkVideoSource* external)
+    : External(external)
+  {
+  }
+
+  virtual ~vtkInternal()
+  {
+  }
+
+};
+
+//----------------------------------------------------------------------------
+// vtkPlusDeckLinkVideoSource
+//----------------------------------------------------------------------------
+
+vtkStandardNewMacro(vtkPlusDeckLinkVideoSource);
+
+//----------------------------------------------------------------------------
+vtkPlusDeckLinkVideoSource::vtkPlusDeckLinkVideoSource()
+  : vtkPlusDevice()
+  , Internal(new vtkInternal(this))
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::vtkPlusDeckLinkVideoSource()");
+
+  this->FrameNumber = 0;
+  this->StartThreadForInternalUpdates = true;
+  this->InternalUpdateRate = 30;
+}
+
+//----------------------------------------------------------------------------
+vtkPlusDeckLinkVideoSource::~vtkPlusDeckLinkVideoSource()
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::~vtkPlusDeckLinkVideoSource()");
+
+  delete Internal;
+  Internal = nullptr;
+}
+
+//----------------------------------------------------------------------------
+void vtkPlusDeckLinkVideoSource::PrintSelf(ostream& os, vtkIndent indent)
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::PrintSelf(ostream& os, vtkIndent indent)");
+  Superclass::PrintSelf(os, indent);
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::ReadConfiguration(vtkXMLDataElement* rootConfigElement)
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::ReadConfiguration");
+
+  XML_FIND_DEVICE_ELEMENT_REQUIRED_FOR_READING(deviceConfig, rootConfigElement);
+
+  XML_FIND_NESTED_ELEMENT_REQUIRED(dataSourcesElement, deviceConfig, "DataSources");
+  
+  for (int nestedElementIndex = 0; nestedElementIndex < dataSourcesElement->GetNumberOfNestedElements(); nestedElementIndex++)
+  {
+    vtkXMLDataElement* toolDataElement = dataSourcesElement->GetNestedElement(nestedElementIndex);
+    if (STRCASECMP(toolDataElement->GetName(), "DataSource") != 0)
+    {
+      // if this is not a data source element, skip it
+      continue;
+    }
+    if (toolDataElement->GetAttribute("Type") != NULL && STRCASECMP(toolDataElement->GetAttribute("Type"), "Video") != 0)
+    {
+      // if this is not a Tool element, skip it
+      continue;
+    }
+    
+  }
+
+  return PLUS_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::WriteConfiguration(vtkXMLDataElement* rootConfigElement)
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::WriteConfiguration");
+  XML_FIND_DEVICE_ELEMENT_REQUIRED_FOR_WRITING(deviceConfig, rootConfigElement);
+  return PLUS_FAIL;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::InternalConnect()
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::InternalConnect");
+
+  return PLUS_FAIL;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::InternalDisconnect()
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::InternalDisconnect");
+
+  return PLUS_FAIL;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::InternalStartRecording()
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::InternalStartRecording");
+  return PLUS_FAIL;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::InternalStopRecording()
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::InternalStopRecording");
+  return PLUS_FAIL;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::Probe()
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::Probe");
+  return PLUS_FAIL;
+}
+
+//----------------------------------------------------------------------------
+PlusStatus vtkPlusDeckLinkVideoSource::InternalUpdate()
+{
+  LOG_TRACE("vtkPlusDeckLinkVideoSource::InternalUpdate");
+
+  return PLUS_FAIL;
+}
---
+++ src/PlusDataCollection/BlackMagic/vtkPlusDeckLinkVideoSource.h
@@ -0,0 +1,75 @@
+/*=Plus=header=begin======================================================
+Program: Plus
+Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+See License.txt for details.
+=========================================================Plus=header=end*/
+
+#ifndef __vtkPlusDeckLinkVideoSource_h
+#define __vtkPlusDeckLinkVideoSource_h
+
+#include "vtkPlusDataCollectionExport.h"
+#include "vtkPlusDevice.h"
+
+#include <combaseapi.h>
+// BlackMagic SDK includes
+#ifdef _WIN32
+#include "DeckLinkAPI_h.h"
+#else
+#include "DeckLinkAPI.h"
+#endif
+
+/*!
+\class vtkPlusDeckLinkVideoSource
+\brief Interface to a BlackMagic DeckLink
+capture card
+\ingroup PlusLibDataCollection
+*/
+class vtkPlusDataCollectionExport vtkPlusDeckLinkVideoSource : public vtkPlusDevice
+{
+public:
+  static vtkPlusDeckLinkVideoSource* New();
+  vtkTypeMacro(vtkPlusDeckLinkVideoSource, vtkPlusDevice);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  /* Device is a hardware tracker. */
+  virtual bool IsTracker() const { return false; }
+  virtual bool IsVirtual() const { return false; }
+
+  /*! Read configuration from xml data */
+  virtual PlusStatus ReadConfiguration(vtkXMLDataElement* config);
+
+  /*! Write configuration to xml data */
+  virtual PlusStatus WriteConfiguration(vtkXMLDataElement* config);
+
+  /*! Connect to device */
+  PlusStatus InternalConnect();
+
+  /*! Disconnect from device */
+  virtual PlusStatus InternalDisconnect();
+
+  /*! Start the tracking system. */
+  PlusStatus InternalStartRecording();
+
+  /*! Stop the tracking system and bring it back to its initial state. */
+  PlusStatus InternalStopRecording();
+
+  /*! Probe to see if the tracking system is present. */
+  PlusStatus Probe();
+
+  /*! Perform update */
+  PlusStatus InternalUpdate();
+
+protected:
+  vtkPlusDeckLinkVideoSource();
+  ~vtkPlusDeckLinkVideoSource();
+
+private:
+  vtkPlusDeckLinkVideoSource(const vtkPlusDeckLinkVideoSource&); // Not implemented
+  void operator=(const vtkPlusDeckLinkVideoSource&); // Not implemented
+
+  class vtkInternal;
+  vtkInternal* Internal;
+
+};
+
+#endif
