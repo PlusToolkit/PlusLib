a2cf9e20b6d8336a67bb62dafded76dfb3c29f08
Sylvain Bernhardt <sylvain.bernhardt@smith-nephew.com>
2021-09-07 17:57:00 +0200

    ENH: Atracsys option handling
    Removed hardcoded option ids, those are now automatically fetched from sdk
    Improved a bit the robustness of setting options

--- src/PlusDataCollection/Atracsys/AtracsysTracker.h
+++ src/PlusDataCollection/Atracsys/AtracsysTracker.h
@@ -11,8 +11,12 @@ See License.txt for details.
 #include <vector>
 #include <vtkNew.h>
 
-
+struct ftkOptionsInfo;
 class vtkMatrix4x4;
+
+// Functions to safely convert string to int32 or float
+bool strToInt32(const std::string& str, int& var);
+bool strToFloat32(const std::string& str, float& var);
 
 class AtracsysTracker
 {
@@ -37,11 +41,9 @@ public:
     ERROR_NO_FRAME_AVAILABLE,
     ERROR_INVALID_FRAME,
     ERROR_TOO_MANY_MARKERS,
-    ERROR_ENABLE_IR_STROBE,
     ERROR_ENABLE_LASER,
     ERROR_SET_USER_LED,
     ERROR_ENABLE_USER_LED,
-    ERROR_SET_MAX_MISSING_FIDUCIALS,
     ERROR_ENABLE_ONBOARD_PROCESSING,
     ERROR_ENABLE_IMAGE_STREAMING,
     ERROR_ENABLE_WIRELESS_MARKER_PAIRING,
@@ -50,16 +52,16 @@ public:
     ERROR_DISCONNECT_ATTEMPT_WHEN_NOT_CONNECTED,
     ERROR_CANNOT_GET_MARKER_INFO,
     ERROR_FAILED_TO_SET_STK_PROCESSING_TYPE,
-    ERROR_FAILED_TO_SET_MAX_MISSING_FIDS,
-    ERROR_OPTION_NOT_FOUND
+    ERROR_OPTION_NOT_FOUND,
+    ERROR_SET_OPTION
   };
 
   enum DEVICE_TYPE
   {
     UNKNOWN_DEVICE = 0,
-    SPRYTRACK_180 = 1,
-    FUSIONTRACK_500 = 2,
-    FUSIONTRACK_250 = 3
+    SPRYTRACK_180,
+    FUSIONTRACK_500,
+    FUSIONTRACK_250
   };
 
   enum SPRYTRACK_IMAGE_PROCESSING_TYPE
@@ -129,17 +131,13 @@ public:
   ATRACSYS_RESULT GetMarkersInFrame(std::vector<Marker>& markers);
 
   /*! */
-  ATRACSYS_RESULT EnableIRStrobe(bool enabled);
-
-  /*! */
   ATRACSYS_RESULT SetUserLEDState(int red, int green, int blue, int frequency, bool enabled = true);
-
 
   /*! */
   ATRACSYS_RESULT EnableUserLED(bool enabled);
 
   /*! */
-  ATRACSYS_RESULT SetMaxMissingFiducials(int maxMissingFids);
+  ATRACSYS_RESULT SetLaserEnabled(bool enabled);
 
   /*! */
   ATRACSYS_RESULT EnableWirelessMarkerPairing(bool enabled);
@@ -161,9 +159,6 @@ public:
   ATRACSYS_RESULT EnableImageStreaming(bool enabled);
 
   /*! */
-  ATRACSYS_RESULT SetLaserEnabled(bool enabled);
-
-  /*! */
   ATRACSYS_RESULT SetSpryTrackProcessingType(SPRYTRACK_IMAGE_PROCESSING_TYPE processingType);
 
   // ------------------------------------------
@@ -176,51 +171,15 @@ public:
   /*! */
   ATRACSYS_RESULT ResetLostFrameCount();
 
-  enum OPTIONS
-  {
-    OPTION_DATA_SENDING = 20,
-    OPTION_FTK_WIRELESS_MARKER_PAIRING_ENABLE = 40,
-    OPTION_FTK_WIRELESS_MARKER_STATUS_STREAMING = 45,
-    OPTION_FTK_WIRELESS_MARKER_BATTERY_STREAMING = 46,
-    OPTION_FTK_DEV_MARKERS_INFO = 47,
-    OPTION_IR_STROBE = 50,
-    OPTION_LOST_FRAME_COUNT = 60,
-    OPTION_CORRUPTED_FRAME_COUNT = 61,
-    OPTION_RESET_LOST_FRAME_COUNT = 69,
-    OPTION_ENABLE_LASER = 80,
-    OPTION_LED_RED_COMPONENT = 90,
-    OPTION_LED_GREEN_COMPONENT = 91,
-    OPTION_LED_BLUE_COMPONENT = 92,
-    OPTION_LED_FREQUENCY = 93,
-    OPTION_LED_ENABLE = 94,
-    OPTION_EPIPOLAR_MIN_DISTANCE = 2001,
-    OPTION_MATCHING_TOLERANCE = 3002,
-    OPTION_MAX_MEAN_REGISTRATION_ERROR = 3003,
-    OPTION_MAX_MISSING_POINTS = 3004,
-    OPTION_MAX_TRACKING_RANGE = 3005,
-    OPTION_ONBOARD_PROCESSING = 6000,
-    OPTION_IMAGE_STREAMING = 6003,
-    OPTION_STK_WIRELESS_MARKER_PAIRING_ENABLE = 7000,
-    OPTION_STK_WIRELESS_MARKER_STATUS_STREAMING = 7001,
-    OPTION_STK_WIRELESS_MARKER_BATTERY_STREAMING = 7002,
-    OPTION_STK_DEV_MARKERS_INFO = 7005,
-  };
+  ATRACSYS_RESULT SetOption(const std::string&, const std::string&);
 
-
+protected:
+  bool GetOptionInfo(const std::string&, ftkOptionsInfo*&);
 
 private:
   DEVICE_TYPE DeviceType = UNKNOWN_DEVICE;
-  
+
   class AtracsysInternal;
   AtracsysInternal* Internal;
-
-  // load Atracsys marker geometry ini file
-  //bool LoadIniFile(std::ifstream& is, ftkGeometry& geometry);
-
-  // helper function to set spryTrack only options
-  ATRACSYS_RESULT SetSpryTrackOnlyOption(int option, int value, ATRACSYS_RESULT errorResult);
-
-  // helper function to set fusionTrack only options
-  ATRACSYS_RESULT SetFusionTrackOnlyOption(int option, int value, ATRACSYS_RESULT errorResult);
 };
 #endif
