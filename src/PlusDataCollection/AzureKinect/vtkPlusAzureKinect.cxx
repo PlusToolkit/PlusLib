ccdf6afbce0d5d36089ab0f6fbd691e4a9289b78
C. G <kenavolic@protonmail.com>
2022-01-28 14:00:36 +0100

    re #894: Fix bad frame rate handling in Azure Kinect
    * Fix issue
    * Add warning for frame rate ambiguity
    * Better compliance with code style
    
    Fixes #894

--- src/PlusDataCollection/AzureKinect/vtkPlusAzureKinect.cxx
+++ src/PlusDataCollection/AzureKinect/vtkPlusAzureKinect.cxx
@@ -134,34 +134,37 @@ public:
   vtkInternal(vtkPlusAzureKinect* external)
     : External(external)
   {
-    DeviceConfig.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
-    DeviceConfig.synchronized_images_only = true;
-    DeviceConfig.depth_mode = K4A_DEPTH_MODE_OFF;
-    DeviceConfig.color_resolution = K4A_COLOR_RESOLUTION_OFF;
-    DeviceConfig.wired_sync_mode = K4A_WIRED_SYNC_MODE_STANDALONE;
-    DeviceConfig.depth_delay_off_color_usec = 0;
-    DeviceConfig.subordinate_delay_off_master_usec = 0;
+    this->DeviceConfig.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
+    this->DeviceConfig.synchronized_images_only = true;
+    this->DeviceConfig.depth_mode = K4A_DEPTH_MODE_OFF;
+    this->DeviceConfig.color_resolution = K4A_COLOR_RESOLUTION_OFF;
+    this->DeviceConfig.wired_sync_mode = K4A_WIRED_SYNC_MODE_STANDALONE;
+    this->DeviceConfig.depth_delay_off_color_usec = 0;
+    this->DeviceConfig.subordinate_delay_off_master_usec = 0;
   }
 
   PlusStatus UpdateConfig()
   {
     bool status{false};
-    for (auto& Stream : StreamList)
+    std::vector<uint32_t> frameRates;
+    for (auto& stream : this->StreamList)
     {
-      std::tie(status, DeviceConfig.camera_fps) = ToKinectFrameRate(Stream.FrameRate);
+      std::tie(status, DeviceConfig.camera_fps) = ToKinectFrameRate(stream.FrameRate);
       if (!status)
       {
         LOG_ERROR("Invalid frame rate detected: 5, 15, 30 expected");
         return PLUS_FAIL;
       }
 
-      if (Stream.Type == KinectSourceType::RGB)
+      frameRates.emplace_back(stream.FrameRate);
+
+      if (stream.Type == KinectSourceType::RGB)
       {
-        std::tie(status, DeviceConfig.color_resolution, Stream.Height) = ToKinectColorMode(Stream.Width);
+        std::tie(status, DeviceConfig.color_resolution, stream.Height) = ToKinectColorMode(stream.Width);
       }
       else
       {
-        std::tie(status, DeviceConfig.depth_mode, Stream.Height) = ToKinectDepthMode(Stream.Width);
+        std::tie(status, DeviceConfig.depth_mode, stream.Height) = ToKinectDepthMode(stream.Width);
       }
 
       if (!status)
@@ -171,23 +174,23 @@ public:
       }
     }
 
-    auto rgbStream = std::find_if(std::begin(StreamList), std::end(StreamList), [](const KinectStreamConfig & stream)
+    auto rgbStream = std::find_if(std::begin(this->StreamList), std::end(this->StreamList), [](const KinectStreamConfig & stream)
     {
       return stream.Type == KinectSourceType::RGB;
     });
-    auto depthStream = std::find_if(std::begin(StreamList), std::end(StreamList), [](const KinectStreamConfig & stream)
+    auto depthStream = std::find_if(std::begin(this->StreamList), std::end(this->StreamList), [](const KinectStreamConfig & stream)
     {
       return stream.Type == KinectSourceType::DEPTH;
     });
 
-    if (rgbStream == std::end(StreamList) || depthStream == std::end(StreamList))
+    if (rgbStream == std::end(this->StreamList) || depthStream == std::end(this->StreamList))
     {
       DeviceConfig.synchronized_images_only = false;
     }
 
     if (AlignDepthStream)
     {
-      if (rgbStream == std::end(StreamList) || depthStream == std::end(StreamList))
+      if (rgbStream == std::end(this->StreamList) || depthStream == std::end(this->StreamList))
       {
         LOG_ERROR("Invalid configuration: AlignDepthStream set to TRUE but no RGB or Depth stream set");
         return PLUS_FAIL;
@@ -197,8 +200,16 @@ public:
       depthStream->Height = rgbStream->Height;
     }
 
-    External->InternalUpdateRate = DeviceConfig.camera_fps;
-    External->AcquisitionRate = DeviceConfig.camera_fps;
+    std::sort(std::begin(frameRates), std::end(frameRates));
+    frameRates.erase(std::unique(std::begin(frameRates), std::end(frameRates)), std::end(frameRates));
+
+    if (frameRates.size() > 1)
+    {
+      LOG_WARNING("Detecting different frame rates for the streams: selecting the lowest rate (" << frameRates[0] << " fps)");
+    }
+
+    this->External->InternalUpdateRate = frameRates[0];
+    this->External->AcquisitionRate = frameRates[0];
 
     return PLUS_SUCCESS;
   }
@@ -207,7 +218,7 @@ public:
   {
     try
     {
-      Device = k4a::device::open(0);
+      this->Device = k4a::device::open(0);
     }
     catch (const k4a::error& err)
     {
@@ -220,7 +231,7 @@ public:
 
   PlusStatus Disconnect()
   {
-    Device.close();
+    this->Device.close();
     return PLUS_SUCCESS;
   }
 
@@ -228,9 +239,9 @@ public:
   {
     try
     {
-      Device.start_cameras(&DeviceConfig);
-      Calibration = Device.get_calibration(DeviceConfig.depth_mode, DeviceConfig.color_resolution);
-      Transformation = k4a::transformation(Calibration);
+      this->Device.start_cameras(&(this->DeviceConfig));
+      this->Calibration = this->Device.get_calibration(this->DeviceConfig.depth_mode, this->DeviceConfig.color_resolution);
+      this->Transformation = k4a::transformation(this->Calibration);
     }
     catch (const k4a::error& err)
     {
@@ -243,31 +254,31 @@ public:
 
   PlusStatus Stop()
   {
-    Device.stop_cameras();
+    this->Device.stop_cameras();
     return PLUS_SUCCESS;
   }
 
   void Update()
   {
     k4a::capture capture;
-    if (!Device.get_capture(&capture, std::chrono::milliseconds(0)))
+    if (!this->Device.get_capture(&capture, std::chrono::milliseconds(0)))
     {
       return;
     }
 
-    if (DeviceConfig.depth_mode != K4A_DEPTH_MODE_OFF)
+    if (this->DeviceConfig.depth_mode != K4A_DEPTH_MODE_OFF)
     {
-      DepthImage = capture.get_depth_image();
+      this->DepthImage = capture.get_depth_image();
     }
 
-    if (DeviceConfig.color_resolution != K4A_COLOR_RESOLUTION_OFF)
+    if (this->DeviceConfig.color_resolution != K4A_COLOR_RESOLUTION_OFF)
     {
-      ColorImage = capture.get_color_image();
+      this->ColorImage = capture.get_color_image();
     }
 
-    if (ColorImage && DepthImage && AlignDepthStream)
+    if (this->ColorImage && this->DepthImage && this->AlignDepthStream)
     {
-      DepthImage = Transformation.depth_image_to_color_camera(DepthImage);
+      this->DepthImage = this->Transformation.depth_image_to_color_camera(this->DepthImage);
     }
   }
 };
@@ -283,8 +294,8 @@ vtkPlusAzureKinect::vtkPlusAzureKinect()
 //----------------------------------------------------------------------------
 vtkPlusAzureKinect::~vtkPlusAzureKinect()
 {
-  delete Internal;
-  Internal = nullptr;
+  delete this->Internal;
+  this->Internal = nullptr;
 }
 
 //----------------------------------------------------------------------------
@@ -293,8 +304,8 @@ void vtkPlusAzureKinect::PrintSelf(ostream& os, vtkIndent indent)
   this->Superclass::PrintSelf(os, indent);
 
   os << indent << "Kinect Azure Configuration" << std::endl;
-  os << indent << "AlignDepthStream: " << Internal->AlignDepthStream << std::endl;
-  for (const auto& config : Internal->StreamList)
+  os << indent << "AlignDepthStream: " << this->Internal->AlignDepthStream << std::endl;
+  for (const auto& config : this->Internal->StreamList)
   {
     os << indent << "Source: " << config.Id << std::endl;
     os << indent << indent << "Type: " << (config.Type == KinectSourceType::RGB ? "RGB" : "DEPTH") << std::endl;
@@ -339,7 +350,7 @@ PlusStatus vtkPlusAzureKinect::ReadConfiguration(vtkXMLDataElement* rootConfigEl
       XML_READ_ENUM2_ATTRIBUTE_NONMEMBER_REQUIRED(FrameType, config.Type, dataElement, "RGB", KinectSourceType::RGB, "DEPTH", KinectSourceType::DEPTH);
       XML_READ_SCALAR_ATTRIBUTE_NONMEMBER_REQUIRED(int, FrameRate, config.FrameRate, dataElement);
       XML_READ_SCALAR_ATTRIBUTE_NONMEMBER_REQUIRED(int, FrameSize, config.Width, dataElement);
-      Internal->StreamList.push_back(config);
+      this->Internal->StreamList.push_back(config);
     }
     else
     {
@@ -369,13 +380,13 @@ PlusStatus vtkPlusAzureKinect::WriteConfiguration(vtkXMLDataElement* rootConfigE
 
 PlusStatus vtkPlusAzureKinect::NotifyConfigured()
 {
-  return Internal->UpdateConfig();
+  return this->Internal->UpdateConfig();
 }
 
 //----------------------------------------------------------------------------
 PlusStatus vtkPlusAzureKinect::InternalConnect()
 {
-  for (auto& stream : Internal->StreamList)
+  for (auto& stream : this->Internal->StreamList)
   {
     GetVideoSource(stream.Id.c_str(), stream.Source);
     if (stream.Source == nullptr)
@@ -384,13 +395,13 @@ PlusStatus vtkPlusAzureKinect::InternalConnect()
       return PLUS_FAIL;
     }
   }
-  return Internal->Connect();
+  return this->Internal->Connect();
 }
 
 //----------------------------------------------------------------------------
 PlusStatus vtkPlusAzureKinect::InternalDisconnect()
 {
-  return Internal->Disconnect();
+  return this->Internal->Disconnect();
 }
 
 //----------------------------------------------------------------------------
@@ -398,13 +409,13 @@ PlusStatus vtkPlusAzureKinect::InternalStartRecording()
 {
   this->FrameNumber = 0;
 
-  return Internal->Start();
+  return this->Internal->Start();
 }
 
 //----------------------------------------------------------------------------
 PlusStatus vtkPlusAzureKinect::InternalStopRecording()
 {
-  return Internal->Stop();
+  return this->Internal->Stop();
 }
 
 //----------------------------------------------------------------------------
@@ -412,10 +423,10 @@ PlusStatus vtkPlusAzureKinect::InternalStopRecording()
 //----------------------------------------------------------------------------
 PlusStatus vtkPlusAzureKinect::InternalUpdate()
 {
-  Internal->Update();
+  this->Internal->Update();
 
   // forward video data to PlusDataSource
-  for (auto& stream : Internal->StreamList)
+  for (auto& stream : this->Internal->StreamList)
   {
     if (stream.Source->GetNumberOfItems() == 0 && stream.Type == KinectSourceType::RGB)
     {
@@ -437,7 +448,7 @@ PlusStatus vtkPlusAzureKinect::InternalUpdate()
 
     if (stream.Type == KinectSourceType::RGB)
     {
-      if (!Internal->ColorImage)
+      if (!this->Internal->ColorImage)
       {
         if (stream.Source->GetNumberOfItems() == 0)
         {
@@ -451,11 +462,11 @@ PlusStatus vtkPlusAzureKinect::InternalUpdate()
       }
 
       // Kinect Azure has a BGRA layout thus convert to RGB
-      const auto* bgraBuffer = Internal->ColorImage.get_buffer();
-      unsigned rgbSize = Internal->ColorImage.get_size() * 3 / 4;
+      const auto* bgraBuffer = this->Internal->ColorImage.get_buffer();
+      unsigned rgbSize = this->Internal->ColorImage.get_size() * 3 / 4;
       std::unique_ptr<uint8_t[]> rgbBuffer(new uint8_t[rgbSize]);
 
-      for (auto i = 0U; i < Internal->ColorImage.get_size(); i += 4)
+      for (auto i = 0U; i < this->Internal->ColorImage.get_size(); i += 4)
       {
         rgbBuffer[3 * i / 4] = bgraBuffer[i + 2];
         rgbBuffer[3 * i / 4 + 1] = bgraBuffer[i + 1];
@@ -472,7 +483,7 @@ PlusStatus vtkPlusAzureKinect::InternalUpdate()
     }
     else
     {
-      if (!Internal->DepthImage)
+      if (!this->Internal->DepthImage)
       {
         if (stream.Source->GetNumberOfItems() == 0)
         {
@@ -486,7 +497,7 @@ PlusStatus vtkPlusAzureKinect::InternalUpdate()
       }
 
       FrameSizeType frameSizeDepth = {stream.Width, stream.Height, 1};
-      if (stream.Source->AddItem((void*)Internal->DepthImage.get_buffer(), stream.Source->GetInputImageOrientation(), frameSizeDepth, VTK_TYPE_UINT16, 1, US_IMG_BRIGHTNESS, 0, this->FrameNumber) == PLUS_FAIL)
+      if (stream.Source->AddItem((void*)this->Internal->DepthImage.get_buffer(), stream.Source->GetInputImageOrientation(), frameSizeDepth, VTK_TYPE_UINT16, 1, US_IMG_BRIGHTNESS, 0, this->FrameNumber) == PLUS_FAIL)
       {
         LOG_ERROR("Unable to send DEPTH image. Skipping frame.");
         return PLUS_FAIL;
