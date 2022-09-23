8a8f7d30c2b8b6db2b2506382b62b92408f63717
C. G <kenavolic@protonmail.com>
2022-09-23 15:53:58 +0200

    ENH: Add points cloud channel to Revopoint device
    Refactor driver class to be able to add RGB channel more easily
    in the futur

--- src/PlusDataCollection/Revopoint3DCamera/vtkPlusRevopoint3DCamera.h
+++ src/PlusDataCollection/Revopoint3DCamera/vtkPlusRevopoint3DCamera.h
@@ -18,53 +18,56 @@
 class vtkPlusDataCollectionExport vtkPlusRevopoint3DCamera : public vtkPlusDevice
 {
 public:
-    static vtkPlusRevopoint3DCamera* New();
+  static vtkPlusRevopoint3DCamera* New();
 
-    vtkTypeMacro(vtkPlusRevopoint3DCamera, vtkPlusDevice);
-    virtual void PrintSelf(ostream& os, vtkIndent indent);
+  vtkTypeMacro(vtkPlusRevopoint3DCamera, vtkPlusDevice);
+  virtual void PrintSelf(ostream& os, vtkIndent indent);
 
-    /*! Read configuration from xml data */
-    virtual PlusStatus ReadConfiguration(vtkXMLDataElement* config);
-    /*! Write configuration to xml data */
-    PlusStatus WriteConfiguration(vtkXMLDataElement* config);
+  /*! Read configuration from xml data */
+  virtual PlusStatus ReadConfiguration(vtkXMLDataElement* config);
+  /*! Write configuration to xml data */
+  PlusStatus WriteConfiguration(vtkXMLDataElement* config);
 
-    /*!
-    Record incoming data at the specified acquisition rate.  The recording
-    continues indefinitely until StopRecording() is called.
-    */
-    PlusStatus InternalStartRecording();
+  /*!
+  Record incoming data at the specified acquisition rate.  The recording
+  continues indefinitely until StopRecording() is called.
+  */
+  PlusStatus InternalStartRecording();
 
-    /*! Stop recording */
-    PlusStatus InternalStopRecording();
+  /*! Stop recording */
+  PlusStatus InternalStopRecording();
 
-    /*! Is this device a tracker */
-    bool IsTracker() const
-    {
-        return false;
-    }
-    bool IsVirtual() const
-    {
-        return false;
-    }
+  /*! Is this device a tracker */
+  bool IsTracker() const
+  {
+    return false;
+  }
+  bool IsVirtual() const
+  {
+    return false;
+  }
 
-    /*! Get an update from the tracking system and push the new transforms to the tools. This function is called by the tracker thread.*/
-    virtual PlusStatus InternalUpdate();
+  /*! Get an update from the tracking system and push the new transforms to the tools. This function is called by the tracker thread.*/
+  virtual PlusStatus InternalUpdate();
 
-    virtual PlusStatus InternalConnect();
-    virtual PlusStatus InternalDisconnect();
+  virtual PlusStatus InternalConnect();
+  virtual PlusStatus InternalDisconnect();
+
+  /*! Verify the device is correctly configured */
+  virtual PlusStatus NotifyConfigured();
 
 protected:
-    vtkPlusRevopoint3DCamera();
-    ~vtkPlusRevopoint3DCamera();
+  vtkPlusRevopoint3DCamera();
+  ~vtkPlusRevopoint3DCamera();
 
 private:
-    vtkPlusRevopoint3DCamera(const vtkPlusRevopoint3DCamera&);
-    void operator=(const vtkPlusRevopoint3DCamera&);
+  vtkPlusRevopoint3DCamera(const vtkPlusRevopoint3DCamera&);
+  void operator=(const vtkPlusRevopoint3DCamera&);
 
-    class vtkInternal;
-    vtkInternal* Internal;
+  class vtkInternal;
+  vtkInternal* Internal;
 
-    unsigned long FrameNumber;
+  unsigned long FrameNumber;
 };
 
 #endif
